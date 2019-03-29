98046e90f3b2f81848c945f60cd6defc0022141e
wesley1263 <weslei.souza@amcom.com.br>
2019-03-29 20:40:45 -0300

    Criado component do login de usuário

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+
+export default class Login extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+  }
+
+  validForm(){
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit = event => {
+    alert('email: '+ this.state.email + ' -  Password: ' + this.state.password)
+    event.preventDefault();
+  }
+
+  handleChange = event =>{
+    this.setState({
+      [event.target.name] : event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+            <div className="card mb-5 mt-5 mx-auto card-width bg-dark">
+                <div className="card-body">
+                  <div className="card-body text-center text-white">LOGO</div>
+                </div>
+            </div>
+            <div className="card card-login mx-auto mt-5 card-width">
+                <div className="card-body">
+                    <form onSubmit={this.handleSubmit}>
+                        
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input
+                                  autoFocus
+                                  autoComplete="off" 
+                                  name="email"
+                                  type="email" 
+                                  value={this.state.email} 
+                                  onChange={this.handleChange} 
+                                  className="form-control" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Senha</label>
+                            <input  
+                                  type="password" 
+                                  name="password" 
+                                  value={this.state.password} 
+                                  onChange={this.handleChange} 
+                                  className="form-control" />
+
+                            <p className="text-right mt-2"><a href="#teste" className="text-primary">Esqueci minha senha</a></p>
+                        </div>
+
+                        <button className="btn btn-primary btn-block" disabled={!this.validForm()}>Acessar</button>
+                        <p className="text-center mt-3"><a href="#teste" className="text-primary">Ainda não sou cadastrado</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/sb-admin-2.css';
+import './styles/custom.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
