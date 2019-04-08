e1a1b7940aad0afc07f6be3bf430ded0a04c9b53
calvin rossinhole <calvin.masters@gmail.com>
2019-04-08 14:43:04 -0300

    tela de permissoes finalizada

--- src/components/Permissions/Permissions.jsx
+++ src/components/Permissions/Permissions.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Collapse from "react-bootstrap/Collapse";
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
 import "./../Shareable/custom.css";
 
@@ -19,131 +19,125 @@ class Permissions extends Component {
   render() {
     const { openSchool, openDRE, openCODAE, openCompany } = this.state;
     return (
-      <div className="accordion">
-        <h3>Permissões</h3>
-        <div className="card">
-          <div className="card-header">
-            <h5 className="mb-0">
-              <button
-                className="btn btn-link"
-                onClick={() =>
-                  this.setState({ ...this.state, openSchool: !openSchool })
-                }
-                type="button"
-                aria-controls="school-toggle"
-                aria-expanded={openSchool}
-              >
-                Escola
-              </button>
-            </h5>
-          </div>
-          <Collapse in={this.state.openSchool}>
-            <div id="school-toggle">
-              <BrowserRouter basename="/permissions/school">
-                <div>
-                  <Link to="/director">Diretor</Link>
-                </div>
-                <div>
-                  <Link to="/direction-assistant">Assistente de Direção</Link>
-                </div>
-              </BrowserRouter>
+      <div className="container">
+        <div className="accordion">
+          <h3 className="category">Permissões</h3>
+          <div className="card">
+            <div className="card-header">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link"
+                  onClick={() =>
+                    this.setState({ ...this.state, openSchool: !openSchool })
+                  }
+                  type="button"
+                  aria-controls="school-toggle"
+                  aria-expanded={openSchool}
+                >
+                  Escola
+                </button>
+              </h5>
             </div>
-          </Collapse>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h5 className="mb-0">
-              <button
-                className="btn btn-link"
-                onClick={() =>
-                  this.setState({ ...this.state, openDRE: !openDRE })
-                }
-                type="button"
-                aria-controls="dre-toggle"
-                aria-expanded={openDRE}
-              >
-                DRE
-              </button>
-            </h5>
-          </div>
-          <Collapse in={this.state.openDRE}>
-            <div id="dre-toggle">
-              <BrowserRouter basename="/permissions/dre">
+            <Collapse in={this.state.openSchool}>
+              <div id="school-toggle">
                 <div>
-                  <Link to="/submanager">Cogestor</Link>
+                  <Link className="permission" to="/permissions-root/permissions/school/director">Diretor</Link>
                 </div>
-              </BrowserRouter>
+                <div>
+                  <Link className="permission" to="/permissions-root/permissions/school/direction-assistant">Assistente de Direção</Link>
+                </div>
+              </div>
+            </Collapse>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link"
+                  onClick={() =>
+                    this.setState({ ...this.state, openDRE: !openDRE })
+                  }
+                  type="button"
+                  aria-controls="dre-toggle"
+                  aria-expanded={openDRE}
+                >
+                  DRE
+                </button>
+              </h5>
             </div>
-          </Collapse>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h5 className="mb-0">
-              <button
-                className="btn btn-link"
-                onClick={() =>
-                  this.setState({ ...this.state, openCODAE: !openCODAE })
-                }
-                type="button"
-                aria-controls="codae-toggle"
-                aria-expanded={openCODAE}
-              >
-                CODAE
-              </button>
-            </h5>
+            <Collapse in={this.state.openDRE}>
+              <div id="dre-toggle">
+                <div>
+                  <Link className="permission" to="/permissions-root/permissions/DRE/submanager">Cogestor</Link>
+                </div>
+              </div>
+            </Collapse>
           </div>
-          <Collapse in={this.state.openCODAE}>
-            <div id="codae-toggle">
-              <BrowserRouter basename="/permissions/codae">
+          <div className="card">
+            <div className="card-header">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link"
+                  onClick={() =>
+                    this.setState({ ...this.state, openCODAE: !openCODAE })
+                  }
+                  type="button"
+                  aria-controls="codae-toggle"
+                  aria-expanded={openCODAE}
+                >
+                  CODAE
+                </button>
+              </h5>
+            </div>
+            <Collapse in={this.state.openCODAE}>
+              <div id="codae-toggle">
                 <div>
-                  <Link to="/admin">Administrador</Link>
+                  <Link className="permission" to="/permissions-root/permissions/CODAE/admin">Administrador</Link>
                 </div>
                 <div>
-                  <Link to="/nutritionist">Nutricionista</Link>
+                  <Link className="permission" to="/permissions-root/permissions/CODAE/nutritionist">Nutricionista</Link>
                 </div>
                 <div>
-                  <Link to="/supervisor">Supervisor</Link>
+                  <Link className="permission" to="/permissions-root/permissions/CODAE/supervisor">Supervisor</Link>
                 </div>
                 <div>
-                  <Link to="/special-diet">Dieta Especial</Link>
+                  <Link className="permission" to="/permissions-root/permissions/CODAE/special-diet">Dieta Especial</Link>
                 </div>
                 <div>
-                  <Link to="/research-and-development">
+                  <Link className="permission" to="/permissions-root/permissions/CODAE/research-and-development">
                     Pesquisa & Desenvolvimento
                   </Link>
                 </div>
                 <div>
-                  <Link to="/financial">Financeiro</Link>
+                  <Link className="permission" to="/permissions-root/permissions/CODAE/financial">Financeiro</Link>
                 </div>
-              </BrowserRouter>
-            </div>
-          </Collapse>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h5 className="mb-0">
-              <button
-                className="btn btn-link"
-                onClick={() =>
-                  this.setState({ ...this.state, openCompany: !openCompany })
-                }
-                type="button"
-                aria-controls="company-toggle"
-                aria-expanded={openCompany}
-              >
-                Empresa Terceirizada
-              </button>
-            </h5>
+              </div>
+            </Collapse>
           </div>
-          <Collapse in={this.state.openCompany}>
-            <div id="company-toggle">
-              <BrowserRouter basename="/permissions/company">
-                <div>
-                  <Link to="/manager">Gestor</Link>
-                </div>
-              </BrowserRouter>
+          <div className="card">
+            <div className="card-header">
+              <h5 className="mb-0">
+                <button
+                  className="btn btn-link"
+                  onClick={() =>
+                    this.setState({ ...this.state, openCompany: !openCompany })
+                  }
+                  type="button"
+                  aria-controls="company-toggle"
+                  aria-expanded={openCompany}
+                >
+                  Empresa Terceirizada
+                </button>
+              </h5>
             </div>
-          </Collapse>
+            <Collapse in={this.state.openCompany}>
+              <div id="company-toggle">
+                <div>
+                  <Link className="permission" to="/permissions-root/permissions/company/manager">Gestor</Link>
+                </div>
+              </div>
+            </Collapse>
+          </div>
         </div>
       </div>
     );
--- src/components/Permissions/PermissionsCheckBoxes.jsx
+++ src/components/Permissions/PermissionsCheckBoxes.jsx
@@ -1,7 +1,6 @@
-import React, { ReactDOM, Component } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import Collapse from "react-bootstrap/Collapse";
-import { BrowserRouter, Link } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
 import "./../Shareable/custom.css";
 import { CheckBoxListDefault, CheckBoxListDashboard } from './../Shareable/CheckboxList';
@@ -9,21 +8,56 @@ import { CheckBoxListDefault, CheckBoxListDashboard } from './../Shareable/Check
 class PermissionsCheckBoxes extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       openAddFoodFeature: false,
       openMenuChangeFeature: false,
       openMealKitFeature: false,
       openDayMenuChangeFeature: false,
       openOrderDashboardFeature: false,
-
+      openMenuSuspensionFeature: false,
+      openMenuSpecialDietFeature: false,
+      openSpecialDietDashboardFeature: false
     };
   }
+
+  translate = (word) => {
+    switch(word) {
+      case 'school':
+        return 'Escola'
+      case 'director':
+        return 'Diretor'
+      case 'direction-assistant':
+        return 'Assistente de Direção'
+      case 'submanager':
+        return 'Cogestor'
+      case 'admin':
+        return 'Administrador'
+      case 'nutritionist':
+        return 'Nutricionista'
+      case 'supervisor':
+        return 'Supervisor'
+      case 'special-diet':
+        return 'Dieta Especial'
+      case 'research-and-development':
+        return 'P & D'
+      case 'financial':
+        return 'Financeiro'
+      case 'company':
+        return 'Empresa Terceirizada'
+      case 'manager':
+        return 'Gestor'
+      default:
+        return word
+    }
+  }
   render() {
-    const { openAddFoodFeature, openMenuChangeFeature } = this.state;
+    const { openAddFoodFeature, openMenuChangeFeature, openMealKitFeature,
+            openDayMenuChangeFeature, openMenuSuspensionFeature, openMenuSpecialDietFeature,
+            openOrderDashboardFeature, openSpecialDietDashboardFeature } = this.state;
+    const { type, subtype } = this.props.match.params;
     return (
       <div className="accordion">
-        <h3>Permissões de Escola - Gestor</h3>
+        <h3 className="category">Permissões de {this.translate(type)} - {this.translate(subtype)}</h3>
         <div className="card">
           <div className="card-header">
             <h5 className="mb-0">
@@ -40,9 +74,9 @@ class PermissionsCheckBoxes extends Component {
               </button>
             </h5>
           </div>
-          <Collapse in={this.state.openAddFoodFeature}>
+          <Collapse in={openAddFoodFeature}>
             <div id="add-food-feature-toggle">
-              <CheckBoxListDashboard
+              <CheckBoxListDefault
                 typeOfFeature={"Inclusão"}
               />
             </div>
@@ -57,22 +91,163 @@ class PermissionsCheckBoxes extends Component {
                   this.setState({ ...this.state, openMenuChangeFeature: !openMenuChangeFeature })
                 }
                 type="button"
-                aria-controls="add-food-feature-toggle"
+                aria-controls="menu-change-feature-toggle"
                 aria-expanded={openMenuChangeFeature}
               >
                 Alteração de Cardápio
               </button>
             </h5>
           </div>
-          <Collapse in={this.state.openMenuChangeFeature}>
-            <div id="add-food-feature-toggle">
-              <CheckBoxListDashboard
+          <Collapse in={openMenuChangeFeature}>
+            <div id="menu-change-feature-toggle">
+              <CheckBoxListDefault
                 typeOfFeature={"Alteração"}
               />
             </div>
           </Collapse>
         </div>
+        <div className="card">
+          <div className="card-header">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                onClick={() =>
+                  this.setState({ ...this.state, openMealKitFeature: !openMealKitFeature })
+                }
+                type="button"
+                aria-controls="meal-kit-feature-toggle"
+                aria-expanded={openMealKitFeature}
+              >
+                Solicitação de Kit Lanche
+              </button>
+            </h5>
+          </div>
+          <Collapse in={openMealKitFeature}>
+            <div id="meal-kit-feature-toggle">
+              <CheckBoxListDefault
+                typeOfFeature={"Solicitação"}
+              />
+            </div>
+          </Collapse>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                onClick={() =>
+                  this.setState({ ...this.state, openDayMenuChangeFeature: !openDayMenuChangeFeature })
+                }
+                type="button"
+                aria-controls="day-menu-change-feature-toggle"
+                aria-expanded={openDayMenuChangeFeature}
+              >
+                Alteração de Dia de Cardápio
+              </button>
+            </h5>
+          </div>
+          <Collapse in={openDayMenuChangeFeature}>
+            <div id="day-menu-change-feature-toggle">
+              <CheckBoxListDefault
+                typeOfFeature={"Alteração"}
+              />
+            </div>
+          </Collapse>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                onClick={() =>
+                  this.setState({ ...this.state, openOrderDashboardFeature: !openOrderDashboardFeature })
+                }
+                type="button"
+                aria-controls="order-dashboard-feature-toggle"
+                aria-expanded={openDayMenuChangeFeature}
+              >
+                Dashboard de Todos os Pedidos
+              </button>
+            </h5>
+          </div>
+          <Collapse in={openOrderDashboardFeature}>
+            <div id="order-dashboard-feature-toggle">
+              <CheckBoxListDashboard />
+            </div>
+          </Collapse>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                onClick={() =>
+                  this.setState({ ...this.state, openMenuSuspensionFeature: !openMenuSuspensionFeature })
+                }
+                type="button"
+                aria-controls="suspension-menu-feature-toggle"
+                aria-expanded={openMenuSuspensionFeature}
+              >
+                Suspensão de Cardápio
+              </button>
+            </h5>
+          </div>
+          <Collapse in={openMenuSuspensionFeature}>
+            <div id="suspension-menu-feature-toggle">
+              <CheckBoxListDefault
+                typeOfFeature={"Suspensão"}
+              />
+            </div>
+          </Collapse>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                onClick={() =>
+                  this.setState({ ...this.state, openMenuSpecialDietFeature: !openMenuSpecialDietFeature })
+                }
+                type="button"
+                aria-controls="special-diet-feature-toggle"
+                aria-expanded={openMenuSpecialDietFeature}
+              >
+                Solicitação de Dieta Especial
+              </button>
+            </h5>
+          </div>
+          <Collapse in={openMenuSpecialDietFeature}>
+            <div id="special-diet-feature-toggle">
+              <CheckBoxListDefault
+                typeOfFeature={"Solicitação"}
+              />
+            </div>
+          </Collapse>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                onClick={() =>
+                  this.setState({ ...this.state, openSpecialDietDashboardFeature: !openSpecialDietDashboardFeature })
+                }
+                type="button"
+                aria-controls="order-dashboard-feature-toggle"
+                aria-expanded={openSpecialDietDashboardFeature}
+              >
+                Dashboard de Pedidos Dieta Especial
+              </button>
+            </h5>
+          </div>
+          <Collapse in={openSpecialDietDashboardFeature}>
+            <div id="order-dashboard-feature-toggle">
+              <CheckBoxListDashboard />
+            </div>
+          </Collapse>
+        </div>
       </div>
+
     );
   }
 }
