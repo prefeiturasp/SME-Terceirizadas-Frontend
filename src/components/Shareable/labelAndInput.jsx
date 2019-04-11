3c32468bff02c3278e3c4274f5f5de21c80e1b09
Marcelo Maia <mmaia.cc@gmail.com>
2019-04-11 10:28:23 -0300

    incremento.

--- src/components/Shareable/labelAndInput.jsx
+++ src/components/Shareable/labelAndInput.jsx
@@ -167,6 +167,7 @@ export class LabelAndTextArea extends Component {
           editorClassName="editor-class"
           toolbarClassName="toolbar-class"
           className="form-control"
+          placeholder="Sua observação aqui"
           onEditorStateChange={editorState => this.handleChange(editorState)}
         />
         <ErrorAlert meta={this.props.meta} />
