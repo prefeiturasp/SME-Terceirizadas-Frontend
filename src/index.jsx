2b4b1386214f3e966349e1c4cbef3f498763205b
Marcelo Maia <mmaia.cc@gmail.com>
2019-04-24 14:20:00 -0300

    redux dev tools condicional

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+// Redux
+import { applyMiddleware, createStore } from "redux";
+import multi from "redux-multi";
+// Middleware
+import promise from "redux-promise";
+import thunk from "redux-thunk";
+import App from "./App";
+import reducers from "./reducers";
+import * as serviceWorker from "./serviceWorker";
+import "./styles/custom.css";
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'startbootstrap-sb-admin-2/css/sb-admin-2.css'
-
-import './styles/sb-admin-2.css';
-import './styles/custom.css';
-import reducers from './reducers'
-// Redux
-import { applyMiddleware, createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-// Middleware
-import promise from 'redux-promise'
-import multi from 'redux-multi'
-import thunk from 'redux-thunk'
+import "./styles/sb-admin-2.css";
 
 // see https://github.com/zalmoxisus/redux-devtools-extension
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-  && window.__REDUX_DEVTOOLS_EXTENSION__()
+let devTools = undefined;
+if (process.env.NODE_ENV === "development") {
+  devTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+}
 
 // foram aplicados 3 middlewares no createstore.
 // thunk: para que os actionCreators poderem chamar metodos (ideal para usar promises)
 // multi: para retornar uma lista de ações em vez de 1
 // promise: para poder usar o componentWillMount no componente
 
-const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(
+  reducers,
+  devTools
+);
 
 // store é o carinha que recebe todos os estados
 ReactDOM.render(
@@ -36,7 +40,9 @@ ReactDOM.render(
     <Router>
       <App />
     </Router>
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
