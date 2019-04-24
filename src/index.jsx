68c14af4300435df0d6e6475a22c12fc0007f008
Marcelo Maia <mmaia.cc@gmail.com>
2019-04-24 15:38:13 -0300

    ajuste  redux dev tools.

--- src/index.jsx
+++ src/index.jsx
@@ -48,3 +48,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ 
