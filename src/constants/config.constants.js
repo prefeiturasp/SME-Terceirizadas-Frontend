998fa62179b22730f728423dcfe6fd3b688ea187
Marcelo Maia <mmaia.cc@gmail.com>
2019-04-30 15:18:28 -0300

    simplificação dos parametros

--- src/constants/config.constants.js
+++ src/constants/config.constants.js
@@ -1,14 +1,14 @@
 let API_URL = process.env.REACT_APP_API_URL;
-let JWT_AUTH = process.env.REACT_APP_JWT_AUTH;
-let USER_URL = process.env.REACT_APP_USER_URL;
+let JWT_AUTH = `${API_URL}/api-token-auth/`;
+let USER_URL = `${API_URL}/users/`;
 let API_MOCK = process.env.REACT_APP_API_MOCK;
-console.log(JWT_AUTH, 'jwt')
+
 if (process.env.NODE_ENV === "production") {
   // This way we can pass params to static files. see Dockerfile.
   // when build default env is production
   API_URL = "API_URL_REPLACE_ME";
-  JWT_AUTH = "JWT_AUTH_REPLACE_ME";
-  USER_URL = "USER_URL_REPLACE_ME";
+  JWT_AUTH = "API_URL_REPLACE_ME/api-token-auth/";
+  USER_URL = "API_URL_REPLACE_ME/users/";
   API_MOCK = "API_MOCK_REPLACE_ME";
 }
 
