7e7c1f785682a5fa24cbf4a4cb9790903867ae24
Weslei Souza <weslei@andwes.com.br>
2019-06-21 16:11:02 -0300

    Adicionado accordion ao historico

--- src/components/DahsboardEscola/CardHistorico.jsx
+++ src/components/DahsboardEscola/CardHistorico.jsx
@@ -5,19 +5,27 @@ const styleHeader = {
     backgroundColor: '#fff',
     borderBottom: '1px solid #e3e6f0',
     color: '#035D96',
-    fontWeight : 'bold',
+    fontWeight: 'bold',
     letterSpacing: '0.01em',
     fontStyle: 'normal',
     fonFamily: 'Roboto',
-    height : '4em'
+    height: '4em'
 }
 export default props => (
-    <div className="card mt-3">
-        <div className="card-header" style={styleHeader}>
-            <i class="fas fa-history mr-2"></i>
+    <div id="accordion">
+        <div className="card mt-3">
+            <div className="card-header" id="headingOne" style={styleHeader}>
+                <i class="fas fa-history mr-2"></i>
                 Histórico de Alimentações solicitadas
-            <i className="float-right fa fa-bars"></i>
+            <a className="btn btn-link float-right" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <i className="float-right fa fa-bars"></i>
+                </a>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div className="card-body">
+                    <h1>OI GENTE</h1>
+                </div>
+            </div>
         </div>
-        {/* <div className="card-body"></div> */}
     </div>
 )
