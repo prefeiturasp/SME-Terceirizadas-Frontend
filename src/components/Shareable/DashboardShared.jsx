af61bd5af65943ac5bd04acd21f87acd50f36fa8
Calvin Rossinhole <calvin.masters@gmail.com>
2019-06-24 15:58:16 -0300

    dashboard dre - pedidos pendente aprovacao

--- src/components/Shareable/DashboardShared.jsx
+++ src/components/Shareable/DashboardShared.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Stand } from "react-burgers";
+import { Collapse } from "react-collapse";
 import "./custom.css";
 
 export const SolicitationStatusCard = props => {
@@ -82,3 +84,102 @@ PendencyCard.propTypes = {
   onLimitOrders: PropTypes.number,
   regularOrders: PropTypes.number
 };
+
+export class CardPendenciaAprovacao extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true,
+      pedidosFiltrados: this.props.pedidos
+    };
+    this.filtrarPedidos = this.filtrarPedidos.bind(this);
+  }
+
+  filtrarPedidos(event) {
+    if (event === undefined) event = { target: { value: "" } };
+    let pedidosFiltrados = this.props.pedidos;
+    pedidosFiltrados = pedidosFiltrados.filter(function(item) {
+      const palavraAFiltrar = event.target.value.toLowerCase();
+      return (
+        item.escola.nome.toLowerCase().search(palavraAFiltrar) !== -1 ||
+        item.escola.cod_eol.includes(palavraAFiltrar)
+      );
+    });
+    this.setState({ pedidosFiltrados });
+  }
+
+  render() {
+    const {
+      titulo,
+      tipoDeCard,
+      totalDePedidos,
+      totalDeEscolas
+    } = this.props;
+    const { collapsed, pedidosFiltrados } = this.state;
+    return (
+      <div className="card card-pendency-approval">
+        <div className={"card-title " + tipoDeCard}>{titulo}</div>
+        <div className="row">
+          <div className="col-2">
+            <div className={"order-box " + tipoDeCard}>
+              <span className="number">{totalDePedidos}</span>
+              <span className="order">pedidos</span>
+            </div>
+          </div>
+          <div className="col-9">
+            <div className="order-lines">
+              <div className="label" />
+              <span className="text">
+                <span className="value">{totalDeEscolas} </span>escolas
+                solicitantes
+              </span>
+            </div>
+          </div>
+          <div className="col-1">
+            <Stand
+              onClick={() => this.setState({ collapsed: !collapsed })}
+              color={"#C8C8C8"}
+              width={30}
+              padding={0}
+              lineSpacing={5}
+              className="float-right"
+              active={!collapsed}
+            />
+          </div>
+        </div>
+        <Collapse isOpened={!collapsed}>
+          <div className="row ml-1 mr-1">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Pesquisar"
+              onChange={this.filtrarPedidos}
+            />
+            <table className="orders-table mt-4">
+              <thead>
+                <tr>
+                  <th>Nº Pedido</th>
+                  <th>Código</th>
+                  <th>Nome da Escola</th>
+                  <th>Qtd solicitada</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pedidosFiltrados.map((pedido, key) => {
+                  return (
+                    <tr>
+                      <td>{pedido.id}</td>
+                      <td>{pedido.escola.cod_eol}</td>
+                      <td>{pedido.escola.nome}</td>
+                      <td>{pedido.quantidade}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
--- src/components/TourRequest/DRE/OrdersDashboard.jsx
+++ src/components/TourRequest/DRE/OrdersDashboard.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import {
+  CardPendenciaAprovacao
+} from "../../Shareable/DashboardShared";
 
 class OrdersDashboard extends Component {
   constructor(props) {
@@ -7,7 +10,45 @@ class OrdersDashboard extends Component {
   }
 
   render() {
-    return <p>a</p>;
+    const { pedidos } = this.props;
+    return (
+      <div>
+        <span className="page-title">Kits Lanche - Pendentes Aprovação</span>
+        <div className="row pt-3">
+          <div className="col-12">
+            <CardPendenciaAprovacao
+              titulo={"Pedidos próximos ao prazo de vencimento (2 dias ou menos)"}
+              tipoDeCard={"priority"}
+              totalDePedidos={20}
+              totalDeEscolas={13}
+              pedidos={pedidos}
+            />
+          </div>
+        </div>
+        <div className="row pt-3">
+          <div className="col-12">
+            <CardPendenciaAprovacao
+              titulo={"Pedidos no prazo limite"}
+              tipoDeCard={"on-limit"}
+              totalDePedidos={40}
+              totalDeEscolas={8}
+              pedidos={pedidos}
+            />
+          </div>
+        </div>
+        <div className="row pt-3">
+          <div className="col-12">
+            <CardPendenciaAprovacao
+              titulo={"Pedidos no prazo regular"}
+              tipoDeCard={"regular"}
+              totalDePedidos={60}
+              totalDeEscolas={20}
+              pedidos={pedidos}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
