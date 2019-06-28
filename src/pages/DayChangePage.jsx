ba53e7c2fd9ccac3c296ace10bfde02662311492
Wesley1263 <weslei.andrade.souza@gmail.com>
2019-06-28 16:15:39 -0300

    Ajustado template da tela de: Inversão de dia de cardápio

--- src/pages/DayChangePage.jsx
+++ src/pages/DayChangePage.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { DayChangeEditor } from "../components/DayChange/DayChange";
-import { Header } from "../components/Main/Header";
-import { Sidebar } from "../components/Main/Sidebar";
+import Page from "../components/Shareable/Page";
 
 export default props => (
-  <div id="wrapper">
-    <Header />
-    <Sidebar />
-    <div id="content-wrapper" className="pt-5">
-      <div className="d-flex flex-column p-4 mt-5">
-        <DayChangeEditor />
-      </div>
-    </div>
-  </div>
+ <Page tipo="Solicitações" titulo="Inversão de dia de cardápio">
+   <DayChangeEditor />
+ </Page>
 );
