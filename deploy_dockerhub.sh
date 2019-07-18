b7474c9840238f93c74ca2bd2fac6b146813f0ab
Marcelo Maia <mmaia.cc@gmail.com>
2019-07-18 09:32:01 -0300

    build constante de dev e com tag pra produção.

---
+++ deploy_dockerhub.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#https://docs.travis-ci.com/user/environment-variables/#convenience-variables
+#https://docs.travis-ci.com/user/deployment/script/
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+if [ "$TRAVIS_BRANCH" = "development" ]; then
+    echo "Deploy da imagem de desenvolvimento..."
+    docker push marcelomaia/terceirizadas_frontend
+fi
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    tag="$TRAVIS_TAG"
+    if [ -n "$tag" ]; then
+        echo "Deploy da imagem de produção..."
+        docker tag marcelomaia/terceirizadas_frontend:latest marcelomaia/terceirizadas_frontend:$tag
+        docker push marcelomaia/terceirizadas_frontend:$tag
+    fi
+fi
+
