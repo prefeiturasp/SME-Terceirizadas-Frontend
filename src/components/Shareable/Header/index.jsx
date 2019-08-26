16341f2a76b85801b7d75a0242400089b75c6fa1
Calvin Rossinhole <calvin.masters@gmail.com>
2019-08-26 15:30:52 -0300

    header

--- src/components/Shareable/Header/index.jsx
+++ src/components/Shareable/Header/index.jsx
@@ -33,17 +33,24 @@ export class Header extends Component {
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <Link className="nav-link">
-                    Profile &nbsp;
-                    <i className="fa fa-1x fa-user-circle" />
+                    <img src="/assets/image/ajuda.svg" alt="Ícone de ajuda" />
                   </Link>
+                  <p className="title">Ajuda</p>
                 </li>
                 <li className="nav-item">
-                  <BaseButton
-                    className="nav-link"
-                    icon={ButtonIcon.POWER_OFF}
-                    label="Sair &nbsp;"
-                    onClick={() => authService.logout()}
-                  />
+                  <Link className="nav-link">
+                    <img
+                      src="/assets/image/vinculo.svg"
+                      alt="Ícone de vínculo"
+                    />
+                  </Link>
+                  <p className="title">Vínculo</p>
+                </li>
+                <li onClick={() => authService.logout()} className="nav-item">
+                  <Link className="nav-link">
+                    <img src="/assets/image/sair.svg" alt="Ícone de logout" />
+                  </Link>
+                  <p className="title">Sair</p>
                 </li>
               </ul>
             </div>
