80116c3546aa79dabe2cb708f88939175e8c037c
Marcelo Maia <mmaia.cc@gmail.com>
2019-09-27 18:35:49 -0300

    - fazendo busca no canceladas dash escola

--- src/components/screens/DashboardEscola/StatusSolicitacoes.jsx
+++ src/components/screens/DashboardEscola/StatusSolicitacoes.jsx
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 import { ESCOLA, PAINEL_CONTROLE } from "../../../configs/constants";
-import {
-  getSolicitacoesAutorizadasEscola,
-  getSolicitacoesPendentesEscola,
-  getSolicitacoesCanceladasEscola,
-  getSolicitacoesNegadasEscola
-} from "../../../services/painelEscola.service";
+import { getSolicitacoesAutorizadasEscola, getSolicitacoesCanceladasEscola, getSolicitacoesNegadasEscola, getSolicitacoesPendentesEscola } from "../../../services/painelEscola.service";
 import { meusDados } from "../../../services/perfil.service";
 import CardLegendas from "../../Shareable/CardLegendas";
-import {
-  CARD_TYPE_ENUM,
-  ICON_CARD_TYPE_ENUM
-} from "../../Shareable/CardStatusDeSolicitacao/CardStatusDeSolicitacao";
+import { CARD_TYPE_ENUM, ICON_CARD_TYPE_ENUM } from "../../Shareable/CardStatusDeSolicitacao/CardStatusDeSolicitacao";
 import { CardStatusDeSolicitacaoLargo } from "../../Shareable/CardStatusDeSolicitacao/CardStatusDeSolicitacaoLargo";
 import { InputSearch } from "../../Shareable/InputSearch";
-import { ajustarFormatoLog } from "../helper";
 import { STATUS } from "../const";
+import { ajustarFormatoLog } from "../helper";
 
 export default class StatusSolicitacoes extends Component {
   constructor(props, context) {
@@ -28,10 +20,32 @@ export default class StatusSolicitacoes extends Component {
           link: "..."
         }
       ],
+      solicitacoesFiltrados: [
+        {
+          text: "...",
+          date: "...",
+          link: "..."
+        }
+      ],
       titulo: "...",
       tipoCard: "...",
       icone: "..."
     };
+    this.onPesquisarChanged = this.onPesquisarChanged.bind(this);
+  }
+
+  onPesquisarChanged(event) {
+    let solicitacoesFiltrados = this.state.solicitacoes;
+    solicitacoesFiltrados = this.filtrarNome(solicitacoesFiltrados, event);
+    this.setState({ solicitacoesFiltrados });
+  }
+
+  filtrarNome(listaFiltro, event) {
+    listaFiltro = listaFiltro.filter(function(item) {
+      const wordToFilter = event.target.value.toLowerCase();
+      return item.text.toLowerCase().search(wordToFilter) !== -1;
+    });
+    return listaFiltro;
   }
 
   async componentDidMount() {
@@ -77,21 +91,30 @@ export default class StatusSolicitacoes extends Component {
     }
 
     solicitacoes = ajustarFormatoLog(solicitacoes.results);
-    this.setState({ solicitacoes, tipoCard, icone, titulo });
+    this.setState({
+      solicitacoes,
+      solicitacoesFiltrados: solicitacoes,
+      tipoCard,
+      icone,
+      titulo
+    });
   }
 
   render() {
-    const { solicitacoes, titulo, tipoCard, icone } = this.state;
+    const { solicitacoesFiltrados, titulo, tipoCard, icone } = this.state;
     return (
       <div className="card mt-3">
         <div className="card-body">
           <div className="pr-3">
-            <InputSearch voltarLink={`/${ESCOLA}/${PAINEL_CONTROLE}`} />
+            <InputSearch
+              voltarLink={`/${ESCOLA}/${PAINEL_CONTROLE}`}
+              filterList={this.onPesquisarChanged}
+            />
           </div>
           <div className="pb-3" />
           <CardStatusDeSolicitacaoLargo
             titulo={titulo}
-            solicitacoes={solicitacoes}
+            solicitacoes={solicitacoesFiltrados}
             tipo={tipoCard}
             icone={icone}
           />
