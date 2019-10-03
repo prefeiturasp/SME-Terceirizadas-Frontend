b1414a3880efb7cfa5d2135b7fa43294824345d5
Marcelo Maia <mmaia.cc@gmail.com>
2019-10-03 18:03:59 -0300

    tirados endpopints redundantes

--- src/components/InclusaoDeAlimentacao/DRE/PainelPedidos/index.jsx
+++ src/components/InclusaoDeAlimentacao/DRE/PainelPedidos/index.jsx
@@ -1,29 +1,25 @@
 import React, { Component } from "react";
-import { CardPendenciaAprovacao } from "../../components/CardPendenciaAprovacao";
-import { FiltroEnum } from "../../../../constants/filtroEnum";
-import { Select } from "../../../Shareable/Select";
 import { connect } from "react-redux";
-import { dataAtualDDMMYYYY } from "../../../../helpers/utilities";
 import { Field, formValueSelector, reduxForm } from "redux-form";
-import { formatarPedidos } from "./helper";
-import {
-  getDiretoriaRegionalPedidosPrioritarios as prioritariosContinuo,
-  getDiretoriaRegionalPedidosNoPrazoLimite as limitesContinuo,
-  getDiretoriaRegionalPedidosNoPrazoRegular as regularesContinuo
-} from "../../../../services/inclusaoDeAlimentacaoContinua.service";
-import {
-  getDiretoriaRegionalPedidosPrioritarios as prioritariosAvulso,
-  getDiretoriaRegionalPedidosNoPrazoLimite as limitesAvulso,
-  getDiretoriaRegionalPedidosNoPrazoRegular as regularesAvulso
-} from "../../../../services/inclusaoDeAlimentacaoAvulsa.service";
-import CardHistorico from "../../components/CardHistorico";
 import { DRE } from "../../../../configs/constants";
+import { FiltroEnum } from "../../../../constants/filtroEnum";
+import { dataAtualDDMMYYYY } from "../../../../helpers/utilities";
+import { getDiretoriaRegionalPedidosDeInclusaoAlimentacaoAvulsa } from "../../../../services/inclusaoDeAlimentacaoAvulsa.service";
+import { getDREPedidosInclusaoContinuosPendentes } from "../../../../services/inclusaoDeAlimentacaoContinua.service";
+import {
+  filtraNoLimite,
+  filtraPrioritarios,
+  filtraRegular
+} from "../../../InversaoDeDiaDeCardapio/DRE/PainelPedidos/helper";
+import { Select } from "../../../Shareable/Select";
+import CardHistorico from "../../components/CardHistorico";
+import { CardPendenciaAprovacao } from "../../components/CardPendenciaAprovacao";
+import { formatarPedidos } from "./helper";
 
 class PainelPedidos extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pedidosCarregados: 0,
       pedidosPrioritarios: [],
       pedidosNoPrazoLimite: [],
       pedidosNoPrazoRegular: []
@@ -31,61 +27,32 @@ class PainelPedidos extends Component {
   }
 
   filtrar(filtro) {
-    let pedidosPrioritarios = [];
-    let pedidosNoPrazoLimite = [];
-    let pedidosNoPrazoRegular = [];
-    this.setState({ pedidosCarregados: 0 });
-    prioritariosContinuo(filtro).then(response => {
-      pedidosPrioritarios = pedidosPrioritarios.concat(response.results);
-      this.setState({
-        pedidosPrioritarios,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-
-    prioritariosAvulso(filtro).then(response => {
-      pedidosPrioritarios = pedidosPrioritarios.concat(response.results);
-      this.setState({
-        pedidosPrioritarios,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-
-    limitesContinuo(filtro).then(response => {
-      pedidosNoPrazoLimite = pedidosNoPrazoLimite.concat(response.results);
-      this.setState({
-        pedidosNoPrazoLimite,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-
-    limitesAvulso(filtro).then(response => {
-      pedidosNoPrazoLimite = pedidosNoPrazoLimite.concat(response.results);
-      this.setState({
-        pedidosNoPrazoLimite,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-
-    regularesContinuo(filtro).then(response => {
-      pedidosNoPrazoRegular = pedidosNoPrazoRegular.concat(response.results);
-      this.setState({
-        pedidosNoPrazoRegular,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-
-    regularesAvulso(filtro).then(response => {
-      pedidosNoPrazoRegular = pedidosNoPrazoRegular.concat(response.results);
-      this.setState({
-        pedidosNoPrazoRegular,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
+    this.atualizarDadosDasInclusoes(filtro);
   }
 
-  componentDidMount() {
-    this.filtrar(FiltroEnum.SEM_FILTRO);
+  async componentDidMount() {
+    const filtro = FiltroEnum.SEM_FILTRO;
+    this.atualizarDadosDasInclusoes(filtro);
+  }
+
+  async atualizarDadosDasInclusoes(filtro) {
+    const inclusoesAvulsas = await getDiretoriaRegionalPedidosDeInclusaoAlimentacaoAvulsa(
+      filtro
+    );
+    const inclusoesContinuas = await getDREPedidosInclusaoContinuosPendentes(
+      filtro
+    );
+    const inclusoesMescladas = inclusoesAvulsas.results.concat(
+      inclusoesContinuas.results
+    );
+    const pedidosPrioritarios = filtraPrioritarios(inclusoesMescladas);
+    const pedidosNoPrazoLimite = filtraNoLimite(inclusoesMescladas);
+    const pedidosNoPrazoRegular = filtraRegular(inclusoesMescladas);
+    this.setState({
+      pedidosPrioritarios,
+      pedidosNoPrazoLimite,
+      pedidosNoPrazoRegular
+    });
   }
 
   onFiltroSelected(value) {
@@ -99,29 +66,8 @@ class PainelPedidos extends Component {
     }
   }
 
-  filtrarHoje() {
-    let pedidosPrioritarios = [];
-    this.setState({ pedidosCarregados: 4 });
-    prioritariosContinuo(FiltroEnum.HOJE).then(response => {
-      pedidosPrioritarios = pedidosPrioritarios.concat(response.results);
-      this.setState({
-        pedidosPrioritarios,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-
-    prioritariosAvulso(FiltroEnum.HOJE).then(response => {
-      pedidosPrioritarios = pedidosPrioritarios.concat(response.results);
-      this.setState({
-        pedidosPrioritarios,
-        pedidosCarregados: this.state.pedidosCarregados + 1
-      });
-    });
-  }
-
   render() {
     const {
-      pedidosCarregados,
       pedidosPrioritarios,
       pedidosNoPrazoLimite,
       pedidosNoPrazoRegular
@@ -132,7 +78,7 @@ class PainelPedidos extends Component {
       pedidosAprovados,
       pedidosReprovados
     } = this.props;
-    const todosOsPedidosForamCarregados = pedidosCarregados === 6;
+    const todosOsPedidosForamCarregados = true;
     return (
       <div>
         {!todosOsPedidosForamCarregados ? (
