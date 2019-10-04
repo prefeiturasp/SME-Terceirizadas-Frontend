fa88241a5691cdf32c5eb2fa6da08e13618a2ab1
Calvin Rossinhole <calvin.masters@gmail.com>
2019-10-04 16:03:55 -0300

    removendo super perfil

--- src/components/Shareable/Sidebar/index.jsx
+++ src/components/Shareable/Sidebar/index.jsx
@@ -10,13 +10,11 @@ export class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      perfil: "escola",
       toggled: false
     };
   }
 
   render() {
-    const { perfil } = this.state;
     const tipo_perfil = sessionStorage.getItem("tipo_perfil");
     const {
       nome,
@@ -35,36 +33,6 @@ export class Sidebar extends Component {
         >
           <div className="sidebar-divider my-0" />
           {/* Somente para testar o sidebar enquanto ainda não há perfil/permissões */}
-          {!toggled && tipo_perfil === `"admin"` && (
-            <div className="testing-sidebar row">
-              <div
-                onClick={() => this.setState({ perfil: "escola" })}
-                className={`col-2 ${perfil === "escola" && "font-weight-bold"}`}
-              >
-                Escola
-              </div>
-              <div
-                onClick={() => this.setState({ perfil: "diretoria_regional" })}
-                className={`col-2 ${perfil === "diretoria_regional" &&
-                  "font-weight-bold"}`}
-              >
-                DRE
-              </div>
-              <div
-                onClick={() => this.setState({ perfil: "codae" })}
-                className={`col-2 ${perfil === "codae" && "font-weight-bold"}`}
-              >
-                CODAE
-              </div>
-              <div
-                onClick={() => this.setState({ perfil: "terceirizada" })}
-                className={`col-2 ${perfil === "terceirizada" &&
-                  "font-weight-bold"}`}
-              >
-                Terceirizada
-              </div>
-            </div>
-          )}
           <p onClick={() => toggle()} className="text-right c-pointer">
             <i
               className={
@@ -109,21 +77,12 @@ export class Sidebar extends Component {
               </span>
             </div>
           </div>
-          {tipo_perfil !== `"admin"` ? (
-            <div className="sidebar-wrapper">
-              {tipo_perfil === `"codae"` && <SidebarCODAE />}
-              {tipo_perfil === `"diretoria_regional"` && <SidebarDRE />}
-              {tipo_perfil === `"escola"` && <SidebarEscola />}
-              {tipo_perfil === `"terceirizada"` && <SidebarTerceirizada />}
-            </div>
-          ) : (
-            <div className="sidebar-wrapper">
-              {perfil === "codae" && <SidebarCODAE />}
-              {perfil === "diretoria_regional" && <SidebarDRE />}
-              {perfil === "escola" && <SidebarEscola />}
-              {perfil === "terceirizada" && <SidebarTerceirizada />}
-            </div>
-          )}
+          <div className="sidebar-wrapper">
+            {tipo_perfil === `"codae"` && <SidebarCODAE />}
+            {tipo_perfil === `"diretoria_regional"` && <SidebarDRE />}
+            {tipo_perfil === `"escola"` && <SidebarEscola />}
+            {tipo_perfil === `"terceirizada"` && <SidebarTerceirizada />}
+          </div>
           {!toggled && (
             <div className="text-center page-footer mx-auto justify-content-center mt-5 pb-2">
               <img
