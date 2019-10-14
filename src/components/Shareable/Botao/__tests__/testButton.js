c53e436f4f0f1cf882fed1c55d0f1cea1a832d53
Calvin Rossinhole <calvin.masters@gmail.com>
2019-10-14 11:27:45 -0300

    testes novo botao

--- src/components/Shareable/Botao/__tests__/testButton.js
+++ src/components/Shareable/Botao/__tests__/testButton.js
@@ -1,19 +1,20 @@
 import { mount } from "enzyme";
 import React from "react";
-import Button, { ButtonIcon, ButtonStyle, ButtonType } from "../button";
+import { Botao } from "../index";
+import { BUTTON_TYPE, BUTTON_STYLE, BUTTON_ICON } from "../constants";
 
-describe("Test <Button>", () => {
+describe("Test <Botao>", () => {
   let wrapper;
   const onClick = jest.fn();
 
   beforeAll(() => {
     wrapper = mount(
-      <Button
-        type={ButtonType.SUBMIT}
-        icon={ButtonIcon.FOLDER}
-        style={ButtonStyle.OutlineDark}
+      <Botao
+        type={BUTTON_TYPE.SUBMIT}
+        style={BUTTON_STYLE.GREEN}
         onClick={onClick}
-        label={"my label"}
+        icon={BUTTON_ICON.ARROW_LEFT}
+        texto={"texto do botao"}
         className="btn-block"
       />
     );
@@ -24,23 +25,15 @@ describe("Test <Button>", () => {
   });
 
   it("renders label props", () => {
-    expect(wrapper.props().label).toBe("my label");
+    expect(wrapper.props().texto).toBe("texto do botao");
   });
 
   it("renders correct style props", () => {
-    expect(wrapper.props().style).toBe("outline-dark");
+    expect(wrapper.props().style).toBe("green-button");
   });
 
   it("correct type props", () => {
     expect(wrapper.props().type).toBe("submit");
-  });
-
-  it("correct className props", () => {
-    expect(wrapper.props().className).toBe("btn-block");
-  });
-
-  it("correct icon props", () => {
-    expect(wrapper.props().icon).toBe("folder");
   });
 
   it("disabled by default", () => {
@@ -48,7 +41,9 @@ describe("Test <Button>", () => {
   });
 
   it("correct div>icon", () => {
-    expect(wrapper.find("i").props().className).toBe("pr-3 fas fa-folder");
+    expect(wrapper.find("i").props().className).toBe(
+      "fas fa-arrow-left text-and-icon"
+    );
   });
 
   it("clicked 2 times", () => {
