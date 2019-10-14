305812438e7b3f5f99d153121e592dabc65f1589
Marcelo Maia <mmaia.cc@gmail.com>
2019-10-14 18:26:40 -0300

    configuração code climate

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@
 # Example:
 # docker run  -p 8081:80 -e API_URL="http://localhost:8000" -e SERVER_NAME="localhost" marcelomaia/terceirizadas_frontend:latest
 set -xe
-  : "${API_URL?Need an api url}"
+  : "${API_URL?Precisa de uma variavel de ambiente API_URL}"
+
+set -xe
+  : "${SERVER_NAME?Precisa de uma variavel de ambiente SERVER_NAME}"
 
 sed -i "s,API_URL_REPLACE_ME,$API_URL,g" /usr/share/nginx/html/static/js/main*.js
 sed -i "s,SERVER_NAME,$SERVER_NAME,g" /etc/nginx/conf.d/default.conf
