515f34b804810dfcd58f313d17760d3c88787bce
Calvin Rossinhole <calvin.masters@gmail.com>
2019-10-15 16:27:33 -0300

    adicionando alguns testes

--- src/helpers/__tests__/utilities/teste_dataPrioriaria.js
+++ src/helpers/__tests__/utilities/teste_dataPrioriaria.js
@@ -1,4 +1,4 @@
-import { dataPrioritaria } from "../utilities";
+import { dataPrioritaria } from "../../utilities";
 
 //TODO: fazer iterando valores
 describe("Teste data prioritária", () => {
---
+++ src/helpers/__tests__/utilities/teste_mensagemCancelamento.js
@@ -0,0 +1,33 @@
+import { mensagemCancelamento } from "../../utilities";
+import { statusEnum } from "../../../constants/statusEnum";
+
+describe("Teste mensagemCancelamento", () => {
+  it("retorna mensagem dre a validar", () => {
+    expect(mensagemCancelamento(statusEnum.DRE_A_VALIDAR)).toEqual(
+      "Esta solicitação está aguardando validação pela DRE. "
+    );
+  });
+  it("retorna mensagem dre validou", () => {
+    expect(mensagemCancelamento(statusEnum.DRE_VALIDADO)).toEqual(
+      "Esta solicitação já foi validada pela DRE. "
+    );
+  });
+  it("retorna mensagem dre validou", () => {
+    expect(mensagemCancelamento(statusEnum.CODAE_A_AUTORIZAR)).toEqual(
+      "Esta solicitação já foi validada pela DRE. "
+    );
+  });
+  it("retorna mensagem codae autorizou", () => {
+    expect(mensagemCancelamento(statusEnum.TERCEIRIZADA_TOMOU_CIENCIA)).toEqual(
+      "Esta solicitação já foi autorizada pela CODAE. "
+    );
+  });
+  it("retorna mensagem codae autorizou", () => {
+    expect(mensagemCancelamento(statusEnum.CODAE_AUTORIZADO)).toEqual(
+      "Esta solicitação já foi autorizada pela CODAE. "
+    );
+  });
+  it("retorna nada", () => {
+    expect(mensagemCancelamento("nada")).toEqual("");
+  });
+});
