4601875db53d00639aa1687e5c6773f90edf1d7f
Calvin Rossinhole <calvin.masters@gmail.com>
2019-10-24 11:35:00 -0300

    prettier

--- src/components/Login/validar.js
+++ src/components/Login/validar.js
@@ -2,16 +2,16 @@ import { validarCPF } from "../../helpers/utilities";
 
 export const validarForm = values => {
   let erro = false;
-  if (values.email.includes('@')) {
-    erro = "Campo e-mail não deve conter @"
+  if (values.email.includes("@")) {
+    erro = "Campo e-mail não deve conter @";
   } else if (values.registro_funcional.length !== 7) {
-    erro = "Campo registro funcional deve conter 7 números"
+    erro = "Campo registro funcional deve conter 7 números";
   } else if (!values.termos) {
-    erro = "É necessário aceitar os termos de uso"
+    erro = "É necessário aceitar os termos de uso";
   } else if (!validarCPF(values.cpf)) {
     erro = "CPF inválido";
   } else if (values.password !== values.confirmar_password) {
-    erro = "Campos senha e confirmar senha divergem"
+    erro = "Campos senha e confirmar senha divergem";
   }
   return erro;
-}
+};
