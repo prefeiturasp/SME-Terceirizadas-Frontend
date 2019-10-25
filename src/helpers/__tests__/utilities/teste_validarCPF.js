04c1512735a1689534a312b023cf0743eb5ed588
Calvin Rossinhole <calvin.masters@gmail.com>
2019-10-25 11:19:16 -0300

    ajustes requisitados

---
+++ src/helpers/__tests__/utilities/teste_validarCPF.js
@@ -0,0 +1,12 @@
+import { validarCPF } from "../../utilities";
+
+describe.each([
+  ["57588740272", true],
+  ["11111111111", false],
+  ["38301662654", true],
+  ["00000000000", false]
+])(".validarCPF(%s, %b)", (cpf, expected) => {
+  test(`returns ${expected}`, () => {
+    expect(validarCPF(cpf)).toBe(expected);
+  });
+});
