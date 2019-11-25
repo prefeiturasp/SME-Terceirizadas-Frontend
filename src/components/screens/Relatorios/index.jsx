f41ed7059d0c63afb13bbf38766f3e7c2fa548a7
Calvin Rossinhole <calvin.masters@gmail.com>
2019-11-25 18:11:50 -0300

    prettier

--- src/components/screens/Relatorios/index.jsx
+++ src/components/screens/Relatorios/index.jsx
@@ -150,34 +150,34 @@ class Relatorios extends Component {
                 </div>
               )}
               {graficoEvolucao && (
-              <div className="row">
-                <div className="col-8 legend">
-                  <div className="row">
-                    {TIPOS_SOLICITACAO_LISTA.map(tipoSolicitacao => {
-                      return (
-                        graficoEvolucao[tipoSolicitacao.titulo] && (
-                          <div className="col-4">
-                            <div className="legend-unit">
-                              <div className="title small">
-                                <div
-                                  className="circle"
-                                  style={{
-                                    backgroundColor: tipoSolicitacao.cor
-                                  }}
-                                />
-                                <div className="text">
-                                  {tipoSolicitacao.titulo}
+                <div className="row">
+                  <div className="col-8 legend">
+                    <div className="row">
+                      {TIPOS_SOLICITACAO_LISTA.map(tipoSolicitacao => {
+                        return (
+                          graficoEvolucao[tipoSolicitacao.titulo] && (
+                            <div className="col-4">
+                              <div className="legend-unit">
+                                <div className="title small">
+                                  <div
+                                    className="circle"
+                                    style={{
+                                      backgroundColor: tipoSolicitacao.cor
+                                    }}
+                                  />
+                                  <div className="text">
+                                    {tipoSolicitacao.titulo}
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                        )
-                      );
-                    })}
+                          )
+                        );
+                      })}
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
             </div>
           </div>
         </div>
