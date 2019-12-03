92fac0be91d1b9694d2b5f281eb27d0c30191bbe
Marcelo Maia <mmaia.cc@gmail.com>
2019-12-03 15:18:53 -0300

    sentry adicionado no react

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,12 @@ set -xe
 set -xe
   : "${SERVER_NAME?Precisa de uma variavel de ambiente SERVER_NAME}"
 
+set -xe
+  : "${SENTRY_URL?Precisa de uma variavel de ambiente SENTRY_URL}"
+
+
 sed -i "s,API_URL_REPLACE_ME,$API_URL,g" /usr/share/nginx/html/static/js/main*.js
 sed -i "s,SERVER_NAME,$SERVER_NAME,g" /etc/nginx/conf.d/default.conf
+sed -i "s,SENTRY_URL_REPLACE_ME,$SENTRY_URL,g" /usr/share/nginx/html/static/js/main*.js
 
 exec "$@"
