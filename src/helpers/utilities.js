04246caf22b917874acec28f854303d8ac2f68f5
Calvin Rossinhole <calvin.masters@gmail.com>
2019-12-17 15:46:55 -0300

    implementa tela de permissoes de administradores terceirizada

--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -234,3 +234,14 @@ export const visualizaBotoesDoFluxo = solicitacao => {
       return false;
   }
 };
+
+export const formatarCPFouCNPJ = value => {
+  const cnpjCpf = value.replace(/\D/g, "");
+  if (cnpjCpf.length === 11) {
+    return cnpjCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+  }
+  return cnpjCpf.replace(
+    /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,
+    "$1.$2.$3/$4-$5"
+  );
+};
