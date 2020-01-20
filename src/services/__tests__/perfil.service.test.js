342c0a33e1328c3a24e5faa090bb7c0860a478ea
Pedro <pedrohardware@gmail.com>
2020-01-20 11:29:13 -0300

    Implementa busca de dados pelo cod EOL

---
+++ src/services/__tests__/perfil.service.test.js
@@ -0,0 +1,18 @@
+import fetchMock from "fetch-mock";
+
+import { API_URL } from "../../constants/config.constants";
+import { obtemDadosAlunoPeloEOL } from "../perfil.service";
+
+fetchMock.get(`${API_URL}/dados-alunos-eol/123456`, {
+  detail: { detalhes: "do aluno" }
+});
+
+describe("test perfil.service", () => {
+  it("obtemDadosAlunoPeloEOL", async () => {
+    const response = await obtemDadosAlunoPeloEOL(123456);
+    expect(response).toEqual({
+      detail: { detalhes: "do aluno" },
+      status: 200
+    });
+  });
+});
