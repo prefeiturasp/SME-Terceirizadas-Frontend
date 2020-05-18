482ba662ee6748e1d9244b7e19299236de3cfb51
Pedro Walter <pedrohardware@gmail.com>
2020-05-18 13:33:41 -0300

    Reordena cards dashboard gestao produto

--- src/helpers/gestaoDeProdutos.js
+++ src/helpers/gestaoDeProdutos.js
@@ -13,70 +13,66 @@ const {
   CODAE_NAO_HOMOLOGADO
 } = ENDPOINT_HOMOLOGACOES_PRODUTO_STATUS;
 
-const CARDS_COMUNS = [
-  {
-    id: CARD_ID.RECLAMACAO_DE_PRODUTO,
-    titulo: "Reclamação de produto",
-    icon: "fa-bullhorn",
-    style: "card-complained",
-    rota: ROTA.RECLAMACAO_DE_PRODUTO,
-    incluir_status: [CODAE_PEDIU_ANALISE_RECLAMACAO]
-  },
-  {
-    id: CARD_ID.PRODUTOS_SUSPENSOS,
-    titulo: "Produtos suspensos",
-    icon: "fa-hand-paper",
-    style: "card-cancelled",
-    rota: ROTA.PRODUTOS_SUSPENSOS,
-    incluir_status: [CODAE_SUSPENDEU]
-  },
-  {
-    id: CARD_ID.HOMOLOGADOS,
-    titulo: "Homologados",
-    icon: "fa-check",
-    style: "card-authorized",
-    rota: ROTA.SOLICITACOES_HOMOLOGADAS,
-    incluir_status: [CODAE_HOMOLOGADO]
-  },
-  {
-    id: CARD_ID.NAO_HOMOLOGADOS,
-    titulo: "Não homologados",
-    icon: "fa-ban",
-    style: "card-denied",
-    rota: ROTA.SOLICITACOES_NAO_HOMOLOGADAS,
-    incluir_status: [CODAE_NAO_HOMOLOGADO]
-  }
-];
-const CARDS_GESTAO = [
-  {
-    id: CARD_ID.AGUARDANDO_ANALISE_RECLAMACAO,
-    titulo: "Aguardando análise de reclamação",
-    titulo_menu: "Ag. análise de reclamação", // FIXME: Confirmar nome no menu
-    icon: "fa-history",
-    style: "card-awaiting-complain",
-    rota: ROTA.AGUARDANDO_ANALISE_RECLAMACAO,
-    incluir_status: [CODAE_QUESTIONADO]
-  },
-  {
-    id: CARD_ID.AGUARDANDO_ANALISE_SENSORIAL,
-    titulo: "Aguardando análise sensorial",
-    titulo_menu: "Ag. análise sensorial",
-    icon: "fa-search",
-    style: "card-awaiting-sensory",
-    rota: ROTA.AGUARDANDO_ANALISE_SENSORIAL,
-    incluir_status: [CODAE_PEDIU_ANALISE_SENSORIAL]
-  },
-  {
-    id: CARD_ID.PENDENTE_HOMOLOGACAO,
-    titulo: "Pendente homologação",
-    icon: "fa-exclamation-triangle",
-    style: "card-pending",
-    rota: ROTA.SOLICITACOES_PENDENTE_HOMOLOGACAO,
-    incluir_status: [CODAE_PENDENTE_HOMOLOGACAO]
-  }
-];
+const CARD_RECLAMACAO_DE_PRODUTO = {
+  id: CARD_ID.RECLAMACAO_DE_PRODUTO,
+  titulo: "Reclamação de produto",
+  icon: "fa-bullhorn",
+  style: "card-complained",
+  rota: ROTA.RECLAMACAO_DE_PRODUTO,
+  incluir_status: [CODAE_PEDIU_ANALISE_RECLAMACAO]
+};
+const CARD_PRODUTOS_SUSPENSOS = {
+  id: CARD_ID.PRODUTOS_SUSPENSOS,
+  titulo: "Produtos suspensos",
+  icon: "fa-hand-paper",
+  style: "card-cancelled",
+  rota: ROTA.PRODUTOS_SUSPENSOS,
+  incluir_status: [CODAE_SUSPENDEU]
+};
+const CARD_HOMOLOGADOS = {
+  id: CARD_ID.HOMOLOGADOS,
+  titulo: "Homologados",
+  icon: "fa-check",
+  style: "card-authorized",
+  rota: ROTA.SOLICITACOES_HOMOLOGADAS,
+  incluir_status: [CODAE_HOMOLOGADO]
+};
+const CARD_NAO_HOMOLOGADOS = {
+  id: CARD_ID.NAO_HOMOLOGADOS,
+  titulo: "Não homologados",
+  icon: "fa-ban",
+  style: "card-denied",
+  rota: ROTA.SOLICITACOES_NAO_HOMOLOGADAS,
+  incluir_status: [CODAE_NAO_HOMOLOGADO]
+};
+const CARD_AGUARDANDO_ANALISE_RECLAMACAO = {
+  id: CARD_ID.AGUARDANDO_ANALISE_RECLAMACAO,
+  titulo: "Aguardando análise de reclamação",
+  titulo_menu: "Ag. análise de reclamação", // FIXME: Confirmar nome no menu
+  icon: "fa-history",
+  style: "card-awaiting-complain",
+  rota: ROTA.AGUARDANDO_ANALISE_RECLAMACAO,
+  incluir_status: [CODAE_QUESTIONADO]
+};
+const CARD_AGUARDANDO_ANALISE_SENSORIAL = {
+  id: CARD_ID.AGUARDANDO_ANALISE_SENSORIAL,
+  titulo: "Aguardando análise sensorial",
+  titulo_menu: "Ag. análise sensorial",
+  icon: "fa-search",
+  style: "card-awaiting-sensory",
+  rota: ROTA.AGUARDANDO_ANALISE_SENSORIAL,
+  incluir_status: [CODAE_PEDIU_ANALISE_SENSORIAL]
+};
+const CARD_PENDENTE_HOMOLOGACAO = {
+  id: CARD_ID.PENDENTE_HOMOLOGACAO,
+  titulo: "Pendente homologação",
+  icon: "fa-exclamation-triangle",
+  style: "card-pending",
+  rota: ROTA.SOLICITACOES_PENDENTE_HOMOLOGACAO,
+  incluir_status: [CODAE_PENDENTE_HOMOLOGACAO]
+};
 
-const CARD_CORRECAO = {
+const CARD_CORRECAO_DE_PRODUTO = {
   id: CARD_ID.CORRECAO_DE_PRODUTO,
   titulo: "Correção de produto",
   icon: "fa-pencil-alt",
@@ -87,22 +83,43 @@ const CARD_CORRECAO = {
 
 export const listarCardsPermitidos = () => {
   const perfil = localStorage.getItem("tipo_perfil");
-  const cards = CARDS_COMUNS.concat();
 
-  switch (perfil) {
-    case TIPO_PERFIL.TERCEIRIZADA:
-    case TIPO_PERFIL.GESTAO_ALIMENTACAO_TERCEIRIZADA:
-      cards.push(...CARDS_GESTAO, CARD_CORRECAO);
-      break;
-    case TIPO_PERFIL.GESTAO_PRODUTO:
-      cards.push(...CARDS_GESTAO);
-      cards
-        .filter(card => card.id === CARD_ID.PENDENTE_HOMOLOGACAO)[0]
-        .incluir_status.push(CODAE_AUTORIZOU_RECLAMACAO);
-      break;
-    default:
-      return cards;
+  if (perfil === TIPO_PERFIL.GESTAO_PRODUTO) {
+    const cardPendenteHomologacao = Object.assign(
+      {},
+      CARD_PENDENTE_HOMOLOGACAO
+    );
+    cardPendenteHomologacao.incluir_status.push(CODAE_AUTORIZOU_RECLAMACAO);
+    return [
+      cardPendenteHomologacao,
+      CARD_RECLAMACAO_DE_PRODUTO,
+      CARD_AGUARDANDO_ANALISE_SENSORIAL,
+      CARD_PRODUTOS_SUSPENSOS,
+      CARD_HOMOLOGADOS,
+      CARD_NAO_HOMOLOGADOS,
+      CARD_AGUARDANDO_ANALISE_RECLAMACAO
+    ];
+  } else if (
+    [
+      TIPO_PERFIL.TERCEIRIZADA,
+      TIPO_PERFIL.GESTAO_ALIMENTACAO_TERCEIRIZADA
+    ].includes(perfil)
+  ) {
+    return [
+      CARD_RECLAMACAO_DE_PRODUTO,
+      CARD_PRODUTOS_SUSPENSOS,
+      CARD_CORRECAO_DE_PRODUTO,
+      CARD_AGUARDANDO_ANALISE_RECLAMACAO,
+      CARD_AGUARDANDO_ANALISE_SENSORIAL,
+      CARD_PENDENTE_HOMOLOGACAO,
+      CARD_HOMOLOGADOS,
+      CARD_NAO_HOMOLOGADOS
+    ];
   }
-
-  return cards;
+  return [
+    CARD_RECLAMACAO_DE_PRODUTO,
+    CARD_PRODUTOS_SUSPENSOS,
+    CARD_NAO_HOMOLOGADOS,
+    CARD_HOMOLOGADOS
+  ];
 };
