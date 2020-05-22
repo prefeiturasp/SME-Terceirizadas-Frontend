379fb4c53e88513622ebd17c670bbf777a7361f0
Pedro Walter <pedrohardware@gmail.com>
2020-05-22 13:13:53 -0300

    Criando pagina e rota

---
+++ src/components/screens/Produto/Reclamacao/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ReclamacaoProduto extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Reclamação de produto</h1>
+      </div>
+    );
+  }
+}
