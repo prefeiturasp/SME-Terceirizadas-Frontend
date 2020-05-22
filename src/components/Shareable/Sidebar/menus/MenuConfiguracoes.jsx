42fb3bd68ed0d6092e5930118c454b351c0b5550
Pedro Walter <pedrohardware@gmail.com>
2020-05-22 15:20:23 -0300

    Refatorando sidebar, incluindo link pra nova pagina

--- src/components/Shareable/Sidebar/menus/MenuConfiguracoes.jsx
+++ src/components/Shareable/Sidebar/menus/MenuConfiguracoes.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Menu, LeafItem } from "./shared";
 import { CONFIGURACOES, PERMISSOES, MENSAGEM } from "configs/constants";
-import { PERFIL } from "constants/shared";
+import {
+  usuarioEhCODAEGestaoAlimentacao,
+  usuarioEhCODAEDietaEspecial,
+  usuarioEhEscola,
+  usuarioEhTerceirizada
+} from "helpers/utilities";
 
 const MenuConfiguracoes = () => {
-  const exibirPermissoes = [
-    PERFIL.COORDENADOR_GESTAO_ALIMENTACAO_TERCEIRIZADA,
-    PERFIL.COORDENADOR_DIETA_ESPECIAL,
-    PERFIL.ADMINISTRADOR_ESCOLA,
-    PERFIL.NUTRI_ADMIN_RESPONSAVEL // NOTE: terceirizada@admin.com
-  ].includes(localStorage.getItem("perfil"));
-
-  const exibirConfigEmail = [
-    PERFIL.COORDENADOR_GESTAO_ALIMENTACAO_TERCEIRIZADA,
-    PERFIL.COORDENADOR_DIETA_ESPECIAL,
-    PERFIL.ADMINISTRADOR_GESTAO_ALIMENTACAO_TERCEIRIZADA
-  ];
+  const exibirPermissoes =
+    usuarioEhEscola() ||
+    usuarioEhCODAEGestaoAlimentacao() ||
+    usuarioEhCODAEDietaEspecial();
+  const exibirConfigEmail =
+    usuarioEhCODAEGestaoAlimentacao() ||
+    usuarioEhCODAEDietaEspecial() ||
+    usuarioEhTerceirizada();
 
   return (
     <Menu id="Configuracoes" icon="fa-cog" title={"Configurações"}>
