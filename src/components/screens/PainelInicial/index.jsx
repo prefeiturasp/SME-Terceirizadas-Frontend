df22f991675b957716d546dfb3aa5625b6c6d4f0
J. Araujo <21344963+jay-jlm@users.noreply.github.com>
2020-06-12 14:53:41 -0300

    correcao do acesso ao painel inicial

--- src/components/screens/PainelInicial/index.jsx
+++ src/components/screens/PainelInicial/index.jsx
@@ -5,39 +5,51 @@ import CardLogo from "../../Shareable/CardLogo/CardLogo";
 import IconeGestaoDeAlimentacao from "../../Shareable/Icones/IconeGestaoDeAlimentacao";
 import IconeGestaoDeProduto from "../../Shareable/Icones/IconeGestaoDeProduto";
 import IconeDietaEspecial from "../../Shareable/Icones/IconeDietaEspecial";
-import { usuarioEhEscola, usuarioEhTerceirizada } from "helpers/utilities";
+import {
+  usuarioEhEscola,
+  usuarioEhTerceirizada,
+  usuarioEhQualquerCODAE,
+  usuarioEhCODAEGestaoAlimentacao,
+  usuarioEhDRE
+} from "helpers/utilities";
 
 const PainelInicial = ({ history }) => {
   return (
     <Row className="mt-3" gutter={[16, 16]}>
-      {!!(usuarioEhEscola() || usuarioEhTerceirizada()) && (
-        <>
-          <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-            <CardLogo
-              titulo={"Gestão de Alimentação"}
-              onClick={() => history.push("/painel-gestao-alimentacao")}
-            >
-              <IconeGestaoDeAlimentacao />
-            </CardLogo>
-          </Col>
-          <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-            <CardLogo
-              titulo={"Dieta Especial"}
-              onClick={() => history.push("/painel-dieta-especial")}
-            >
-              <IconeDietaEspecial />
-            </CardLogo>
-          </Col>
-        </>
-      )}
       <Col xs={24} sm={24} md={24} lg={8} xl={8}>
         <CardLogo
-          titulo={"Gestão de Produto"}
-          onClick={() => history.push("/painel-gestao-produto")}
+          titulo={"Gestão de Alimentação"}
+          onClick={() => history.push("/painel-gestao-alimentacao")}
         >
-          <IconeGestaoDeProduto />
+          <IconeGestaoDeAlimentacao />
         </CardLogo>
       </Col>
+      {(usuarioEhCODAEGestaoAlimentacao() ||
+        usuarioEhTerceirizada() ||
+        usuarioEhDRE() ||
+        usuarioEhEscola()) && (
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <CardLogo
+            titulo={"Dieta Especial"}
+            onClick={() => history.push("/painel-dieta-especial")}
+          >
+            <IconeDietaEspecial />
+          </CardLogo>
+        </Col>
+      )}
+
+      {(usuarioEhQualquerCODAE() ||
+        usuarioEhTerceirizada() ||
+        usuarioEhEscola()) && (
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <CardLogo
+            titulo={"Gestão de Produto"}
+            onClick={() => history.push("/painel-gestao-produto")}
+          >
+            <IconeGestaoDeProduto />
+          </CardLogo>
+        </Col>
+      )}
     </Row>
   );
 };
