b86d2a1dfe64cd955b97b9dd0ae40e7370fcd0ba
Calvin Rossinhole <calvin.masters@gmail.com>
2020-06-19 10:16:31 -0300

    exibe dados do produto ao clicar ver produto

---
+++ src/components/screens/Produto/AvaliarReclamacaoProduto/TabelaProdutos/components/DadosReclamacao/index.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+export const DadosReclamacaoProduto = ({ produto }) => {
+  return (
+    <div className="produto-exibir">
+      <div className="row">
+        <div className="col-5 report-label-value">
+          <p>Empresa solicitante (Terceirizada)</p>
+          <p className="value">
+            {produto.ultima_homologacao.rastro_terceirizada.nome_fantasia}
+          </p>
+        </div>
+        <div className="col-3 report-label-value">
+          <p>Telefone</p>
+          <p className="value">
+            {
+              produto.ultima_homologacao.rastro_terceirizada.contatos[0]
+                .telefone
+            }
+          </p>
+        </div>
+        <div className="col-4 report-label-value">
+          <p>E-mail</p>
+          <p className="value">
+            {produto.ultima_homologacao.rastro_terceirizada.contatos[0].email}
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-5 report-label-value">
+          <p>Nome da Escola</p>
+          <p className="value">
+            {produto.ultima_homologacao.reclamacoes[0].vinculo.instituicao.nome}
+          </p>
+        </div>
+        <div className="col-7 report-label-value">
+          <p>Código EOL</p>
+          <p className="value">
+            {
+              produto.ultima_homologacao.reclamacoes[0].vinculo.instituicao
+                .codigo_eol
+            }
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-5 report-label-value">
+          <p>Nome do reclamante</p>
+          <p className="value">
+            {produto.ultima_homologacao.reclamacoes[0].reclamante_nome}
+          </p>
+        </div>
+        <div className="col-3 report-label-value">
+          <p>RF/CRN/CFN</p>
+          <p className="value">
+            {
+              produto.ultima_homologacao.reclamacoes[0]
+                .reclamante_registro_funcional
+            }
+          </p>
+        </div>
+        <div className="col-4 report-label-value">
+          <p>Cargo</p>
+          <p className="value">
+            {produto.ultima_homologacao.reclamacoes[0].reclamante_cargo}
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 report-label-value">
+          <p>Reclamação</p>
+          <p
+            className="value"
+            dangerouslySetInnerHTML={{
+              __html: produto.ultima_homologacao.reclamacoes[0].reclamacao
+            }}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 report-label-value">
+          <p>Anexos</p>
+          {produto.ultima_homologacao.reclamacoes[0].anexos.map(
+            (anexo, key) => {
+              return (
+                <div key={key}>
+                  <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={anexo.arquivo}
+                    className="link"
+                  >
+                    {`Anexo ${key + 1}`}
+                  </a>
+                </div>
+              );
+            }
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/screens/Produto/AvaliarReclamacaoProduto/TabelaProdutos/index.jsx
+++ src/components/screens/Produto/AvaliarReclamacaoProduto/TabelaProdutos/index.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import "./style.scss";
 import { NOME_STATUS } from "./helpers";
 import Botao from "components/Shareable/Botao";
-import { BUTTON_STYLE } from "components/Shareable/Botao/constants";
+import {
+  BUTTON_STYLE,
+  BUTTON_ICON
+} from "components/Shareable/Botao/constants";
+import { DadosReclamacaoProduto } from "./components/DadosReclamacao";
+import { DadosProduto } from "./components/DadosProduto";
 
 export const TabelaProdutos = ({ produtos, exibirDadosProduto }) => {
+  const [verProduto, setVerProduto] = useState(false);
+
   return (
     <div className="tabela-lista-produtos">
       {produtos && (
@@ -43,122 +50,19 @@ export const TabelaProdutos = ({ produtos, exibirDadosProduto }) => {
                   </td>
                 </tr>,
                 produto.exibir && (
-                  <div className="produto-exibir">
-                    <div className="row">
-                      <div className="col-5 report-label-value">
-                        <p>Empresa solicitante (Terceirizada)</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.rastro_terceirizada
-                              .nome_fantasia
-                          }
-                        </p>
-                      </div>
-                      <div className="col-3 report-label-value">
-                        <p>Telefone</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.rastro_terceirizada
-                              .contatos[0].telefone
-                          }
-                        </p>
-                      </div>
-                      <div className="col-4 report-label-value">
-                        <p>E-mail</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.rastro_terceirizada
-                              .contatos[0].email
-                          }
-                        </p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-5 report-label-value">
-                        <p>Nome da Escola</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.reclamacoes[0].vinculo
-                              .instituicao.nome
-                          }
-                        </p>
-                      </div>
-                      <div className="col-7 report-label-value">
-                        <p>Código EOL</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.reclamacoes[0].vinculo
-                              .instituicao.codigo_eol
-                          }
-                        </p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-5 report-label-value">
-                        <p>Nome do reclamante</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.reclamacoes[0]
-                              .reclamante_nome
-                          }
-                        </p>
-                      </div>
-                      <div className="col-3 report-label-value">
-                        <p>RF/CRN/CFN</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.reclamacoes[0]
-                              .reclamante_registro_funcional
-                          }
-                        </p>
-                      </div>
-                      <div className="col-4 report-label-value">
-                        <p>Cargo</p>
-                        <p className="value">
-                          {
-                            produto.ultima_homologacao.reclamacoes[0]
-                              .reclamante_cargo
-                          }
-                        </p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-12 report-label-value">
-                        <p>Reclamação</p>
-                        <p
-                          className="value"
-                          dangerouslySetInnerHTML={{
-                            __html:
-                              produto.ultima_homologacao.reclamacoes[0]
-                                .reclamacao
-                          }}
-                        />
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-12 report-label-value">
-                        <p>Anexos</p>
-                        {produto.ultima_homologacao.reclamacoes[0].anexos.map(
-                          (anexo, key) => {
-                            return (
-                              <div key={key}>
-                                <a
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                                  href={anexo.arquivo}
-                                  className="link"
-                                >
-                                  {`Anexo ${key + 1}`}
-                                </a>
-                              </div>
-                            );
-                          }
-                        )}
-                      </div>
-                    </div>
+                  <Fragment>
+                    <DadosReclamacaoProduto produto={produto} />
+                    {verProduto && <DadosProduto produto={produto} />}
                     <div className="row mb-3">
                       <div className="col-12 text-right">
-                        <Botao texto="Ver produto" style={BUTTON_STYLE.GREEN} />
+                        <Botao
+                          onClick={() => setVerProduto(!verProduto)}
+                          texto={verProduto ? "Voltar" : "Ver produto"}
+                          style={
+                            verProduto ? BUTTON_STYLE.BLUE : BUTTON_STYLE.GREEN
+                          }
+                          icon={verProduto && BUTTON_ICON.ARROW_LEFT}
+                        />
                         <Botao
                           className="ml-3"
                           texto="Questionar terceirizada"
@@ -176,7 +80,7 @@ export const TabelaProdutos = ({ produtos, exibirDadosProduto }) => {
                         />
                       </div>
                     </div>
-                  </div>
+                  </Fragment>
                 )
               ];
             })}
