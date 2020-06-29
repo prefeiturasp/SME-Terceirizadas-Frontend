35b0cb6289829b0003a9644aededd1c421d27489
J. Araujo <21344963+jay-jlm@users.noreply.github.com>
2020-06-29 11:09:34 -0300

    corrige mensagem anexo ao ativar/suspender um produto

--- src/components/screens/Produto/AtivacaoSuspensao/ModalAtivacaoSuspensaoProduto/index.jsx
+++ src/components/screens/Produto/AtivacaoSuspensao/ModalAtivacaoSuspensaoProduto/index.jsx
@@ -151,6 +151,7 @@ export default class ModalAtivacaoSuspensaoProduto extends Component {
                       name="anexos"
                       accept=".png, .doc, .pdf, .docx, .jpeg, .jpg"
                       icone={BUTTON_ICON.ATTACH}
+                      toastSuccessMessage="Anexo incluso com sucesso"
                       concatenarNovosArquivos
                     />
                   </div>
