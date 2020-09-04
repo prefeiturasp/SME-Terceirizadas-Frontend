24cd5c472245641b94af3ee6f84a912c8f8ca479
rodolfo <rodolfo@lemeconsultoria.com.br>
2020-09-04 16:59:21 -0300

    Lista produtos analise reclamação terceirizada

--- src/pages/Produto/helpers.js
+++ src/pages/Produto/helpers.js
@@ -1,13 +1,9 @@
 import { ENDPOINT_HOMOLOGACOES_PRODUTO_STATUS } from "../../constants/shared";
-import { usuarioEhTerceirizada } from "helpers/utilities";
 
 export const escolheStatusPendenteHomologacao = () => {
   return ENDPOINT_HOMOLOGACOES_PRODUTO_STATUS.CODAE_PENDENTE_HOMOLOGACAO;
 };
 
 export const escolheStatusAguardandoAnaliseReclamacao = () => {
-  if (usuarioEhTerceirizada()) {
-    return ENDPOINT_HOMOLOGACOES_PRODUTO_STATUS.CODAE_QUESTIONADO;
-  }
   return ENDPOINT_HOMOLOGACOES_PRODUTO_STATUS.CODAE_PEDIU_ANALISE_RECLAMACAO;
 };
