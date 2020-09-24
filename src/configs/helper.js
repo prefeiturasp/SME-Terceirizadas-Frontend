aa4e4d4409e3efc4561f6e6662b38fd7e34cb0d8
Pedro Walter <pedrohardware@gmail.com>
2020-09-24 10:37:37 -0300

    Usa a tela correta para o perfil CODAE

--- src/configs/helper.js
+++ src/configs/helper.js
@@ -158,6 +158,7 @@ export const StatusSolicitacoesDietaEspecial = () => {
     case TIPO_PERFIL.DIRETORIA_REGIONAL:
       return StatusSolicitacoesDietaEspecialPage.SolicitacoesDietaEspecialDRE;
     case TIPO_PERFIL.DIETA_ESPECIAL:
+    case TIPO_PERFIL.GESTAO_ALIMENTACAO_TERCEIRIZADA:
       return StatusSolicitacoesDietaEspecialPage.SolicitacoesDietaEspecialCODAE;
     case TIPO_PERFIL.TERCEIRIZADA:
       return StatusSolicitacoesDietaEspecialPage.SolicitacoesDietaEspecialTerceirizada;
