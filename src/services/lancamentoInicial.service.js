265cf774d6ef67d44637eed34ae2a73a2c66f17e
Pedro Walter <pedrohardware@gmail.com>
2020-10-19 09:11:38 -0300

    Finaliza tela

--- src/services/lancamentoInicial.service.js
+++ src/services/lancamentoInicial.service.js
@@ -1,4 +1,7 @@
 import axios from "./_base";
 
 export const registraLancamentoDiario = async payload =>
-  await axios.post(`/lancamento-diario/`, payload);
+  await axios.post("/lancamento-diario/", payload);
+
+export const getLancamentosPorMes = async payload =>
+  await axios.get("/lancamento-diario/por-mes/", { params: payload });
