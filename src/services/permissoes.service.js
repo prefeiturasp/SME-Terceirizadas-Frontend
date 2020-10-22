bcb21c0837fad85554f5e4bd15c923b0cf445089
Pedro Walter <pedrohardware@gmail.com>
2020-10-22 13:16:12 -0300

    Exibe erro corretamente quando RF não é encontrado no EOL

--- src/services/permissoes.service.js
+++ src/services/permissoes.service.js
@@ -1,29 +1,15 @@
 import { API_URL } from "../constants/config";
 import authService from "./auth";
 
+import axios from "./_base";
+
 const authToken = {
   Authorization: `JWT ${authService.getToken()}`,
   "Content-Type": "application/json"
 };
 
-export const getDadosUsuarioEOL = registroFuncional => {
-  const url = `${API_URL}/dados-usuario-eol/${registroFuncional}/`;
-  let status = 0;
-  return fetch(url, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" }
-  })
-    .then(res => {
-      status = res.status;
-      return res.json();
-    })
-    .then(data => {
-      return { data: data, status: status };
-    })
-    .catch(error => {
-      return error.json();
-    });
-};
+export const getDadosUsuarioEOL = async registroFuncional =>
+  axios.get(`/dados-usuario-eol/${registroFuncional}/`);
 
 export const criarEquipeAdministradoraEscola = (uuid, registroFuncional) => {
   const url = `${API_URL}/vinculos-escolas/${uuid}/criar_equipe_administradora/`;
