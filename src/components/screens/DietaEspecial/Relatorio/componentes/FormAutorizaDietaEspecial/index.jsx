12b30b27c51c6d3806bf7b3c422cccb90697f546
rodolfo <rodolfo@lemeconsultoria.com.br>
2020-11-11 11:23:01 -0300

    Adapta visoes para diferentes tipos de dieta especial

--- src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/index.jsx
+++ src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/index.jsx
@@ -3,6 +3,7 @@ import moment from "moment";
 import React, { Component } from "react";
 import { Form, Field } from "react-final-form";
 import arrayMutators from "final-form-arrays";
+import { InputComData } from "components/Shareable/DatePicker";
 
 import {
   atualizaDietaEspecial,
@@ -15,6 +16,8 @@ import {
 } from "../../../../../../services/dietaEspecial.service";
 import { getNomeProdutosHomologados } from "services/produto.service";
 import { SelectWithHideOptions } from "components/Shareable/SelectWithHideOptions";
+
+import { TIPO_SOLICITACAO_DIETA } from "../../../../../../constants/shared";
 
 import { required } from "../../../../../../helpers/fieldValidators";
 import {
@@ -273,135 +276,163 @@ export default class FormAutorizaDietaEspecial extends Component {
           keepDirtyOnReinitialize={true}
           render={({ form, handleSubmit, pristine, submitting, values }) => (
             <form onSubmit={handleSubmit}>
-              <div className="information-codae">
-                <div className="pt-2 title">Relação por Diagnóstico</div>
-                {diagnosticos && (
-                  <Field
-                    component={DiagnosticosField}
-                    name="alergias_intolerancias"
-                    diagnosticos={diagnosticos}
-                    validate={required}
-                  />
-                )}
-                <div className="pt-2 title">Classificação da Dieta</div>
-                {classificacoesDieta && (
-                  <Field
-                    component={ClassificacaoDaDieta}
-                    name="classificacao"
-                    onClassificacaoChanged={this.onClassificacaoChanged}
-                    classificacoes={classificacoesDieta}
-                    validate={required}
-                  />
-                )}
-                <div className="card mt-3">
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-12">
-                        <div className="row">
-                          <div className="col-12">
-                            <div className="title">
-                              Nome do Protocolo de Dieta Especial
+              {dietaEspecial.tipo_solicitacao !==
+                TIPO_SOLICITACAO_DIETA.ALTERACAO_UE && (
+                <div className="information-codae">
+                  <div className="pt-2 title">Relação por Diagnóstico</div>
+                  {diagnosticos && (
+                    <Field
+                      component={DiagnosticosField}
+                      name="alergias_intolerancias"
+                      diagnosticos={diagnosticos}
+                      validate={required}
+                    />
+                  )}
+                  <div className="pt-2 title">Classificação da Dieta</div>
+                  {classificacoesDieta && (
+                    <Field
+                      component={ClassificacaoDaDieta}
+                      name="classificacao"
+                      onClassificacaoChanged={this.onClassificacaoChanged}
+                      classificacoes={classificacoesDieta}
+                      validate={required}
+                    />
+                  )}
+                  <div className="card mt-3">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-12">
+                          <div className="row">
+                            <div className="col-12">
+                              <div className="title">
+                                Nome do Protocolo de Dieta Especial
+                              </div>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-10">
+                              {protocolosDietaEspecial && (
+                                <Field
+                                  component={SelectWithHideOptions}
+                                  options={protocolosDietaEspecial}
+                                  name="nome_protocolo"
+                                  selectedItems={[]}
+                                />
+                              )}
+                            </div>
+                            <div className="col-2 text-center">
+                              <Botao
+                                texto="Adicionar"
+                                type={BUTTON_TYPE.BUTTON}
+                                style={BUTTON_STYLE.BLUE_OUTLINE}
+                                onClick={() =>
+                                  this.showModalAdicionaProtocolo()
+                                }
+                                disabled={submitting}
+                              />
                             </div>
                           </div>
                         </div>
-                        <div className="row">
-                          <div className="col-10">
-                            {protocolosDietaEspecial && (
-                              <Field
-                                component={SelectWithHideOptions}
-                                options={protocolosDietaEspecial}
-                                name="nome_protocolo"
-                                selectedItems={[]}
-                              />
-                            )}
-                          </div>
-                          <div className="col-2 text-center">
-                            <Botao
-                              texto="Adicionar"
-                              type={BUTTON_TYPE.BUTTON}
-                              style={BUTTON_STYLE.BLUE_OUTLINE}
-                              onClick={() => this.showModalAdicionaProtocolo()}
-                              disabled={submitting}
+                      </div>
+                      <div className="pt-2 title">
+                        Substituições de Alimentos
+                      </div>
+                      <SubstituicoesField
+                        alimentos={alimentos}
+                        produtos={produtos}
+                      />
+                      <div className="row">
+                        {dietaEspecial.tipo_solicitacao ===
+                        TIPO_SOLICITACAO_DIETA.ALUNO_NAO_MATRICULADO ? (
+                          <div className="col-3">
+                            <Field
+                              component={InputComData}
+                              label="Data de Término"
+                              name="data_termino"
+                              required
+                              className="form-control"
+                              minDate={moment().add(1, "days")._d}
+                              validate={required}
                             />
                           </div>
+                        ) : (
+                          <div className="col-12">
+                            <Field
+                              component={DataOpcional}
+                              label="Data de Término"
+                              labelLigado="Com data de término"
+                              labelDesligado="Sem data de término"
+                              minDate={moment().add(1, "day")["_d"]}
+                              name="data_termino"
+                              format={v => v && moment(v, "YYYY-MM-DD")["_d"]}
+                              parse={v => v && moment(v).format("YYYY-MM-DD")}
+                            />
+                          </div>
+                        )}
+                      </div>
+                      <div className="row">
+                        <div className="col-9">
+                          <Field
+                            component={CKEditorField}
+                            label="Informações Adicionais"
+                            name="informacoes_adicionais"
+                          />
                         </div>
                       </div>
-                    </div>
-                    <div className="pt-2 title">Substituições de Alimentos</div>
-                    <SubstituicoesField
-                      alimentos={alimentos}
-                      produtos={produtos}
-                    />
-                    <div className="row">
-                      <div className="col-12">
-                        <Field
-                          component={DataOpcional}
-                          label="Data de Término"
-                          labelLigado="Com data de término"
-                          labelDesligado="Sem data de término"
-                          minDate={moment().add(1, "day")["_d"]}
-                          name="data_termino"
-                          format={v => v && moment(v, "YYYY-MM-DD")["_d"]}
-                          parse={v => v && moment(v).format("YYYY-MM-DD")}
-                        />
+                      <div className="pt-2 title">
+                        Identificação do Nutricionista
                       </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-9">
-                        <Field
-                          component={CKEditorField}
-                          label="Informações Adicionais"
-                          name="informacoes_adicionais"
-                        />
-                      </div>
-                    </div>
-                    <div className="pt-2 title">
-                      Identificação do Nutricionista
-                    </div>
-                    <div className="row">
-                      <div className="col-9">
-                        <Field
-                          component={InputText}
-                          name="registro_funcional_nutricionista"
-                          disabled
-                        />
+                      <div className="row">
+                        <div className="col-9">
+                          <Field
+                            component={InputText}
+                            name="registro_funcional_nutricionista"
+                            disabled
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="form-group row float-right mt-4">
-                  <Botao
-                    texto="Solicitar Novo Produto"
-                    type={BUTTON_TYPE.BUTTON}
-                    style={BUTTON_STYLE.BLUE_OUTLINE}
-                    onClick={() => this.showModalSolicitacaoCadastroProduto()}
-                    className="ml-3"
-                    disabled={submitting}
-                  />
-                  <Botao
-                    texto="Salvar Rascunho"
-                    type={BUTTON_TYPE.BUTTON}
-                    style={BUTTON_STYLE.BLUE}
-                    onClick={() => this.salvaRascunho(values)}
-                    className="ml-3"
-                    disabled={pristine || submitting}
-                  />
-                  <Botao
-                    texto="Negar"
-                    type={BUTTON_TYPE.BUTTON}
-                    style={BUTTON_STYLE.GREEN_OUTLINE}
-                    onClick={() => this.showNaoAprovaModal("Não")}
-                    className="ml-3"
-                    disabled={submitting}
-                  />
-                  <Botao
-                    texto="Autorizar"
-                    type={BUTTON_TYPE.SUBMIT}
-                    style={BUTTON_STYLE.GREEN}
-                    className="ml-3"
-                    disabled={submitting}
-                  />
-                </div>
+              )}
+
+              <div className="form-group row float-right mt-4">
+                {dietaEspecial.tipo_solicitacao !==
+                  TIPO_SOLICITACAO_DIETA.ALTERACAO_UE && (
+                  <>
+                    <Botao
+                      texto="Solicitar Novo Produto"
+                      type={BUTTON_TYPE.BUTTON}
+                      style={BUTTON_STYLE.BLUE_OUTLINE}
+                      onClick={() => this.showModalSolicitacaoCadastroProduto()}
+                      className="ml-3"
+                      disabled={submitting}
+                    />
+                    <Botao
+                      texto="Salvar Rascunho"
+                      type={BUTTON_TYPE.BUTTON}
+                      style={BUTTON_STYLE.BLUE}
+                      onClick={() => this.salvaRascunho(values)}
+                      className="ml-3"
+                      disabled={pristine || submitting}
+                    />
+                  </>
+                )}
+                <Botao
+                  texto="Negar"
+                  type={BUTTON_TYPE.BUTTON}
+                  style={BUTTON_STYLE.GREEN_OUTLINE}
+                  onClick={() => this.showNaoAprovaModal("Não")}
+                  className="ml-3"
+                  disabled={submitting}
+                />
+                <Botao
+                  texto="Autorizar"
+                  type={BUTTON_TYPE.SUBMIT}
+                  style={BUTTON_STYLE.GREEN}
+                  className="ml-3"
+                  disabled={submitting}
+                />
               </div>
               <ModalAutorizaDietaEspecial
                 closeModal={this.closeAutorizarModal}
