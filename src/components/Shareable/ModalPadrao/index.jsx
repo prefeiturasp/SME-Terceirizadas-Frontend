9922e11e2dc79472513fae12d9cc5124011e7860
rg3915 <regis42santos@gmail.com>
2021-02-09 20:30:09 -0300

    feature: [P&D]  Visualizar todo o texto digitado no modal de justificativa do processo de solicitação de correção. feature 33617-33688

--- src/components/Shareable/ModalPadrao/index.jsx
+++ src/components/Shareable/ModalPadrao/index.jsx
@@ -77,7 +77,7 @@ export class ModalPadrao extends Component {
                 </div>
               </Modal.Body>
               <Modal.Footer>
-                <div className="row mt-5">
+                <div className="row">
                   <div className="col-12">
                     <Botao
                       texto="Cancelar"
