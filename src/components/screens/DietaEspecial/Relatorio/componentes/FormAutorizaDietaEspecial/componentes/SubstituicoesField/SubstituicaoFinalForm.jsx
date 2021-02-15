1a4374630d8fee43febeaf159c6154c2de8f0ee2
rg3915 <regis42santos@gmail.com>
2021-02-15 18:24:48 -0300

    feature: [Dieta Especial] Na Autorização da Dieta Especial criar busca no campo Alimento. feature 32221-32606

--- src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/SubstituicoesField/SubstituicaoFinalForm.jsx
+++ src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/SubstituicoesField/SubstituicaoFinalForm.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import MultiSelect from "./MultiSelect";
 import { Field } from "react-final-form";
-import { OnChange } from "react-final-form-listeners";
 import { required } from "helpers/fieldValidators";
 import Select from "../../../../../../../Shareable/Select";
+import { Icon, Select as SelectAntd } from "antd";
 import Botao from "../../../../../../../Shareable/Botao";
+import { ASelect } from "components/Shareable/MakeField";
 import {
   BUTTON_STYLE,
   BUTTON_ICON
 } from "../../../../../../../Shareable/Botao/constants";
 
 import "./style.scss";
+
+const { Option } = SelectAntd;
 
 const SelectSelecione = props => {
   const {
@@ -47,25 +50,23 @@ export default class SubstituicoesField extends Component {
 
     return (
       <div className="row">
-        <div className="col-3">
+        <div className="col-3 select-produto">
           <Field
-            component={SelectSelecione}
-            options={alimentos.map(a => {
-              return {
-                uuid: a.id.toString(),
-                nome: a.nome
-              };
-            })}
+            component={ASelect}
+            className={"select-form-produto"}
+            suffixIcon={<Icon type="caret-down" />}
+            showSearch
             name={`${name}.alimento`}
-            validate={required}
-          />
-          <OnChange name={`${name}.alimento`}>
-            {value => {
-              this.setState({
-                valorSelecionado: alimentos.find(al => String(al.id) === value)
-              });
-            }}
-          </OnChange>
+            filterOption={(input, option) =>
+              option.props.children
+                .toLowerCase()
+                .indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            {alimentos.map(a => {
+              return <Option key={a.id.toString()}>{a.nome}</Option>;
+            })}
+          </Field>
         </div>
         <div className="col-2">
           <Field
