c46c3ddcb5b6f8d11baee93160aeeca42ca0ede2
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-05-18 17:52:57 -0300

    modifica page  com multiplos Breadcrumb e parametro

--- src/pages/Logistica/ConferenciaDeGuiaComOcorrenciaPage.jsx
+++ src/pages/Logistica/ConferenciaDeGuiaComOcorrenciaPage.jsx
@@ -1,37 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HOME } from "constants/config";
 import Breadcrumb from "components/Shareable/Breadcrumb";
 import Page from "components/Shareable/Page/Page";
 import {
   CONFERIR_ENTREGA,
+  CONFERENCIA_GUIA,
   CONFERENCIA_GUIA_COM_OCORRENCIA,
   LOGISTICA
 } from "configs/constants";
 import ConferenciaDeGuiaComOcorrencia from "components/screens/Logistica/ConferenciaDeGuiaComOcorrencia";
 
-const atual = {
-  href: `/${LOGISTICA}/${CONFERENCIA_GUIA_COM_OCORRENCIA}`,
-  titulo: "Conferência da Guia de Remessa"
+export default () => {
+  const [uuid, setUuid] = useState();
+  const atual = {
+    href: `/${LOGISTICA}${CONFERENCIA_GUIA_COM_OCORRENCIA}`,
+    titulo: "Conferência individual de itens"
+  };
+
+  const anteriores = [
+    {
+      href: `/`,
+      titulo: "Abastecimento"
+    },
+    {
+      href: `/${LOGISTICA}/${CONFERIR_ENTREGA}`,
+      titulo: "Conferir Entrega"
+    },
+    {
+      href: `/${LOGISTICA}/${CONFERENCIA_GUIA}?uuid=${uuid}`,
+      titulo: "Conferência da guia"
+    }
+  ];
+
+  useEffect(() => {
+    const queryString = window.location.search;
+
+    if (queryString) {
+      const urlParams = new URLSearchParams(window.location.search);
+      const param = urlParams.get("uuid");
+      setUuid(param);
+    }
+  }, []);
+
+  return (
+    <Page
+      botaoVoltar
+      voltarPara={`/${LOGISTICA}/${CONFERIR_ENTREGA}`}
+      titulo={atual.titulo}
+    >
+      <Breadcrumb home={HOME} atual={atual} anteriores={anteriores} />
+      <ConferenciaDeGuiaComOcorrencia />
+    </Page>
+  );
 };
-
-const anteriores = [
-  {
-    href: `/`,
-    titulo: "Abastecimento"
-  },
-  {
-    href: `/${LOGISTICA}/${CONFERIR_ENTREGA}`,
-    titulo: "Conferir Entrega"
-  }
-];
-
-export default () => (
-  <Page
-    botaoVoltar
-    voltarPara={`/${LOGISTICA}/${CONFERIR_ENTREGA}`}
-    titulo={atual.titulo}
-  >
-    <Breadcrumb home={HOME} atual={atual} anteriores={anteriores} />
-    <ConferenciaDeGuiaComOcorrencia />
-  </Page>
-);
