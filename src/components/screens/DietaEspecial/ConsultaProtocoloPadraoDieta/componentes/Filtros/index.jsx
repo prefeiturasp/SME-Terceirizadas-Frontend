467a8ae4d29d47653c6328996972f63728e87ab9
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-06-14 16:06:02 -0300

    Feature: novos componentes para consulta de protocolo padrão

---
+++ src/components/screens/DietaEspecial/ConsultaProtocoloPadraoDieta/componentes/Filtros/index.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { Form, Field } from "react-final-form";
+import AutoCompleteField from "components/Shareable/AutoCompleteField";
+import Botao from "components/Shareable/Botao";
+import { useHistory } from "react-router-dom";
+import { DIETA_ESPECIAL, PROTOCOLO_PADRAO_DIETA } from "configs/constants";
+import {
+  BUTTON_STYLE,
+  BUTTON_TYPE
+} from "components/Shareable/Botao/constants";
+import { consultaProtocoloPadrao } from "services/dietaEspecial.service";
+import HTTP_STATUS from "http-status-codes";
+import { toastError } from "components/Shareable/Toast/dialogs";
+
+export default ({
+  setResultado,
+  nomes,
+  status,
+  setCarregando,
+  setTotal,
+  setFiltros,
+  setPage
+}) => {
+  const history = useHistory();
+
+  const getNomesProtocolosFiltrado = nomeProtocolo => {
+    if (nomeProtocolo) {
+      const reg = new RegExp(nomeProtocolo, "i");
+      return nomes.filter(a => reg.test(a));
+    }
+    return [];
+  };
+
+  const getStatusProtocolosFiltrado = statusProtocolo => {
+    if (statusProtocolo) {
+      const reg = new RegExp(statusProtocolo, "i");
+      return status.filter(a => reg.test(a));
+    }
+    return [];
+  };
+
+  const onSubmit = async formValues => {
+    try {
+      setCarregando(true);
+      const response = await consultaProtocoloPadrao(formValues);
+      if (response.status === HTTP_STATUS.OK) {
+        setResultado(response.data);
+        setTotal(response.data.count);
+        setFiltros(formValues);
+        setCarregando(false);
+      }
+    } catch (e) {
+      toastError("Houve um erro ao tentar filtrar os Protocolos");
+    }
+  };
+
+  return (
+    <Form
+      onSubmit={onSubmit}
+      initialValues={{}}
+      render={({ submitting, form, handleSubmit, values }) => (
+        <form onSubmit={handleSubmit}>
+          <div className="row mt-3 mb-3">
+            <div className="col-6">
+              <Field
+                label="Nome do Protocolo Padrão"
+                component={AutoCompleteField}
+                dataSource={getNomesProtocolosFiltrado(values.nome_protocolo)}
+                name="nome_protocolo"
+                placeholder="Insira o Nome do Protocolo Padrão"
+                className="input-busca-nome-protocolo"
+              />
+            </div>
+            <div className="col-6">
+              <Field
+                label="Nome do Protocolo Padrão"
+                component={AutoCompleteField}
+                dataSource={getStatusProtocolosFiltrado(values.status)}
+                name="status"
+                placeholder="Insira o Status do Protocolo Padrão"
+                className="input-busca-status-protocolo"
+              />
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-8">
+              <Botao
+                texto="Criar Protocolo Padrão"
+                type={BUTTON_TYPE.BUTTON}
+                style={BUTTON_STYLE.GREEN}
+                onClick={() =>
+                  history.push(`/${DIETA_ESPECIAL}/${PROTOCOLO_PADRAO_DIETA}`)
+                }
+              />
+            </div>
+            <div className="col-4">
+              <Botao
+                texto="Consultar"
+                type={BUTTON_TYPE.SUBMIT}
+                style={BUTTON_STYLE.GREEN}
+                className="float-right ml-3"
+                disabled={submitting}
+              />
+              <Botao
+                texto="Limpar campos"
+                type={BUTTON_TYPE.BUTTON}
+                style={BUTTON_STYLE.GREEN_OUTLINE}
+                className="float-right ml-3"
+                onClick={() => {
+                  form.reset({});
+                  setResultado(undefined);
+                  setPage(1);
+                }}
+              />
+            </div>
+          </div>
+        </form>
+      )}
+    />
+  );
+};
