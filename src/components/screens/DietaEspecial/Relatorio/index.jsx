f4a7c99aa5417c5ac8fee8ade6514db992131228
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-06-28 17:17:50 -0300

    Fix: botão cancelar dieta, botão anexo.

--- src/components/screens/DietaEspecial/Relatorio/index.jsx
+++ src/components/screens/DietaEspecial/Relatorio/index.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { getDietaEspecial } from "services/dietaEspecial.service";
 import { toastError } from "components/Shareable/Toast/dialogs";
 import HTTP_STATUS from "http-status-codes";
+import { ESCOLA } from "configs/constants";
+import { statusEnum } from "constants/shared";
 import "antd/dist/antd.css";
 import { cabecalhoDieta } from "./helpers";
 import CorpoRelatorio from "./componentes/CorpoRelatorio";
+import EscolaCancelaDietaEspecial from "./componentes/EscolaCancelaDietaEspecial";
 import { Spin } from "antd";
 import "./style.scss";
 
-const Relatorio = () => {
+const Relatorio = ({ visao }) => {
   const [dietaEspecial, setDietaEspecial] = useState(null);
   const [carregando, setCarregando] = useState(false);
 
@@ -45,6 +48,14 @@ const Relatorio = () => {
           {dietaEspecial && (
             <>
               <CorpoRelatorio dietaEspecial={dietaEspecial} />
+              {dietaEspecial.status_solicitacao ===
+                statusEnum.CODAE_A_AUTORIZAR &&
+                visao === ESCOLA && (
+                  <EscolaCancelaDietaEspecial
+                    uuid={dietaEspecial.uuid}
+                    onCancelar={() => loadSolicitacao(dietaEspecial.uuid)}
+                  />
+                )}
             </>
           )}
         </div>
