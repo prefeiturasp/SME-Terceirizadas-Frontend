b442b24beaabe154562839ea37e60a5e63b78906
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-07-04 12:27:51 -0300

    Feature: importar componente de formulário para autorizar ou negar dietas

--- src/components/screens/DietaEspecial/Relatorio/index.jsx
+++ src/components/screens/DietaEspecial/Relatorio/index.jsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { getDietaEspecial } from "services/dietaEspecial.service";
-import { toastError } from "components/Shareable/Toast/dialogs";
+import {
+  getDietaEspecial,
+  escolaCancelaSolicitacao
+} from "services/dietaEspecial.service";
+import { getProtocoloDietaEspecial } from "services/relatorios";
+import { toastSuccess, toastError } from "components/Shareable/Toast/dialogs";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_TYPE,
+  BUTTON_STYLE,
+  BUTTON_ICON
+} from "components/Shareable/Botao/constants";
 import HTTP_STATUS from "http-status-codes";
-import { ESCOLA } from "configs/constants";
+import { ESCOLA, CODAE } from "configs/constants";
 import { statusEnum } from "constants/shared";
 import "antd/dist/antd.css";
 import { cabecalhoDieta } from "./helpers";
 import CorpoRelatorio from "./componentes/CorpoRelatorio";
 import EscolaCancelaDietaEspecial from "./componentes/EscolaCancelaDietaEspecial";
+import FormAutorizaDietaEspecial from "./componentes/FormAutorizaDietaEspecial";
+import ModalNegaDietaEspecial from "./componentes/ModalNegaDietaEspecial";
 import { Spin } from "antd";
 import "./style.scss";
 
 const Relatorio = ({ visao }) => {
   const [dietaEspecial, setDietaEspecial] = useState(null);
   const [carregando, setCarregando] = useState(false);
+  const [showNaoAprovaModal, setShowNaoAprovaModal] = useState(false);
+  const [status, setStatus] = useState(undefined);
 
   const fetchData = uuid => {
     loadSolicitacao(uuid);
@@ -24,6 +38,7 @@ const Relatorio = ({ visao }) => {
     const responseDietaEspecial = await getDietaEspecial(uuid);
     if (responseDietaEspecial.status === HTTP_STATUS.OK) {
       setDietaEspecial(responseDietaEspecial.data);
+      setStatus(responseDietaEspecial.data.status_solicitacao);
       setCarregando(false);
     } else {
       toastError("Houve um erro ao carregar Solicitação");
@@ -38,28 +53,109 @@ const Relatorio = ({ visao }) => {
     }
   }, []);
 
+  const gerarProtocolo = async uuid => {
+    setCarregando(true);
+    await getProtocoloDietaEspecial(uuid);
+    setCarregando(false);
+  };
+
+  const BotaoAutorizaCancelamento = ({ uuid, onAutorizar, setCarregando }) => {
+    return (
+      <div className="form-group row float-right mt-4">
+        <Botao
+          texto="Autorizar"
+          type={BUTTON_TYPE.BUTTON}
+          style={BUTTON_STYLE.GREEN}
+          className="ml-3"
+          onClick={() => {
+            setCarregando(true);
+            escolaCancelaSolicitacao(uuid).then(() => {
+              onAutorizar();
+              toastSuccess(
+                "Autorização do Cancelamento realizada com sucesso!"
+              );
+            });
+          }}
+        />
+      </div>
+    );
+  };
+
+  const BotaoGerarRelatorio = ({ uuid }) => {
+    return (
+      <div className="row">
+        <div className="col-12">
+          <Botao
+            type={BUTTON_TYPE.BUTTON}
+            style={BUTTON_STYLE.GREEN}
+            icon={BUTTON_ICON.PRINT}
+            className="ml-3 float-right mt-4"
+            onClick={() => gerarProtocolo(uuid)}
+          />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Spin tip="Carregando..." spinning={carregando}>
-      {dietaEspecial && (
+      {dietaEspecial && status && (
         <span className="page-title">{cabecalhoDieta(dietaEspecial)}</span>
       )}
       <div className="card mt-3">
         <div className="card-body">
+          {dietaEspecial && status === statusEnum.CODAE_AUTORIZADO && (
+            <BotaoGerarRelatorio uuid={dietaEspecial.uuid} />
+          )}
           {dietaEspecial && (
             <>
               <CorpoRelatorio dietaEspecial={dietaEspecial} />
-              {dietaEspecial.status_solicitacao ===
-                statusEnum.CODAE_A_AUTORIZAR &&
-                visao === ESCOLA && (
-                  <EscolaCancelaDietaEspecial
-                    uuid={dietaEspecial.uuid}
-                    onCancelar={() => loadSolicitacao(dietaEspecial.uuid)}
-                  />
-                )}
+              {status === statusEnum.CODAE_A_AUTORIZAR && visao === ESCOLA && (
+                <EscolaCancelaDietaEspecial
+                  uuid={dietaEspecial.uuid}
+                  onCancelar={() => loadSolicitacao(dietaEspecial.uuid)}
+                />
+              )}
             </>
           )}
+          {dietaEspecial &&
+            status === statusEnum.CODAE_A_AUTORIZAR &&
+            visao === CODAE && (
+              <FormAutorizaDietaEspecial
+                dietaEspecial={dietaEspecial}
+                onAutorizarOuNegar={() => loadSolicitacao(dietaEspecial.uuid)}
+                setTemSolicitacaoCadastroProduto={() =>
+                  setDietaEspecial({
+                    ...dietaEspecial,
+                    tem_solicitacao_cadastro_produto: true
+                  })
+                }
+              />
+            )}
+          {dietaEspecial &&
+            status === statusEnum.ESCOLA_SOLICITOU_INATIVACAO &&
+            visao === CODAE && (
+              <BotaoAutorizaCancelamento
+                uuid={dietaEspecial.uuid}
+                showNaoAprovaModal={showNaoAprovaModal}
+                onAutorizar={() => {
+                  loadSolicitacao(dietaEspecial.uuid);
+                }}
+                setCarregando={setCarregando}
+              />
+            )}
         </div>
       </div>
+      {dietaEspecial && (
+        <ModalNegaDietaEspecial
+          showModal={showNaoAprovaModal}
+          closeModal={setShowNaoAprovaModal}
+          onNegar={() => {
+            loadSolicitacao(dietaEspecial.uuid);
+          }}
+          uuid={dietaEspecial.uuid}
+        />
+      )}
     </Spin>
   );
 };
