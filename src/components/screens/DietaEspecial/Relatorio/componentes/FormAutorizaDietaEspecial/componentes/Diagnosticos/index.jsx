f6ce2d0c698c6d28d2cd8e2e1cb1ba35d5089c9b
anderson89marques <andersonoanjo18@gmail.com>
2021-07-12 12:00:48 -0300

    Ajuste no componente de Diagnosticos na hora de selecionar os mesmos.

--- src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/Diagnosticos/index.jsx
+++ src/components/screens/DietaEspecial/Relatorio/componentes/FormAutorizaDietaEspecial/componentes/Diagnosticos/index.jsx
@@ -1,23 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Field } from "react-final-form";
 import Multiselect from "multiselect-react-dropdown";
+import $ from "jquery";
 
 const Diagnosticos = ({
   diagnosticos,
+  setDiagnosticos,
   setDiagnosticosSelecionados,
   selectedValues,
   alergiasError,
   setAlergiasError
 }) => {
+  const [values, setValues] = useState([]);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    setOptions(diagnosticos);
+  }, [diagnosticos]);
+
+  useEffect(() => {
+    // Retirada do negrito dos diagnóticos
+    let lis = $(`.optionContainer li:nth-child(-n+${values.length + 1})`);
+    $(lis).each((_, element) => {
+      $(element).css("font-weight", "");
+    });
+
+    // Negrito colocado só nos que estão selecionados
+    lis = $(`.optionContainer li:nth-child(-n+${values.length})`);
+    $(lis).each((_, element) => {
+      $(element).css("font-weight", "900");
+    });
+  }, [options]);
+
+  const compare = (a, b) => {
+    return a.nome > b.nome ? 1 : b.nome > a.nome ? -1 : 0;
+  };
+
   const onSelect = values => {
     if (alergiasError) {
       setAlergiasError(false);
     }
     setDiagnosticosSelecionados(formatarDiagnostico(values));
+    const stateOptions = diagnosticos.filter(
+      op => !values.find(o => o.uuid === op.uuid)
+    );
+    const orderedNewOptions = values.sort(compare);
+    setDiagnosticos(orderedNewOptions.concat(stateOptions.sort(compare)));
+    setValues(orderedNewOptions);
   };
 
   const onRemove = values => {
     setDiagnosticosSelecionados(formatarDiagnostico(values));
+    const stateOptions = diagnosticos.filter(
+      op => !values.find(o => o.uuid === op.uuid)
+    );
+    const orderedNewOptions = values.sort(compare);
+    setDiagnosticos(orderedNewOptions.concat(stateOptions.sort(compare)));
+    setValues(orderedNewOptions);
   };
 
   const formatarDiagnostico = values => {
@@ -34,7 +73,7 @@ const Diagnosticos = ({
         <Field
           component={Multiselect}
           name="alergias_intolerancias"
-          options={diagnosticos}
+          options={options}
           onSelect={onSelect}
           onRemove={onRemove}
           selectedValues={selectedValues}
