7b336b0d26531470174357e437d740f51bae574b
Luis <lhzimmermann@gmailc.om>
2021-08-23 16:34:15 -0300

    Fluxo completo da edição de conferencia de guia de remessa

---
+++ src/components/screens/Logistica/ConferenciaDeGuiaResumoFinal/components/confirmacaoEdicao/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_TYPE,
+  BUTTON_STYLE
+} from "components/Shareable/Botao/constants";
+
+export default ({ texto, disabled, guia, onSubmit }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    if (["Reposição total", "Reposição parcial"].includes(guia.status)) {
+      setShow(true);
+    } else {
+      onSubmit();
+    }
+  };
+
+  const handleSim = () => {
+    onSubmit();
+  };
+
+  return (
+    <>
+      <Botao
+        texto={texto}
+        type={BUTTON_TYPE.BUTTON}
+        style={BUTTON_STYLE.GREEN}
+        disabled={disabled}
+        onClick={handleShow}
+      />
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          <p>
+            Ao finalizar a edição de conferência desta Guia de Remessa, o
+            registro de reposição será apagado. Deseja continuar?
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Botao
+            texto="Não"
+            type={BUTTON_TYPE.BUTTON}
+            onClick={handleClose}
+            style={BUTTON_STYLE.GREEN_OUTLINE}
+            className="ml-3"
+          />
+          <Botao
+            texto="Sim"
+            type={BUTTON_TYPE.BUTTON}
+            style={BUTTON_STYLE.GREEN}
+            className="ml-3"
+            onClick={handleSim}
+          />
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
