6e88028853a7fcebf3865a8a9a79524590243998
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-10-27 10:12:42 -0300

    Feature: filtros de status e lote para ver mais dos cards de dieta especial

--- src/components/Shareable/InputSearchPendencias/index.jsx
+++ src/components/Shareable/InputSearchPendencias/index.jsx
@@ -1,17 +1,107 @@
 import React from "react";
+import { Form, Field } from "react-final-form";
+import { OnChange } from "react-final-form-listeners";
+import { Select } from "components/Shareable/Select";
+import InputText from "components/Shareable/Input/InputText";
+import { usuarioEhTerceirizada } from "helpers/utilities";
+import {
+  SOLICITACOES_AUTORIZADAS,
+  SOLICITACOES_CANCELADAS
+} from "configs/constants";
 import "./style.scss";
 
-export const InputSearchPendencias = props => (
-  <div className="div-input-search">
-    <div className="container-input">
-      <span>
-        <input
-          className="input-search"
-          placeholder="Pesquisar"
-          onChange={props.filterList}
-        />
-        <i className="fas fa-search" />
-      </span>
-    </div>
-  </div>
-);
+export const InputSearchPendencias = props => {
+  const ehTerceirizada = usuarioEhTerceirizada();
+  const listaStatus = [
+    { nome: "Conferência Status", uuid: "" },
+    { nome: "Conferida", uuid: "1" },
+    { nome: "Não Conferida", uuid: "0" }
+  ];
+
+  const defineColunas = () => {
+    let classTitulo = "offset-9 col-3";
+    if (
+      ehTerceirizada &&
+      window.location.pathname.includes("solicitacoes-dieta-especial")
+    ) {
+      classTitulo = "offset-6 col-3";
+      if (
+        [SOLICITACOES_AUTORIZADAS, SOLICITACOES_CANCELADAS].includes(
+          props.tipoSolicitacao
+        )
+      ) {
+        classTitulo = "offset-3 col-3";
+      }
+    }
+    return classTitulo;
+  };
+
+  const classTitulo = defineColunas();
+
+  return (
+    <Form
+      onSubmit={() => {}}
+      initialValues={{}}
+      render={({ handleSubmit, values }) => (
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className={`${classTitulo}`}>
+              <Field
+                component={InputText}
+                name="titulo"
+                placeholder="Pesquisar"
+              />
+              <OnChange name="titulo">
+                {() => {
+                  props.filterList(values);
+                }}
+              </OnChange>
+            </div>
+            {ehTerceirizada && (
+              <>
+                {listaStatus &&
+                  [SOLICITACOES_AUTORIZADAS, SOLICITACOES_CANCELADAS].includes(
+                    props.tipoSolicitacao
+                  ) && (
+                    <div className="col-3">
+                      <Field
+                        component={Select}
+                        options={listaStatus}
+                        name="status"
+                        placeholder="Conferência Status"
+                        naoDesabilitarPrimeiraOpcao
+                      />
+                      <OnChange name="status">
+                        {() => {
+                          props.filterList(values);
+                        }}
+                      </OnChange>
+                    </div>
+                  )}
+                {props.listaLotes &&
+                  window.location.pathname.includes(
+                    "solicitacoes-dieta-especial"
+                  ) && (
+                    <div className="col-3">
+                      <Field
+                        component={Select}
+                        options={props.listaLotes}
+                        name="lote"
+                        placeholder="Selecione um Lote"
+                        naoDesabilitarPrimeiraOpcao
+                      />
+                      <OnChange name="lote">
+                        {() => {
+                          props.filterList(values);
+                        }}
+                      </OnChange>
+                    </div>
+                  )}
+              </>
+            )}
+          </div>
+        </form>
+      )}
+    />
+  );
+};
