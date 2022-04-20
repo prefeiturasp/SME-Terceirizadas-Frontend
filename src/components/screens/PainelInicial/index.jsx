2c25da71958a3beb71ccc0bf144d44b9c379170e
Diogo Santos <euamofisica2006@hotmail.com>
2022-04-20 09:32:08 -0300

    Cria card de abastecimento na tela inicial

--- src/components/screens/PainelInicial/index.jsx
+++ src/components/screens/PainelInicial/index.jsx
@@ -5,6 +5,7 @@ import CardLogo from "../../Shareable/CardLogo/CardLogo";
 import IconeGestaoDeAlimentacao from "../../Shareable/Icones/IconeGestaoDeAlimentacao";
 import IconeGestaoDeProduto from "../../Shareable/Icones/IconeGestaoDeProduto";
 import IconeDietaEspecial from "../../Shareable/Icones/IconeDietaEspecial";
+import IconeAbastecimento from "../../Shareable/Icones/IconeAbastecimento";
 import {
   usuarioEhEscola,
   usuarioEhTerceirizada,
@@ -16,6 +17,7 @@ import {
   usuarioEhNutricionistaSupervisao,
   usuarioEscolaEhGestaoDireta,
   usuarioEscolaEhGestaoMistaParceira,
+  usuarioEhEscolaAbastecimento,
   exibirGA
 } from "helpers/utilities";
 
@@ -58,7 +60,6 @@ const PainelInicial = ({ history }) => {
           </CardLogo>
         </Col>
       )}
-
       {(usuarioEhQualquerCODAE() ||
         usuarioEhCODAENutriManifestacao() ||
         usuarioEhTerceirizada() ||
@@ -73,6 +74,26 @@ const PainelInicial = ({ history }) => {
             onClick={() => history.push("/painel-gestao-produto")}
           >
             <IconeGestaoDeProduto />
+          </CardLogo>
+        </Col>
+      )}
+      {usuarioEhDRE() && (
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <CardLogo
+            titulo={"Abastecimento"}
+            onClick={() => history.push("/logistica/entregas-dre")}
+          >
+            <IconeAbastecimento />
+          </CardLogo>
+        </Col>
+      )}
+      {usuarioEhEscolaAbastecimento() && (
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <CardLogo
+            titulo={"Abastecimento"}
+            onClick={() => history.push("/logistica/conferir-entrega")}
+          >
+            <IconeAbastecimento />
           </CardLogo>
         </Col>
       )}
