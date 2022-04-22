cb4dc673bdbc999ce16d48752beb786d39d72e2c
Diogo Santos <euamofisica2006@hotmail.com>
2022-04-22 12:06:04 -0300

    Melhora exibição de extensões de aquivos de imagens

--- src/components/screens/Logistica/RegistrarInsucessoEntrega/index.jsx
+++ src/components/screens/Logistica/RegistrarInsucessoEntrega/index.jsx
@@ -32,7 +32,7 @@ import "./styles.scss";
 import { gerarParametrosConsulta } from "helpers/utilities";
 
 const FORM_NAME = "registrarInsucessoEntrega";
-const FORMATOS_IMAGEM = ".png, .jpeg, .jpg";
+const FORMATOS_IMAGEM = "PNG, JPG ou JPEG";
 
 export default () => {
   const [uuid, setUuid] = useState();
@@ -254,12 +254,6 @@ export default () => {
                       <label className="mb-3">
                         Se possível, insira uma foto que demonstre o motivo do
                         insucesso de entrega.
-                        <TooltipIcone
-                          tooltipText={
-                            "Os formatos de imagem aceitos são: " +
-                            FORMATOS_IMAGEM
-                          }
-                        />
                       </label>
                       <Field
                         component={InputFile}
@@ -273,6 +267,9 @@ export default () => {
                         alignLeft
                         disabled={arquivo.length > 0}
                       />
+                      <label className="mb-3">
+                        {"Formatos aceitos: " + FORMATOS_IMAGEM}
+                      </label>
                     </article>
                   </div>
 
