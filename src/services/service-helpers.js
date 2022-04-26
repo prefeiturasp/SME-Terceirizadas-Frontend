5cdeffaeff3eb09b6927a4d7db03f3a4e9d4e744
Calvin Rossinhole <calvin.masters@gmail.com>
2022-04-26 14:44:51 -0300

    Demanda técnica: ajustes na Inclusão de Alimentação CEI

---
+++ src/services/service-helpers.js
@@ -0,0 +1,15 @@
+export const ErrorHandlerFunction = err => {
+  if (err instanceof Error) {
+    let data;
+    const responseError = err;
+    if (responseError.response) {
+      data = {
+        data: responseError.response.data,
+        status: responseError.response.status
+      };
+    } else {
+      data = { data: "Error during request", status: 500 };
+    }
+    return data;
+  }
+};
