4f721541d2008e627a41f7f06e97a929d06b43c5
Calvin Rossinhole <calvin.masters@gmail.com>
2022-06-09 14:59:39 -0300

    remove extends component

--- src/components/Shareable/ModalCODAEQuestiona/index.jsx
+++ src/components/Shareable/ModalCODAEQuestiona/index.jsx
@@ -1,5 +1,5 @@
 import HTTP_STATUS from "http-status-codes";
-import React, { Component } from "react";
+import React from "react";
 import { Modal } from "react-bootstrap";
 import { Field } from "redux-form";
 import Botao from "../Botao";
@@ -8,92 +8,85 @@ import { TextAreaWYSIWYG } from "../TextArea/TextAreaWYSIWYG";
 import { toastError, toastSuccess } from "../Toast/dialogs";
 import "./style.scss";
 
-export class ModalCODAEQuestiona extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      justificativa: ""
-    };
-  }
+export const ModalCODAEQuestiona = ({ ...props }) => {
+  const {
+    showModal,
+    closeModal,
+    uuid,
+    justificativa,
+    loadSolicitacao,
+    endpoint,
+    tipoSolicitacao
+  } = props;
 
-  async enviarQuestionamento(uuid) {
-    const { justificativa } = this.state;
+  const enviarQuestionamento = async uuid => {
     let resp = "";
-    resp = await this.props.endpoint(
+    resp = await endpoint(
       uuid,
       justificativa && justificativa !== "<p></p>\n" ? justificativa : "",
-      this.props.tipoSolicitacao
+      tipoSolicitacao
     );
     if (resp.status === HTTP_STATUS.OK) {
-      this.props.closeModal();
-      this.props.loadSolicitacao(this.props.uuid, this.props.tipoSolicitacao);
+      closeModal();
+      loadSolicitacao(uuid, tipoSolicitacao);
       toastSuccess("Questionamento enviado com sucesso!");
     } else {
       toastError(resp.data.detail);
     }
-  }
+  };
+  return (
+    <Modal
+      dialogClassName="modal-50w modal-question"
+      show={showModal}
+      onHide={closeModal}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Questionamento</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="row">
+          <div className="col-12">
+            <p className="title">
+              É possível atender a solicitação com todos os itens previstos no
+              contrato?
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <Field
+              component={TextAreaWYSIWYG}
+              label="Observação"
+              placeholder="Alguma observação para a Terceirizada?"
+              name="justificativa"
+            />
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="row mt-5">
+          <div className="col-12">
+            <Botao
+              texto="Cancelar"
+              type={BUTTON_TYPE.BUTTON}
+              onClick={closeModal}
+              style={BUTTON_STYLE.BLUE_OUTLINE}
+              className="ml-3"
+            />
+            <Botao
+              texto="Enviar"
+              type={BUTTON_TYPE.BUTTON}
+              onClick={() => {
+                enviarQuestionamento(uuid);
+              }}
+              style={BUTTON_STYLE.BLUE}
+              className="ml-3"
+            />
+          </div>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.justificativa !== this.props.justificativa) {
-      this.setState({
-        justificativa: this.props.justificativa
-      });
-    }
-  }
-  render() {
-    const { showModal, closeModal, uuid } = this.props;
-    return (
-      <Modal
-        dialogClassName="modal-50w modal-question"
-        show={showModal}
-        onHide={closeModal}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Questionamento</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="row">
-            <div className="col-12">
-              <p className="title">
-                É possível atender a solicitação com todos os itens previstos no
-                contrato?
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12">
-              <Field
-                component={TextAreaWYSIWYG}
-                label="Observação"
-                placeholder="Alguma observação para a Terceirizada?"
-                name="justificativa"
-              />
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <div className="row mt-5">
-            <div className="col-12">
-              <Botao
-                texto="Cancelar"
-                type={BUTTON_TYPE.BUTTON}
-                onClick={closeModal}
-                style={BUTTON_STYLE.BLUE_OUTLINE}
-                className="ml-3"
-              />
-              <Botao
-                texto="Enviar"
-                type={BUTTON_TYPE.BUTTON}
-                onClick={() => {
-                  this.enviarQuestionamento(uuid);
-                }}
-                style={BUTTON_STYLE.BLUE}
-                className="ml-3"
-              />
-            </div>
-          </div>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-}
+export default ModalCODAEQuestiona;
