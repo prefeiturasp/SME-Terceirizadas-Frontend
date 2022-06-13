fb08ada88eedb437466924906ec59a19813df093
Luis <lhzimmermann@gmailc.om>
2022-06-13 18:03:28 -0300

    melhoria no modal da central de downloads

--- src/components/Shareable/ModalSolicitacaoDownload/index.jsx
+++ src/components/Shareable/ModalSolicitacaoDownload/index.jsx
@@ -1,21 +1,46 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 import "antd/dist/antd.css";
+import { BUTTON_STYLE, BUTTON_TYPE } from "../Botao/constants";
+import Botao from "../Botao";
+import { CENTRAL_DOWNLOADS } from "configs/constants";
+import { useHistory } from "react-router-dom";
+import "./styles.scss";
 
 const ModalSolicitacaoDownload = ({ show, setShow }) => {
+  const history = useHistory();
+
   const handleClose = () => {
     setShow(false);
   };
 
   return (
-    <Modal show={show} onHide={handleClose} dialogClassName="modal-entregas">
+    <Modal show={show} onHide={handleClose} dialogClassName="modal-downloads">
       <Modal.Header closeButton>
         <Modal.Title>Geração solicitada com sucesso.</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        A geração foi solicitada. Em breve você receberá um aviso na central de
-        downloads com o resultado.
+        Como este arquivo poderá ser muito grande, acompanhe o seu processamento
+        na Central de Downloads.
       </Modal.Body>
+      <Modal.Footer>
+        <Botao
+          texto="Fechar"
+          type={BUTTON_TYPE.BUTTON}
+          onClick={handleClose}
+          style={BUTTON_STYLE.GREEN_OUTLINE}
+          className="ml-3"
+        />
+        <Botao
+          texto="Ir para a Central de Downloads"
+          type={BUTTON_TYPE.BUTTON}
+          onClick={() => {
+            history.push(`/${CENTRAL_DOWNLOADS}`);
+          }}
+          style={BUTTON_STYLE.GREEN}
+          className="ml-3"
+        />
+      </Modal.Footer>
     </Modal>
   );
 };
