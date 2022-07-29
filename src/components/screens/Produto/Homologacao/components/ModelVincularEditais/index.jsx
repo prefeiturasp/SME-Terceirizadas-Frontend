2f1d225ebe402f783129b9eb3561e94672dc0bc6
jvictor-am <joaovictorengp@gmail.com>
2022-07-29 20:54:10 -0300

    Feature: adicionar loaders e função para homologar produto após seleção de 1 ou mais editais

--- src/components/screens/Produto/Homologacao/components/ModelVincularEditais/index.jsx
+++ src/components/screens/Produto/Homologacao/components/ModelVincularEditais/index.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Spin } from "antd";
+import HTTP_STATUS from "http-status-codes";
 import StatefulMultiSelect from "@khanacademy/react-multi-select";
 import { Field } from "redux-form";
 import { Modal } from "react-bootstrap";
@@ -8,6 +10,8 @@ import {
   BUTTON_STYLE,
   BUTTON_TYPE
 } from "components/Shareable/Botao/constants";
+import { toastError, toastSuccess } from "components/Shareable/Toast/dialogs";
+import { CODAEHomologaProduto } from "services/produto.service";
 
 export const ModalVincularEditais = ({ ...props }) => {
   const {
@@ -15,8 +19,12 @@ export const ModalVincularEditais = ({ ...props }) => {
     closeModal,
     editaisOptions,
     editais,
-    onChangeEditais
+    onChangeEditais,
+    uuid,
+    loadSolicitacao
   } = props;
+
+  const [loading, setLoading] = useState(false);
 
   const renderizarLabelEditais = (selected, options) => {
     if (selected.length === 0) {
@@ -31,90 +39,109 @@ export const ModalVincularEditais = ({ ...props }) => {
     return `${selected.length} editais selecionados`;
   };
 
+  const onSubmit = () => {
+    setLoading(true);
+    CODAEHomologaProduto(uuid, editais).then(response => {
+      if (response.status === HTTP_STATUS.OK) {
+        toastSuccess("Solicitação de homologado enviada com sucesso");
+        setLoading(false);
+        closeModal();
+        loadSolicitacao(uuid);
+      } else {
+        toastError(response.data.detail);
+        setLoading(false);
+      }
+    });
+  };
+
   return (
     <Modal dialogClassName="modal-90w" show={showModal} onHide={closeModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>Homologação do produto</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="row mb-3">
-          <div className="col-4">
-            <Field
-              name="nome"
-              label="Nome do produto"
-              component={InputText}
-              disabled
-            />
-          </div>
-          <div className="col-4">
-            <Field
-              name="marca"
-              label="Marca do produto"
-              component={InputText}
-              disabled
-            />
-          </div>
-          <div className="col-4">
-            <Field
-              name="fabricante"
-              label="Fabricante do produto"
-              component={InputText}
-              disabled
-            />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <div className="col-4">
-            <Field
-              name="tipo"
-              label="Tipo de produto"
-              component={InputText}
-              disabled
-            />
-          </div>
-          <div className="col-4">
-            <div className="mb-2">
-              <label>Editais</label>
+      <Spin tip="Enviando..." spinning={loading}>
+        <Modal.Header closeButton>
+          <Modal.Title>Homologação do produto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row mb-3">
+            <div className="col-4">
+              <Field
+                name="nome"
+                label="Nome do produto"
+                component={InputText}
+                disabled
+              />
             </div>
-            <Field
-              component={StatefulMultiSelect}
-              name="editais"
-              selected={editais}
-              disableSearch={true}
-              options={editaisOptions.map(edital => ({
-                label: edital.numero,
-                value: edital.uuid
-              }))}
-              valueRenderer={(selected, options) =>
-                renderizarLabelEditais(selected, options)
-              }
-              overrideStrings={{
-                selectAll: "Todos os editais"
-              }}
-              onSelectedChanged={values => onChangeEditais(values)}
-            />
+            <div className="col-4">
+              <Field
+                name="marca"
+                label="Marca do produto"
+                component={InputText}
+                disabled
+              />
+            </div>
+            <div className="col-4">
+              <Field
+                name="fabricante"
+                label="Fabricante do produto"
+                component={InputText}
+                disabled
+              />
+            </div>
           </div>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <div className="row mt-4">
-          <div className="col-12">
-            <Botao
-              texto="Voltar"
-              type={BUTTON_TYPE.BUTTON}
-              onClick={closeModal}
-              style={BUTTON_STYLE.DARK_OUTLINE}
-              className="ml-3"
-            />
-            <Botao
-              texto="Homologar"
-              type={BUTTON_TYPE.SUBMIT}
-              style={BUTTON_STYLE.GREEN}
-              className="ml-3"
-            />
+          <div className="row mb-3">
+            <div className="col-4">
+              <Field
+                name="tipo"
+                label="Tipo de produto"
+                component={InputText}
+                disabled
+              />
+            </div>
+            <div className="col-4">
+              <div className="mb-2">
+                <label>Editais</label>
+              </div>
+              <Field
+                component={StatefulMultiSelect}
+                name="editais"
+                selected={editais}
+                disableSearch={true}
+                options={editaisOptions.map(edital => ({
+                  label: edital.numero,
+                  value: edital.uuid
+                }))}
+                valueRenderer={(selected, options) =>
+                  renderizarLabelEditais(selected, options)
+                }
+                overrideStrings={{
+                  selectAll: "Todos os editais"
+                }}
+                onSelectedChanged={values => onChangeEditais(values)}
+              />
+            </div>
           </div>
-        </div>
-      </Modal.Footer>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="row mt-4">
+            <div className="col-12">
+              <Botao
+                texto="Voltar"
+                type={BUTTON_TYPE.BUTTON}
+                onClick={closeModal}
+                style={BUTTON_STYLE.GREEN_OUTLINE_WHITE}
+                className="ml-3"
+              />
+              <Botao
+                texto="Homologar"
+                type={BUTTON_TYPE.BUTTON}
+                onClick={() => onSubmit()}
+                style={BUTTON_STYLE.GREEN}
+                className="ml-3"
+                disabled={editais.length === 0}
+              />
+            </div>
+          </div>
+        </Modal.Footer>
+      </Spin>
     </Modal>
   );
 };
