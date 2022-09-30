e81593f3c2eab1bc0b173b3c5f6d3b920f7ccb64
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2022-09-30 15:09:32 -0300

    Fix: exibir botao voltar e imprimir no rodapé e linha de divisão do bloco

---
+++ src/components/screens/Produto/Homologacao/components/BotoesRodape.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_TYPE,
+  BUTTON_STYLE,
+  BUTTON_ICON
+} from "components/Shareable/Botao/constants";
+import { imprimeFichaIdentificacaoProduto } from "services/produto.service";
+import { useHistory } from "react-router-dom";
+
+export const BotoesRodape = ({ homologacao }) => {
+  const history = useHistory();
+
+  return (
+    <div className="row">
+      <div className="col-12">
+        <Botao
+          type={BUTTON_TYPE.BUTTON}
+          style={BUTTON_STYLE.GREEN}
+          texto="Imprimir"
+          icon={BUTTON_ICON.PRINT}
+          onClick={() => imprimeFichaIdentificacaoProduto(homologacao.uuid)}
+          className="float-right"
+        />
+        <Botao
+          type={BUTTON_TYPE.BUTTON}
+          texto="Voltar"
+          style={BUTTON_STYLE.GREEN_OUTLINE}
+          onClick={() => history.goBack()}
+          className="mr-2 float-right"
+        />
+      </div>
+    </div>
+  );
+};
+export default BotoesRodape;
