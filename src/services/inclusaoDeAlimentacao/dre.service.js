6333957d560da15894cc9e444ef34b4e868bb1df
Calvin Rossinhole <calvin.masters@gmail.com>
2022-11-17 13:02:11 -0300

    ajustes pontuais - alteracao cemei

--- src/services/inclusaoDeAlimentacao/dre.service.js
+++ src/services/inclusaoDeAlimentacao/dre.service.js
@@ -1,4 +1,6 @@
+import axios from "../_base";
 import { PEDIDOS, FLUXO, AUTH_TOKEN } from "services/constants";
+import { ErrorHandlerFunction } from "services/service-helpers";
 import { getPath } from "./helper";
 
 export const dreListarSolicitacoesDeInclusaoDeAlimentacao = async (
@@ -37,50 +39,28 @@ export const dreListarSolicitacoesDeInclusaoDeAlimentacaoReprovados = tipoSolici
     });
 };
 
-export const dreValidarSolicitacaoDeInclusaoDeAlimentacao = (
+export const dreValidarSolicitacaoDeInclusaoDeAlimentacao = async (
   uuid,
-  justificativa,
+  payload,
   tipoSolicitacao
 ) => {
   const url = `${getPath(tipoSolicitacao)}/${uuid}/${FLUXO.DRE_VALIDA}/`;
-  let status = 0;
-  return fetch(url, {
-    method: "PATCH",
-    headers: AUTH_TOKEN,
-    body: JSON.stringify({ justificativa })
-  })
-    .then(res => {
-      status = res.status;
-      return res.json();
-    })
-    .then(data => {
-      return { data: data, status: status };
-    })
-    .catch(error => {
-      return error.json();
-    });
+  const response = await axios.patch(url, payload).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
 };
 
-export const dreReprovarSolicitacaoDeInclusaoDeAlimentacao = (
+export const dreReprovarSolicitacaoDeInclusaoDeAlimentacao = async (
   uuid,
-  justificativa,
+  payload,
   tipoSolicitacao
 ) => {
   const url = `${getPath(tipoSolicitacao)}/${uuid}/${FLUXO.DRE_NAO_VALIDA}/`;
-  let status = 0;
-  return fetch(url, {
-    method: "PATCH",
-    headers: AUTH_TOKEN,
-    body: JSON.stringify({ justificativa })
-  })
-    .then(res => {
-      status = res.status;
-      return res.json();
-    })
-    .then(data => {
-      return { data: data, status: status };
-    })
-    .catch(error => {
-      return error.json();
-    });
+  const response = await axios.patch(url, payload).catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
 };
