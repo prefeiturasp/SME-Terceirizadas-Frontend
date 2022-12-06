bf83f13f2af34412430209e12bc5152b01cdbf6d
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-12-06 09:11:04 -0300

    Adiciona service para busca de embalagens

--- src/services/qualidade.service.js
+++ src/services/qualidade.service.js
@@ -3,5 +3,7 @@ import axios from "./_base";
 export const getListaEmbalagens = async () =>
   await axios.get("/embalagens/lista-nomes-embalagens/");
 
+export const getEmbalagens = async () => await axios.get("/embalagens/");
+
 export const cadastraEmbalagens = async payload =>
   await axios.post("/embalagens/", payload);
