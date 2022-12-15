bf185f05d1afc0d3a4b9ec93ddebcbe02290319e
jvictor-am <joaovictorengp@gmail.com>
2022-12-15 17:02:53 -0300

    Feature: criar modal de lanche emergencial

---
+++ src/components/AlteracaoDeCardapioCEMEI/componentes/ModalLancheEmergencial.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_TYPE,
+  BUTTON_STYLE
+} from "components/Shareable/Botao/constants";
+
+export const ModalLancheEmergencial = ({
+  closeModal,
+  showModal,
+  resetForm
+}) => {
+  const fecharModal = () => {
+    resetForm();
+    closeModal();
+  };
+
+  return (
+    <Modal dialogClassName="modal-50w" show={showModal} onHide={() => {}}>
+      <Modal.Header>
+        <Modal.Title>Lanche Emergencial</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <label className="font-weight-bold">
+          O lanche emergencial somente é previsto para os alunos da EMEI da
+          CEMEI.
+        </label>
+        <label className="font-weight-bold">
+          Para realizar a solicitação selecione a opção de alunos EMEI.
+        </label>
+      </Modal.Body>
+      <Modal.Footer>
+        <Botao
+          texto="OK"
+          type={BUTTON_TYPE.BUTTON}
+          onClick={() => fecharModal()}
+          style={BUTTON_STYLE.GREEN_OUTLINE}
+          className="float-right"
+        />
+      </Modal.Footer>
+    </Modal>
+  );
+};
