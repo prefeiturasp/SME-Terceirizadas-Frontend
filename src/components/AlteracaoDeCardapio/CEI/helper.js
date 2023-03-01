f0ce567971e9049236e1216d8c90cf001f110a57
Diogo Santos <euamofisica2006@hotmail.com>
2023-03-01 15:04:25 -0300

    Corrige index das substituições

--- src/components/AlteracaoDeCardapio/CEI/helper.js
+++ src/components/AlteracaoDeCardapio/CEI/helper.js
@@ -22,7 +22,7 @@ export const totalAlunosInputPorPeriodo = (values, index) => {
 export const formataPayload = values => {
   const values_ = deepCopy(values);
   let filteredSubstituicoes = values_.substituicoes.filter(obj =>
-    obj.hasOwnProperty("faixas_etarias")
+    obj.hasOwnProperty("faixas")
   );
   values_.substituicoes = filteredSubstituicoes;
   values_.data = values_.data
