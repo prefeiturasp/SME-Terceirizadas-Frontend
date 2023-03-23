a1d1fe06c8b5fd67a5966da0e1b902110c517c15
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2023-03-23 15:32:00 -0300

    Fix: adicionar coluna para tipo de turma

--- src/components/screens/Relatorios/AlunosMatriculados/componentes/TabelaResultado/index.jsx
+++ src/components/screens/Relatorios/AlunosMatriculados/componentes/TabelaResultado/index.jsx
@@ -42,18 +42,22 @@ export const TabelaResultado = ({ ...props }) => {
                 <th className="col-1">Lote</th>
                 <th className="col-1">Tipo de unid.</th>
                 <th className="col-3">Unid. educacional</th>
-                <th className="col-2 text-center">Período</th>
+                <th className="col-1 text-center">Tipo de turma</th>
+                <th className="col-1 text-center">Período</th>
                 <th className="col-1 text-center">Matriculados</th>
                 <th className="col-1" />
               </tr>
             </thead>
             <tbody>
               {resultado.map((item, index) => {
-                const totalMatriculados = Object.values(
-                  item.alunos_por_faixa_etaria
-                ).reduce((total, value) => {
-                  return total + value;
-                }, 0);
+                let totalMatriculados = item.quantidade_alunos;
+                if (item.alunos_por_faixa_etaria && item.escola.exibir_faixas) {
+                  totalMatriculados = Object.values(
+                    item.alunos_por_faixa_etaria
+                  ).reduce((total, value) => {
+                    return total + value;
+                  }, 0);
+                }
                 return (
                   <Fragment key={index}>
                     <tr className="table-body-items" key={index}>
@@ -79,6 +83,7 @@ export const TabelaResultado = ({ ...props }) => {
                           {formataNome(item.escola.nome)}
                         </Tooltip>
                       </td>
+                      <td className="text-center">{item.tipo_turma}</td>
                       <td className="text-center">
                         {item.periodo_escolar.nome}
                       </td>
