0065e0963773b571ee73a145ae07ac11832025d2
jvictor-am <joaovictorengp@gmail.com>
2023-03-24 17:00:12 -0300

    Feature: ajustar exibição de filtros para usuário DRE

--- src/components/screens/Relatorios/AlunosMatriculados/componentes/Filtros/index.jsx
+++ src/components/screens/Relatorios/AlunosMatriculados/componentes/Filtros/index.jsx
@@ -10,7 +10,7 @@ import {
 import { toastError } from "components/Shareable/Toast/dialogs";
 import { formataOpcoes } from "../../helpers";
 import { filtrarAlunosMatriculados } from "services/alunosMatriculados.service";
-import { deepCopy } from "helpers/utilities";
+import { deepCopy, usuarioEhDRE } from "helpers/utilities";
 import { formataOpcoesDropdown } from "../../helpers";
 
 export const Filtros = ({ ...props }) => {
@@ -72,102 +72,183 @@ export const Filtros = ({ ...props }) => {
       <Form onSubmit={onSubmit}>
         {({ form, handleSubmit, values }) => (
           <form onSubmit={handleSubmit}>
-            <div className="row">
-              <div className="col-8">
-                <label>DRE</label>
-                <Field
-                  component={StatefulMultiSelect}
-                  name="diretorias_regionais"
-                  selected={values.diretorias_regionais || []}
-                  options={dres}
-                  onSelectedChanged={values_ => {
-                    form.change("diretorias_regionais", values_);
-                    form.change("lotes", []);
-                    form.change("unidades_educacionais", []);
-                  }}
-                  hasSelectAll
-                  overrideStrings={{
-                    selectSomeItems: "Selecione",
-                    allItemsAreSelected: "Todas as DREs",
-                    selectAll: "Todas"
-                  }}
-                />
-              </div>
-              <div className="col-4">
-                <label>Lote</label>
-                <Field
-                  component={StatefulMultiSelect}
-                  name="lotes"
-                  selected={values.lotes || []}
-                  options={
-                    values.diretorias_regionais &&
-                    values.diretorias_regionais.length
-                      ? formataOpcoes(
-                          listaOpcoes.lotes.filter(lote =>
-                            values.diretorias_regionais.includes(
-                              lote.diretoria_regional.uuid
+            {usuarioEhDRE() ? (
+              <>
+                <div className="row">
+                  <div className="col-3">
+                    <label>Lote</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="lotes"
+                      selected={values.lotes || []}
+                      options={
+                        values.diretorias_regionais &&
+                        values.diretorias_regionais.length
+                          ? formataOpcoes(
+                              listaOpcoes.lotes.filter(lote =>
+                                values.diretorias_regionais.includes(
+                                  lote.diretoria_regional.uuid
+                                )
+                              )
                             )
-                          )
-                        )
-                      : lotes
-                  }
-                  onSelectedChanged={values_ => {
-                    form.change("lotes", values_);
-                    form.change("unidades_educacionais", []);
-                  }}
-                  hasSelectAll
-                  overrideStrings={{
-                    selectSomeItems: "Selecione",
-                    allItemsAreSelected: "Todos os lotes",
-                    selectAll: "Todos"
-                  }}
-                />
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-4">
-                <label>Tipo de unidade</label>
-                <Field
-                  component={StatefulMultiSelect}
-                  name="tipos_unidades"
-                  selected={values.tipos_unidades || []}
-                  options={tiposUnidades}
-                  onSelectedChanged={values_ =>
-                    form.change("tipos_unidades", values_)
-                  }
-                  hasSelectAll
-                  overrideStrings={{
-                    selectSomeItems: "Selecione",
-                    allItemsAreSelected: "Todos os Tipos de Unidades",
-                    selectAll: "Todos"
-                  }}
-                />
-              </div>
-              <div className="col-8">
-                <label>Unidade Educacional</label>
-                <Field
-                  component={StatefulMultiSelect}
-                  name="unidades_educacionais"
-                  selected={values.unidades_educacionais || []}
-                  options={
-                    (values.diretorias_regionais &&
-                      values.diretorias_regionais.length) ||
-                    (values.lotes && values.lotes.length)
-                      ? filtrarOpcoesEscola(values)
-                      : unidadesEducacionais
-                  }
-                  onSelectedChanged={values_ =>
-                    form.change("unidades_educacionais", values_)
-                  }
-                  hasSelectAll
-                  overrideStrings={{
-                    selectSomeItems: "Selecione",
-                    allItemsAreSelected: "Todas as Unidades Educacionais",
-                    selectAll: "Todas"
-                  }}
-                />
-              </div>
-            </div>
+                          : lotes
+                      }
+                      onSelectedChanged={values_ => {
+                        form.change("lotes", values_);
+                        form.change("unidades_educacionais", []);
+                      }}
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todos os lotes",
+                        selectAll: "Todos"
+                      }}
+                    />
+                  </div>
+                  <div className="col-3">
+                    <label>Tipo de Unidade</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="tipos_unidades"
+                      selected={values.tipos_unidades || []}
+                      options={tiposUnidades}
+                      onSelectedChanged={values_ =>
+                        form.change("tipos_unidades", values_)
+                      }
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todos os Tipos de Unidades",
+                        selectAll: "Todos"
+                      }}
+                    />
+                  </div>
+                  <div className="col-6">
+                    <label>Unidade Educacional</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="unidades_educacionais"
+                      selected={values.unidades_educacionais || []}
+                      options={
+                        (values.diretorias_regionais &&
+                          values.diretorias_regionais.length) ||
+                        (values.lotes && values.lotes.length)
+                          ? filtrarOpcoesEscola(values)
+                          : unidadesEducacionais
+                      }
+                      onSelectedChanged={values_ =>
+                        form.change("unidades_educacionais", values_)
+                      }
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todas as Unidades Educacionais",
+                        selectAll: "Todas"
+                      }}
+                    />
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="row">
+                  <div className="col-8">
+                    <label>DRE</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="diretorias_regionais"
+                      selected={values.diretorias_regionais || []}
+                      options={dres}
+                      onSelectedChanged={values_ => {
+                        form.change("diretorias_regionais", values_);
+                        form.change("lotes", []);
+                        form.change("unidades_educacionais", []);
+                      }}
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todas as DREs",
+                        selectAll: "Todas"
+                      }}
+                    />
+                  </div>
+                  <div className="col-4">
+                    <label>Lote</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="lotes"
+                      selected={values.lotes || []}
+                      options={
+                        values.diretorias_regionais &&
+                        values.diretorias_regionais.length
+                          ? formataOpcoes(
+                              listaOpcoes.lotes.filter(lote =>
+                                values.diretorias_regionais.includes(
+                                  lote.diretoria_regional.uuid
+                                )
+                              )
+                            )
+                          : lotes
+                      }
+                      onSelectedChanged={values_ => {
+                        form.change("lotes", values_);
+                        form.change("unidades_educacionais", []);
+                      }}
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todos os lotes",
+                        selectAll: "Todos"
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="row mt-3">
+                  <div className="col-4">
+                    <label>Tipo de Unidade</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="tipos_unidades"
+                      selected={values.tipos_unidades || []}
+                      options={tiposUnidades}
+                      onSelectedChanged={values_ =>
+                        form.change("tipos_unidades", values_)
+                      }
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todos os Tipos de Unidades",
+                        selectAll: "Todos"
+                      }}
+                    />
+                  </div>
+                  <div className="col-8">
+                    <label>Unidade Educacional</label>
+                    <Field
+                      component={StatefulMultiSelect}
+                      name="unidades_educacionais"
+                      selected={values.unidades_educacionais || []}
+                      options={
+                        (values.diretorias_regionais &&
+                          values.diretorias_regionais.length) ||
+                        (values.lotes && values.lotes.length)
+                          ? filtrarOpcoesEscola(values)
+                          : unidadesEducacionais
+                      }
+                      onSelectedChanged={values_ =>
+                        form.change("unidades_educacionais", values_)
+                      }
+                      hasSelectAll
+                      overrideStrings={{
+                        selectSomeItems: "Selecione",
+                        allItemsAreSelected: "Todas as Unidades Educacionais",
+                        selectAll: "Todas"
+                      }}
+                    />
+                  </div>
+                </div>
+              </>
+            )}
             <div className="row mt-3">
               <div className="col-12 text-right">
                 <Botao
