2044f90d946b8c815b8fe78af21210ac9299bcb8
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2023-05-19 13:49:36 -0300

    Feature: alterar nome do componente, permitir usar mesmo modal para aprovar e solicitar ocorrência

--- src/components/screens/LancamentoInicial/ConferenciaDosLancamentos/components/ModalOcorrencia/index.jsx
+++ src/components/screens/LancamentoInicial/ConferenciaDosLancamentos/components/ModalOcorrencia/index.jsx
@@ -10,12 +10,25 @@ import { peloMenosUmCaractere, required } from "helpers/fieldValidators";
 import { Field, Form } from "react-final-form";
 import CKEditorField from "components/Shareable/CKEditorField";
 import { toastError, toastSuccess } from "components/Shareable/Toast/dialogs";
-import { drePedeCorrecaoOcorrencia } from "services/medicaoInicial/solicitacaoMedicaoInicial.service";
+import {
+  drePedeCorrecaoOcorrencia,
+  drePedeAprovacaoOcorrencia
+} from "services/medicaoInicial/solicitacaoMedicaoInicial.service";
 
-export const ModalSolicitarCorrecaoOcorrencia = ({ ...props }) => {
-  const { showModal, setShowModal, ocorrencia, atualizarDados } = props;
+export const ModalOcorrencia = ({ ...props }) => {
+  const {
+    showModal,
+    setShowModal,
+    ocorrencia,
+    atualizarDados,
+    titulo,
+    descricao,
+    temJustificativa,
+    ehCorrecao,
+    tituloBotoes
+  } = props;
 
-  const onSubmit = async values => {
+  const solicitarCorrecao = async values => {
     const response = await drePedeCorrecaoOcorrencia(ocorrencia.uuid, values);
     if (response.status === HTTP_STATUS.OK) {
       setShowModal(false);
@@ -31,6 +44,22 @@ export const ModalSolicitarCorrecaoOcorrencia = ({ ...props }) => {
     setShowModal(false);
   };
 
+  const aprovarOcorrencia = async () => {
+    const response = await drePedeAprovacaoOcorrencia(ocorrencia.uuid);
+    if (response.status === HTTP_STATUS.OK) {
+      setShowModal(false);
+      toastSuccess(
+        `Solicitação de aprovação no Formulário de Ocorrências salva com sucesso!`
+      );
+      atualizarDados();
+    } else {
+      toastError(
+        `Houve um erro ao salvar aprovação no Formulário de Ocorrências!`
+      );
+    }
+    setShowModal(false);
+  };
+
   return (
     <Modal
       dialogClassName="modal-50w"
@@ -38,58 +67,63 @@ export const ModalSolicitarCorrecaoOcorrencia = ({ ...props }) => {
       onHide={() => setShowModal(false)}
     >
       <Modal.Header closeButton>
-        <Modal.Title>
-          Solicitar correção no formulário de ocorrências
-        </Modal.Title>
+        <Modal.Title>{titulo}</Modal.Title>
       </Modal.Header>
 
       <Form
-        onSubmit={values => onSubmit(values)}
+        onSubmit={values =>
+          ehCorrecao ? solicitarCorrecao(values) : aprovarOcorrencia()
+        }
         initialValues={{}}
         render={({ handleSubmit, submitting, values }) => (
           <form onSubmit={handleSubmit}>
             <Modal.Body>
               <div className="form-row row-ativacao mt-3">
                 <div className="col-12">
-                  <p>
-                    Informe quais os pontos necessários de correção no
-                    Formulário de Ocorrências
-                  </p>
+                  <p>{descricao}</p>
                 </div>
-                <div className="col-12">
-                  <Field
-                    component={CKEditorField}
-                    label="Descrição da Correção"
-                    name="justificativa"
-                    required
-                    validate={value => {
-                      for (let validator of [peloMenosUmCaractere, required]) {
-                        const erro = validator(value);
-                        if (erro) return erro;
-                      }
-                    }}
-                  />
-                </div>
+                {temJustificativa && (
+                  <div className="col-12">
+                    <Field
+                      component={CKEditorField}
+                      label="Descrição da Correção"
+                      name="justificativa"
+                      required
+                      validate={value => {
+                        for (let validator of [
+                          peloMenosUmCaractere,
+                          required
+                        ]) {
+                          const erro = validator(value);
+                          if (erro) return erro;
+                        }
+                      }}
+                    />
+                  </div>
+                )}
               </div>
             </Modal.Body>
             <Modal.Footer className="border-0">
               <div className="row mt-4">
                 <div className="col-12">
                   <Botao
-                    texto="Cancelar"
+                    texto={tituloBotoes[0]}
                     type={BUTTON_TYPE.BUTTON}
                     onClick={() => setShowModal(false)}
                     style={BUTTON_STYLE.GREEN_OUTLINE}
                     className="ml-3"
                   />
                   <Botao
-                    texto="Enviar"
+                    texto={tituloBotoes[1]}
                     type={BUTTON_TYPE.SUBMIT}
                     style={BUTTON_STYLE.GREEN}
                     className="ml-3"
                     disabled={
-                      submitting ||
-                      peloMenosUmCaractere(values.justificativa) !== undefined
+                      ehCorrecao
+                        ? submitting ||
+                          peloMenosUmCaractere(values.justificativa) !==
+                            undefined
+                        : false
                     }
                   />
                 </div>
