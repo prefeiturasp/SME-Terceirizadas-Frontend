e7754369846c2fcc1b5e7f58ba80c845049285b1
Luis <lhzimmermann@gmail.com>
2023-06-20 09:46:27 -0300

    modal de detalhamento de guias

---
+++ src/components/screens/Logistica/CadastroNotificacao/components/ModalDetalharGuia/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_TYPE,
+  BUTTON_STYLE
+} from "components/Shareable/Botao/constants";
+import DetalheGuiaRemessa from "components/Logistica/DetalheGuiaRemessa";
+import "./style.scss";
+import { Spin } from "antd";
+import { imprimirGuiaRemessa } from "services/logistica.service";
+import { toastError } from "components/Shareable/Toast/dialogs";
+
+export default ({ guia, handleClose, botaoAcao }) => {
+  const [carregando, setCarregando] = useState(false);
+
+  const baixarPDFGuiaRemessa = () => {
+    setCarregando(true);
+    let uuid = guia.uuid;
+    imprimirGuiaRemessa(uuid)
+      .then(() => {
+        setCarregando(false);
+      })
+      .catch(error => {
+        error.response.data.text().then(text => toastError(text));
+        setCarregando(false);
+      });
+  };
+
+  return (
+    <Modal
+      show={guia ? true : false}
+      onHide={handleClose}
+      dialogClassName="modal-detalhe-guia-ocorrencia"
+    >
+      <Spin tip="Carregando..." spinning={carregando}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {" "}
+            Nº da Guia de Remessa: {guia ? guia.numero_guia : ""}{" "}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <DetalheGuiaRemessa guia={guia} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Botao
+            texto="Baixar PDF da Guia"
+            type={BUTTON_TYPE.BUTTON}
+            onClick={() => baixarPDFGuiaRemessa()}
+            style={BUTTON_STYLE.GREEN_OUTLINE}
+            className="ml-3"
+          />
+          {botaoAcao(guia)}
+        </Modal.Footer>
+      </Spin>
+    </Modal>
+  );
+};
