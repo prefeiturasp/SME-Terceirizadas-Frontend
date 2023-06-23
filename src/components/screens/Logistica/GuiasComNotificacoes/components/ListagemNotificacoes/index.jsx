12890a2b932e87a8f83107dfa175dc8ad658afce
Luis <lhzimmermann@gmail.com>
2023-06-23 17:33:43 -0300

    correcao de conflitos

--- src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemNotificacoes/index.jsx
+++ src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemNotificacoes/index.jsx
@@ -1,21 +1,14 @@
 import { LOGISTICA, NOTIFICAR_EMPRESA } from "configs/constants";
 import React from "react";
-<<<<<<< Updated upstream:src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemGuias/index.jsx
 import { useHistory } from "react-router-dom";
-=======
 import { NavLink } from "react-router-dom";
 
->>>>>>> Stashed changes:src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemNotificacoes/index.jsx
 import "./styles.scss";
 import { EDITAR_NOTIFICACAO } from "configs/constants";
 
-<<<<<<< Updated upstream:src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemGuias/index.jsx
-const ListagemSolicitacoes = ({ guias }) => {
+const ListagemNotificacoes = ({ notificacoes }) => {
   const history = useHistory();
 
-=======
-const ListagemNotificacoes = ({ notificacoes }) => {
->>>>>>> Stashed changes:src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemNotificacoes/index.jsx
   return (
     <section className="resultado-guias-notificacoes">
       <div className="titulo-verde">Lista de Notificações</div>
@@ -30,35 +23,6 @@ const ListagemNotificacoes = ({ notificacoes }) => {
         {notificacoes.map(notificacao => {
           return (
             <>
-<<<<<<< Updated upstream:src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemGuias/index.jsx
-              <div key={guia.uuid} className="grid-table body-table">
-                <div>{guia.numero}</div>
-                <div>{guia.nome_empresa}</div>
-                <div>{guia.status}</div>
-                <div>{guia.processo_sei ? guia.processo_sei : "--"}</div>
-                <div>
-                  <span
-                    onClick={() =>
-                      history.push({
-                        pathname: `/logistica/${EDITAR_NOTIFICACAO}`,
-                        state: {
-                          guia: guia
-                        }
-                      })
-                    }
-                  >
-                    <i
-                      title={
-                        guia.status === "RASCUNHO"
-                          ? "Editar Rascunho"
-                          : "Visualizar Notificação"
-                      }
-                      className={`verde fas fa-${
-                        guia.status === "RASCUNHO" ? "edit" : "eye"
-                      }`}
-                    />
-                  </span>
-=======
               <div key={notificacao.uuid} className="grid-table body-table">
                 <div>{notificacao.numero}</div>
                 <div>{notificacao.nome_empresa}</div>
@@ -67,6 +31,27 @@ const ListagemNotificacoes = ({ notificacoes }) => {
                   {notificacao.processo_sei ? notificacao.processo_sei : "--"}
                 </div>
                 <div>
+                  <span
+                    onClick={() =>
+                      history.push({
+                        pathname: `/logistica/${EDITAR_NOTIFICACAO}`,
+                        state: {
+                          guia: notificacao
+                        }
+                      })
+                    }
+                  >
+                    <i
+                      title={
+                        notificacao.status === "RASCUNHO"
+                          ? "Editar Rascunho"
+                          : "Visualizar Notificação"
+                      }
+                      className={`verde fas fa-${
+                        notificacao.status === "RASCUNHO" ? "edit" : "eye"
+                      }`}
+                    />
+                  </span>
                   <NavLink
                     to={`/${LOGISTICA}/${NOTIFICAR_EMPRESA}?uuid=${
                       notificacao.uuid
@@ -76,7 +61,6 @@ const ListagemNotificacoes = ({ notificacoes }) => {
                       <i className="fas fa-eye green" />
                     </span>
                   </NavLink>
->>>>>>> Stashed changes:src/components/screens/Logistica/GuiasComNotificacoes/components/ListagemNotificacoes/index.jsx
                 </div>
               </div>
             </>
