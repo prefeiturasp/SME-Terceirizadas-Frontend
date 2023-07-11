c5b26d00b254f88c7d9c815bda8317c305b58a89
Kelwy Oliveira <kelwyduarte@gmail.com>
2023-07-11 12:04:02 -0300

    Adiciona e ajusta endpoints

--- src/services/qualidade.service.js
+++ src/services/qualidade.service.js
@@ -3,7 +3,13 @@ import axios from "./_base";
 export const getListaNomesEmbalagens = async () =>
   await axios.get("/embalagens/lista-nomes-embalagens/");
 
-export const getEmbalagens = async () => await axios.get("/embalagens/");
+export const getListaAbreviacaoEmbalagens = async () =>
+  await axios.get("/embalagens/lista-abreviacao-embalagens/");
+
+export const getEmbalagens = async params => {
+  const url = `/embalagens/`;
+  return await axios.get(url, { params });
+};
 
 export const getEmbalagem = async uuid =>
   await axios.get(`/embalagens/${uuid}/`);
