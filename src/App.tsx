f1faa4791da7a438a106cba54ec1af90a14015cb
Calvin Rossinhole <calvin.masters@gmail.com>
2023-09-11 11:11:49 -0300

    build(deps): bump react-toastify to 9.1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { GlobalContext } from "context";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import Routes from "./routes";
 
 export const App = () => {
   return (
     <GlobalContext>
+      <ToastContainer />
       <Routes />
     </GlobalContext>
   );
