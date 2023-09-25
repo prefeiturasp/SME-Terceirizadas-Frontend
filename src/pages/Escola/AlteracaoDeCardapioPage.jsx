729f08389345d12f50643c07a85d35ebada9a70b
Calvin Rossinhole <calvin.masters@gmail.com>
2023-09-25 09:24:32 -0300

    feat: refatora container alteracao cardapio

--- src/pages/Escola/AlteracaoDeCardapioPage.jsx
+++ src/pages/Escola/AlteracaoDeCardapioPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Breadcrumb from "../../components/Shareable/Breadcrumb";
-import Container from "../../components/AlteracaoDeCardapio/Container";
-import Page from "../../components/Shareable/Page/Page";
+import Breadcrumb from "components/Shareable/Breadcrumb";
+import { Container } from "components/AlteracaoDeCardapio/Container";
+import Page from "components/Shareable/Page/Page";
 import { HOME } from "./constants";
 
 const atual = {
