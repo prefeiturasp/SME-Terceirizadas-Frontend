200283c4f0a2797759aff81dea9ce3639049c5bd
Elder Santos <elder_rn@hotmail.com>
2023-09-27 15:50:47 -0300

    finaliza screen de Detalhar

--- src/components/screens/PreRecebimento/LayoutEmbalagem/components/Detalhar/index.jsx
+++ src/components/screens/PreRecebimento/LayoutEmbalagem/components/Detalhar/index.jsx
@@ -6,34 +6,41 @@ import { useHistory } from "react-router-dom";
 import BotaoVoltar from "components/Shareable/Page/BotaoVoltar";
 import { detalharLayoutEmabalagem } from "services/layoutEmbalagem.service";
 import { TextArea } from "components/Shareable/TextArea/TextArea";
+import BotaoAnexo from "./components/BotaoAnexo";
 
 export default () => {
   const history = useHistory();
   const [carregando, setCarregando] = useState(true);
   const [objeto, setObjeto] = useState({});
-  // const [primaria, setPrimaria] = useState([]);
-  // const [secundaria, setSecundaria] = useState([]);
-  // const [terciaria, setTerciaria] = useState([]);
+  const [embalagemPrimaria, setEmbalagemPrimaria] = useState([]);
+  const [embalagemSecundaria, setEmbalagemSecundaria] = useState([]);
+  const [embalagemTerciaria, setEmbalagemTerciaria] = useState([]);
 
   const voltarPagina = () =>
     history.push(`/${PRE_RECEBIMENTO}/${LAYOUT_EMBALAGEM}`);
 
-  const buscarObjeto = async () => {
+  const carregarDados = async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const uuid = urlParams.get("uuid");
     const response = await detalharLayoutEmabalagem(uuid);
 
-    console.log("RESPONSE", response);
-
     setObjeto(response.data);
+    setEmbalagemPrimaria(obterImagensEmbalagem(response, "PRIMARIA"));
+    setEmbalagemSecundaria(obterImagensEmbalagem(response, "SECUNDARIA"));
+    setEmbalagemTerciaria(obterImagensEmbalagem(response, "TERCIARIA"));
+  };
 
-    console.log("OBJETO", objeto);
+  const obterImagensEmbalagem = (response, tipo_embalagem) => {
+    return response.data.tipos_de_embalagens
+      .filter((e) => e.tipo_embalagem === tipo_embalagem)
+      .map((e) => e.imagens)
+      .flat();
   };
 
   useEffect(() => {
     setCarregando(true);
 
-    buscarObjeto();
+    carregarDados();
 
     setCarregando(false);
   }, []);
@@ -42,8 +49,8 @@ export default () => {
     <Spin tip="Carregando..." spinning={carregando}>
       <div className="card mt-3 card-cadastro-layout-embalagem">
         <div className="card-body">
-          <div className="subtitulo">Dados do Produto</div>
-          <div className="row">
+          <div className="subtitulo mb-3">Dados do Produto</div>
+          <div className="row mt-3">
             <div className="col-4">
               <label className="label-dados-produto">Nº do Cronograma</label>
             </div>
@@ -74,30 +81,40 @@ export default () => {
 
           <hr />
 
-          <div className="subtitulo">Embalagem Primária</div>
+          <div className="subtitulo mb-3">Embalagem Primária</div>
           <div className="row">
-            <div className="col-12">
-              <article>Visualizar Anexo</article>
+            <div className="col-4">
+              {embalagemPrimaria.map((e) => (
+                <BotaoAnexo key={e.arquivo} urlAnexo={e.arquivo} />
+              ))}
             </div>
           </div>
 
           <hr />
 
-          <div className="subtitulo">Embalagem Secundária</div>
+          <div className="subtitulo mb-3">Embalagem Secundária</div>
           <div className="row">
-            <div className="col-12">
-              <article>Visualizar Anexo</article>
+            <div className="col-4">
+              {embalagemSecundaria.map((e) => (
+                <BotaoAnexo key={e.arquivo} urlAnexo={e.arquivo} />
+              ))}
             </div>
           </div>
 
-          <hr />
+          {embalagemTerciaria.length > 0 && (
+            <>
+              <hr />
 
-          <div className="subtitulo">Embalagem Terciária</div>
-          <div className="row">
-            <div className="col-12">
-              <article>Visualizar Anexo</article>
-            </div>
-          </div>
+              <div className="subtitulo mb-3">Embalagem Terciária</div>
+              <div className="row">
+                <div className="col-4">
+                  {embalagemTerciaria.map((e) => (
+                    <BotaoAnexo key={e.arquivo} urlAnexo={e.arquivo} />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
 
           {objeto.observacoes && (
             <>
