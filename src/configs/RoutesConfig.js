46592cd004087991da4638181d1ac506e3b049c0
Elder Santos <elder_rn@hotmail.com>
2023-11-13 11:30:31 -0300

    cria pagina Ver Mais do card Aprovados

--- src/configs/RoutesConfig.js
+++ src/configs/RoutesConfig.js
@@ -274,6 +274,7 @@ import CadastroDocumentosRecebimentoPage from "../pages/PreRecebimento/CadastroD
 import DetalharDocumentosRecebimentoPage from "../pages/PreRecebimento/DetalharDocumentosRecebimentoPage";
 import PainelDocumentosRecebimentoPage from "../pages/PreRecebimento/PainelDocumentosRecebimentoPage";
 import StatusDocumentoPendenteAprovacao from "../pages/PreRecebimento/CardsDocumentosRecebimento/StatusDocumentoPendenteAprovacao";
+import StatusDocumentoAprovados from "../pages/PreRecebimento/CardsDocumentosRecebimento/StatusDocumentoAprovados";
 
 const routesConfig = [
   {
@@ -2008,7 +2009,13 @@ const routesConfig = [
     path: `/${constants.PRE_RECEBIMENTO}/${constants.PAINEL_DOCUMENTOS_RECEBIMENTO}/${constants.PENDENTES_APROVACAO}/`,
     component: StatusDocumentoPendenteAprovacao,
     exact: true,
-    tipoUsuario: usuarioComAcessoAoPainelEmbalagens(),
+    tipoUsuario: usuarioComAcessoAoPainelDocumentos(),
+  },
+  {
+    path: `/${constants.PRE_RECEBIMENTO}/${constants.PAINEL_DOCUMENTOS_RECEBIMENTO}/${constants.APROVADOS}/`,
+    component: StatusDocumentoAprovados,
+    exact: true,
+    tipoUsuario: usuarioComAcessoAoPainelDocumentos(),
   },
 ];
 
