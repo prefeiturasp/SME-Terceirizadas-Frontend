163400dd768772b0921d4e57f61461c70b5d04e7
Luis Guerreiro <luiscvlh11@gmail.com>
2023-12-06 12:37:55 -0300

    feat(108371): :sparkles: ajusta as informacoes para inclusoes continuas e etec

--- src/components/Shareable/SolicitacoesSimilaresInclusao/index.jsx
+++ src/components/Shareable/SolicitacoesSimilaresInclusao/index.jsx
@@ -1,9 +1,55 @@
 import React from "react";
 import { Collapse } from "react-collapse";
 import "./style.scss";
+import { WEEK } from "configs/constants";
 
 export const SolicitacoesSimilaresInclusao = ({ ...props }) => {
   const { solicitacao, index } = props;
+
+  const ehInclusaoContinuaENaoETEC = (quantidade_por_periodo) => {
+    return (
+      quantidade_por_periodo.inclusao_alimentacao_continua !== null &&
+      solicitacao.motivo.nome !== "ETEC"
+    );
+  };
+
+  const possuiAlgumaInclusaoContinuaNaoETEC =
+    solicitacao.quantidades_periodo.some((qp) =>
+      ehInclusaoContinuaENaoETEC(qp)
+    );
+
+  const renderDataSolicitacao = (solicitacao) => {
+    if (solicitacao.data_inicial && solicitacao.data_final) {
+      return (
+        <>
+          <div className="col-2">
+            <p>DE:</p>
+            <p>
+              <b>{solicitacao.data_inicial}</b>
+            </p>
+          </div>
+          <div className="col-2">
+            <p>ATÉ:</p>
+            <p>
+              <b>{solicitacao.data_final}</b>
+            </p>
+          </div>
+        </>
+      );
+    }
+    return (
+      <div className="col-4">
+        <p>Dia(s) de inclusão:</p>
+        <p>
+          {solicitacao.inclusoes?.map((inclusao, index) => (
+            <b className="mr-4" key={index}>
+              {inclusao.data}
+            </b>
+          ))}
+        </p>
+      </div>
+    );
+  };
 
   return (
     <Collapse isOpened={solicitacao.collapsed} key={index}>
@@ -39,28 +85,33 @@ export const SolicitacoesSimilaresInclusao = ({ ...props }) => {
               <div className="col-4">
                 <p>Motivo:</p>
                 <p>
-                  <b>{solicitacao.inclusoes[0].motivo.nome}</b>
+                  <b>
+                    {solicitacao.motivo?.nome ||
+                      solicitacao.inclusoes[0].motivo.nome}
+                  </b>
                 </p>
               </div>
-              <div className="col-4">
-                <p>Dia(s) de inclusão:</p>
-                <p>
-                  {solicitacao.inclusoes.map((inclusao, index) => (
-                    <b className="mr-4" key={index}>
-                      {inclusao.data}
-                    </b>
-                  ))}
-                </p>
-              </div>
+              {renderDataSolicitacao(solicitacao)}
             </div>
             <div>
               <table className="periodos-emef mt-3">
                 <thead>
                   <tr className="row bg-black">
+                    {possuiAlgumaInclusaoContinuaNaoETEC && (
+                      <th className="col-2 text-white">
+                        <b>Repetir</b>
+                      </th>
+                    )}
                     <th className="col-2 text-white">
                       <b>Período</b>
                     </th>
-                    <th className="col-8 text-white">
+                    <th
+                      className={
+                        possuiAlgumaInclusaoContinuaNaoETEC
+                          ? "col-6 text-white"
+                          : "col-8 text-white"
+                      }
+                    >
                       <b>Tipos de Alimentação</b>
                     </th>
                     <th className="col-2 text-white text-center">
@@ -69,32 +120,66 @@ export const SolicitacoesSimilaresInclusao = ({ ...props }) => {
                   </tr>
                 </thead>
                 <tbody>
-                  {solicitacao.quantidades_periodo.map((periodo, idx) => {
-                    return (
-                      <tr className="row bg-white" key={idx}>
-                        <td className="col-2">
-                          <p>{periodo.periodo_escolar.nome}</p>
-                        </td>
-                        <td className="col-8">
-                          <p>
-                            {periodo.tipos_alimentacao.reduce(
-                              (acc, tipo, idxTipo) =>
-                                acc +
-                                tipo.nome +
-                                (idxTipo !==
-                                periodo.tipos_alimentacao.length - 1
-                                  ? ", "
-                                  : ""),
-                              ""
-                            )}
-                          </p>
-                        </td>
-                        <td className="col-2 text-center">
-                          <p>{periodo.numero_alunos}</p>
-                        </td>
-                      </tr>
-                    );
-                  })}
+                  {solicitacao.quantidades_periodo.map(
+                    (quantidade_por_periodo, idx) => {
+                      return (
+                        <tr className="row bg-white" key={idx}>
+                          {ehInclusaoContinuaENaoETEC(
+                            quantidade_por_periodo
+                          ) && (
+                            <td className="col-2 weekly">
+                              {WEEK.map((day, key) => {
+                                return (
+                                  <span
+                                    key={key}
+                                    className={
+                                      quantidade_por_periodo.dias_semana
+                                        .map(String)
+                                        .includes(day.value)
+                                        ? "week-circle-clicked green"
+                                        : "week-circle"
+                                    }
+                                    data-cy={`dia-${key}`}
+                                    value={day.value}
+                                  >
+                                    {day.label}
+                                  </span>
+                                );
+                              })}
+                            </td>
+                          )}
+                          <td className="col-2">
+                            <p>{quantidade_por_periodo.periodo_escolar.nome}</p>
+                          </td>
+                          <td
+                            className={
+                              possuiAlgumaInclusaoContinuaNaoETEC
+                                ? "col-6"
+                                : "col-8"
+                            }
+                          >
+                            <p>
+                              {quantidade_por_periodo.tipos_alimentacao.reduce(
+                                (acc, tipo, idxTipo) =>
+                                  acc +
+                                  tipo.nome +
+                                  (idxTipo !==
+                                  quantidade_por_periodo.tipos_alimentacao
+                                    .length -
+                                    1
+                                    ? ", "
+                                    : ""),
+                                ""
+                              )}
+                            </p>
+                          </td>
+                          <td className="col-2 text-center">
+                            <p>{quantidade_por_periodo.numero_alunos}</p>
+                          </td>
+                        </tr>
+                      );
+                    }
+                  )}
                 </tbody>
               </table>
             </div>
@@ -104,7 +189,8 @@ export const SolicitacoesSimilaresInclusao = ({ ...props }) => {
                 <p
                   className="value-black"
                   dangerouslySetInnerHTML={{
-                    __html: solicitacao.observacao,
+                    __html:
+                      solicitacao.quantidades_periodo[0].observacao || "-",
                   }}
                 />
               </div>
