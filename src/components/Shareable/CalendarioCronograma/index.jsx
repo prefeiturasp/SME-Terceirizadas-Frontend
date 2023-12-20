1941b27e0b95646e6f18712bd70e433469f5f71b
Luis Henrique Zimmermann <lhzimmermann@gmail.com>
2023-12-20 14:42:17 -0300

    feat(108489): transforma calendario cronograma em function component

--- src/components/Shareable/CalendarioCronograma/index.jsx
+++ src/components/Shareable/CalendarioCronograma/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HTTP_STATUS from "http-status-codes";
 import { CustomToolbar } from "components/Shareable/CalendarioCronograma/componentes/CustomToolbar";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
@@ -7,130 +7,104 @@ import { formataComoEventos } from "components/Shareable/CalendarioCronograma/he
 import { Spin } from "antd";
 import { ModalCronograma } from "components/Shareable/CalendarioCronograma/componentes/ModalCronograma";
 import "components/Shareable/CalendarioCronograma/style.scss";
+import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
-export class CalendarioCronograma extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      objetos: undefined,
-      loadingDiasCalendario: false,
-      showModalCadastrar: false,
-      showModalCronograma: false,
-      showModalConfirmarExclusao: false,
-      currentEvent: undefined,
-      mes: moment().month() + 1,
-      ano: moment().year(),
-    };
 
-    this.handleEvent = this.handleEvent.bind(this);
-    this.getObjetosAsync = this.getObjetosAsync.bind(this);
-  }
+export const CalendarioCronograma = ({
+  getObjetos,
+  nomeObjeto,
+  nomeObjetoMinusculo,
+}) => {
+  const [objetos, setObjetos] = useState(undefined);
+  const [loadingDiasCalendario, setLoadingDiasCalendario] = useState(false);
+  const [currentEvent, setCurrentEvent] = useState(undefined);
+  const [showModalCronograma, setShowModalCronograma] = useState(false);
+  const [mes, setMes] = useState(moment().month() + 1);
+  const [ano, setAno] = useState(moment().year());
 
-  componentDidMount() {
-    this.getObjetosAsync();
-  }
+  useEffect(() => {
+    getObjetosAsync();
+  });
 
-  async getObjetosAsync(params) {
-    const { getObjetos } = this.props;
-    const { mes, ano } = this.state;
+  const getObjetosAsync = async (params) => {
     this.setState({ loadingDiasCalendario: true });
     const response = await getObjetos(
       params ? { mes: params.mes, ano: params.ano } : { mes, ano }
     );
     if (response.status === HTTP_STATUS.OK) {
-      this.setState({
-        objetos: formataComoEventos(response.data.results),
-      });
+      setObjetos(formataComoEventos(response.data.results));
     }
     if (response) {
-      this.setState({ loadingDiasCalendario: false });
+      setLoadingDiasCalendario(false);
     }
-  }
+  };
 
-  handleEvent(event) {
-    this.setState({
-      currentEvent: event,
-      showModalCronograma: true,
-    });
-  }
+  const handleEvent = (event) => {
+    setCurrentEvent(event);
+    setShowModalCronograma(true);
+  };
 
-  render() {
-    const {
-      objetos,
-      loadingDiasCalendario,
-      currentEvent,
-      showModalCronograma,
-    } = this.state;
-    const { nomeObjeto, nomeObjetoMinusculo } = this.props;
-
-    return (
-      <div className="card calendario-sobremesa mt-3">
-        <div className="card-body">
-          <Spin tip="Carregando calendário..." spinning={!objetos}>
-            {objetos && (
-              <>
-                <p>
-                  Para cadastrar um dia para {nomeObjetoMinusculo}, clique sobre
-                  o dia e selecione o tipo de unidade.
-                </p>
-                <Spin
-                  tip={`Carregando dias de ${nomeObjeto}...`}
-                  spinning={loadingDiasCalendario}
-                >
-                  <Calendar
-                    style={{ height: 1000 }}
-                    formats={{
-                      weekdayFormat: (date, culture, localizer) =>
-                        localizer.format(date, "dddd", culture),
-                    }}
-                    selectable
-                    resizable={false}
-                    localizer={localizer}
-                    events={objetos}
-                    onSelectEvent={this.handleEvent}
-                    components={{
-                      toolbar: CustomToolbar,
-                    }}
-                    messages={{
-                      showMore: (target) => (
-                        <span className="ms-2" role="presentation">
-                          ...{target} mais
-                        </span>
-                      ),
-                    }}
-                    onNavigate={(date) => {
-                      this.setState({
-                        mes: date.getMonth() + 1,
-                        ano: date.getFullYear(),
-                      });
-                      this.getObjetosAsync({
-                        mes: date.getMonth() + 1,
-                        ano: date.getFullYear(),
-                      });
-                    }}
-                    defaultView={Views.MONTH}
-                  />
-                </Spin>
-                {showModalCronograma && currentEvent && (
-                  <ModalCronograma
-                    showModal={showModalCronograma}
-                    nomeObjetoNoCalendario={nomeObjeto}
-                    nomeObjetoNoCalendarioMinusculo={nomeObjetoMinusculo}
-                    closeModal={() =>
-                      this.setState({ showModalCronograma: false })
-                    }
-                    event={currentEvent}
-                    setShowModalConfirmarExclusao={() =>
-                      this.setState({ showModalConfirmarExclusao: true })
-                    }
-                  />
-                )}
-              </>
-            )}
-          </Spin>
-        </div>
+  return (
+    <div className="card calendario-sobremesa mt-3">
+      <div className="card-body">
+        <Spin tip="Carregando calendário..." spinning={!objetos}>
+          {objetos && (
+            <>
+              <p>
+                Para cadastrar um dia para {nomeObjetoMinusculo}, clique sobre o
+                dia e selecione o tipo de unidade.
+              </p>
+              <Spin
+                tip={`Carregando dias de ${nomeObjeto}...`}
+                spinning={loadingDiasCalendario}
+              >
+                <Calendar
+                  style={{ height: 1000 }}
+                  formats={{
+                    weekdayFormat: (date, culture, localizer) =>
+                      localizer.format(date, "dddd", culture),
+                  }}
+                  selectable
+                  resizable={false}
+                  localizer={localizer}
+                  events={objetos}
+                  onSelectEvent={handleEvent}
+                  components={{
+                    toolbar: CustomToolbar,
+                  }}
+                  messages={{
+                    showMore: (target) => (
+                      <span className="ms-2" role="presentation">
+                        ...{target} mais
+                      </span>
+                    ),
+                  }}
+                  onNavigate={(date) => {
+                    setMes(date.getMonth() + 1);
+                    setAno(date.getFullYear());
+                    getObjetosAsync({
+                      mes: date.getMonth() + 1,
+                      ano: date.getFullYear(),
+                    });
+                  }}
+                  defaultView={Views.MONTH}
+                />
+              </Spin>
+              {showModalCronograma && currentEvent && (
+                <ModalCronograma
+                  showModal={showModalCronograma}
+                  nomeObjetoNoCalendario={nomeObjeto}
+                  nomeObjetoNoCalendarioMinusculo={nomeObjetoMinusculo}
+                  closeModal={() => setShowModalCronograma(false)}
+                  event={currentEvent}
+                  setShowModalConfirmarExclusao={() => {}}
+                />
+              )}
+            </>
+          )}
+        </Spin>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
