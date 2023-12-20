8a3e42acd64b4f4ea5c9954d209f12aaf5707eb9
Joao Mesquita <joaovictorengp@gmail.com>
2023-12-19 23:00:57 -0300

    feat(109985): cria service getPeriodosPermissoesLancamentosEspeciaisMesAno

--- src/services/medicaoInicial/permissaoLancamentosEspeciais.service.ts
+++ src/services/medicaoInicial/permissaoLancamentosEspeciais.service.ts
@@ -29,9 +29,25 @@ export const getEscolasComPermissoesLancamentosEspeciais = async () => {
   }
 };
 
-export const getPermissoesLancamentosEspeciaisMesAno = async (params: any) => {
+export const getPermissoesLancamentosEspeciaisMesAnoPorPeriodo = async (
+  params: any
+) => {
   const url =
-    "medicao-inicial/permissao-lancamentos-especiais/permissoes-lancamentos-especiais-mes-ano/";
+    "medicao-inicial/permissao-lancamentos-especiais/permissoes-lancamentos-especiais-mes-ano-por-periodo/";
+  const response: any = await axios
+    .get(url, { params })
+    .catch(ErrorHandlerFunction);
+  if (response) {
+    const data = { data: response.data, status: response.status };
+    return data;
+  }
+};
+
+export const getPeriodosPermissoesLancamentosEspeciaisMesAno = async (
+  params: any
+) => {
+  const url =
+    "medicao-inicial/permissao-lancamentos-especiais/periodos-permissoes-lancamentos-especiais-mes-ano/";
   const response: any = await axios
     .get(url, { params })
     .catch(ErrorHandlerFunction);
