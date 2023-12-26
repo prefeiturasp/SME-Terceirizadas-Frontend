d2074b6e7554e646e67444c4e140bc730b231e4a
Elder Santos <elder_rn@hotmail.com>
2023-12-26 16:05:08 -0300

    feat(105980): corrige estilização do componente Label

--- src/components/Shareable/Label/index.jsx
+++ src/components/Shareable/Label/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./style.scss";
 
 export default ({ content, required, htmlFor }) => {
   return (
-    <>
+    <div className="custom-label">
       {required && <span className="required-asterisk">*</span>}
       <label htmlFor={htmlFor} className="col-form-label">
         {content}
       </label>
-    </>
+    </div>
   );
 };
