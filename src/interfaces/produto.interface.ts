e37e8d4943540702da8ad93bca7c61dd9ea25d68
Elder Santos <elder_rn@hotmail.com>
2023-12-26 15:01:18 -0300

    feat(105980): cria interface InformacaoNutricional e TipoNutricional

--- src/interfaces/produto.interface.ts
+++ src/interfaces/produto.interface.ts
@@ -4,3 +4,17 @@ export interface ProdutoLogistica {
   uuid: string;
   status: string;
 }
+
+export interface TipoNutricional {
+  uuid: string;
+  nome: string;
+}
+
+export interface InformacaoNutricional {
+  uuid: string;
+  nome: string;
+  medida: string;
+  eh_fixo: boolean;
+  eh_dependente: boolean;
+  tipo_nutricional: TipoNutricional;
+}
