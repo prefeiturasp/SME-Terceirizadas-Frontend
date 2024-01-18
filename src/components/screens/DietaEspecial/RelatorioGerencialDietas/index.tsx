c28c9583277f8b9da0e8294e80b737870c3a418c
Luis Guerreiro <luiscvlh11@gmail.com>
2024-01-18 12:51:59 -0300

    feat(112070): :sparkles: adiciona cards dos resultados e botao baixar pdf

--- src/components/screens/DietaEspecial/RelatorioGerencialDietas/index.tsx
+++ src/components/screens/DietaEspecial/RelatorioGerencialDietas/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 import CollapseFiltros from "components/Shareable/CollapseFiltros";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_ICON,
+  BUTTON_STYLE,
+  BUTTON_TYPE,
+} from "components/Shareable/Botao/constants";
 
 import FormFiltro from "./components/FormFiltro";
+import CardResultado from "./components/CardResultado";
 
 export default () => {
   const filtrar = () => {};
   const limparFiltro = () => {};
+  const exportarPDF = () => {};
 
   return (
     <div className="card mt-3">
@@ -18,6 +26,29 @@ export default () => {
         >
           {(values, form) => <FormFiltro values={values} form={form} />}
         </CollapseFiltros>
+        <div className="d-flex gap-2 mt-4">
+          <CardResultado
+            titulo="Total de Solicitações"
+            classeCor="azul-escuro"
+            total={1253}
+          />
+          <CardResultado
+            titulo="Autorizadas"
+            classeCor="verde-claro"
+            total={1000}
+          />
+          <CardResultado titulo="Negadas" classeCor="laranja" total={53} />
+          <CardResultado titulo="Canceladas" classeCor="vermelho" total={100} />
+        </div>
+        <div className="d-flex justify-content-end mt-5">
+          <Botao
+            texto="Baixar PDF"
+            style={BUTTON_STYLE.GREEN_OUTLINE}
+            icon={BUTTON_ICON.FILE_PDF}
+            type={BUTTON_TYPE.BUTTON}
+            onClick={() => exportarPDF()}
+          />
+        </div>
       </div>
     </div>
   );
