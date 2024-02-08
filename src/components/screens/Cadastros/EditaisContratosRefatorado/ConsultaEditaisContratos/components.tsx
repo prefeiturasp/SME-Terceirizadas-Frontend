fd18bbf9000dc749a26deb985936f41a604702b1
Calvin Rossinhole <calvin.masters@gmail.com>
2024-02-08 17:18:49 -0300

    refactor(113179): refatora campos edital contrato

--- src/components/screens/Cadastros/EditaisContratosRefatorado/ConsultaEditaisContratos/components.tsx
+++ src/components/screens/Cadastros/EditaisContratosRefatorado/ConsultaEditaisContratos/components.tsx
@@ -135,3 +135,86 @@ export const LinhaEditalContrato = ({
     </div>
   );
 };
+
+export const CamposEditalContrato = ({ editalContrato }) => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-2 title">Objeto resumido:</div>
+        <div className="col-10">{editalContrato.objeto}</div>
+      </div>
+      {editalContrato.contratos.map((contrato, indexContrato) => {
+        return (
+          <div key={indexContrato}>
+            <div className="row pt-3">
+              <div className="col-12">
+                <div className="label">
+                  <span className="com-linha">Contratos Relacionados</span>
+                </div>
+              </div>
+            </div>
+            <div className="row pt-2">
+              <div className="col-6 d-flex">
+                <div className="title pe-2">Contrato nº:</div>
+                {contrato.numero}
+              </div>
+              <div className="col-6">
+                {contrato.vigencias.map((vigencia, indexVigencia) => {
+                  return (
+                    <div key={indexVigencia}>
+                      {indexVigencia === 0 && (
+                        <span className="title pe-2">Vigência:</span>
+                      )}
+                      <span
+                        className={`${
+                          indexVigencia === contrato.vigencias.length - 1
+                            ? vigencia.status
+                            : ""
+                        }`}
+                        style={{
+                          paddingLeft: `${indexVigencia > 0 ? "4.7em" : 0}`,
+                        }}
+                      >
+                        {vigencia.data_inicial} até {vigencia.data_final}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="row pt-2">
+              <div className="col-6 d-flex">
+                <div className="title pe-2">
+                  Processo administrativo do contrato:
+                </div>
+                {contrato.processo}
+              </div>
+              <div className="col-6 d-flex">
+                <div className="title pe-2">Data da proposta:</div>
+                {contrato.data_proposta}
+              </div>
+            </div>
+            <div className="row pt-2">
+              <div className="col-6">
+                <div className="title">Lotes:</div>
+                {contrato.lotes.map((lote) => lote.nome).join(" | ")}
+              </div>
+              <div className="col-6">
+                <div className="title">DREs:</div>
+                {contrato.diretorias_regionais.map((dre, indexDRE) => {
+                  return <div key={indexDRE}>{dre.nome}</div>;
+                })}
+              </div>
+            </div>
+            <div className="row pt-2">
+              <div className="col-12">
+                <div className="title">Empresa:</div>
+                {contrato.terceirizada.nome_fantasia}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
--- src/components/screens/Cadastros/EditaisContratosRefatorado/ConsultaEditaisContratos/index.tsx
+++ src/components/screens/Cadastros/EditaisContratosRefatorado/ConsultaEditaisContratos/index.tsx
@@ -8,7 +8,11 @@ import { Collapse } from "react-collapse";
 import { Form } from "react-final-form";
 import { getEditaisContratos } from "services/edital.service.js";
 import { EditalContratoListadoInterface } from "../interfaces";
-import { Header, LinhaEditalContrato } from "./components";
+import {
+  CamposEditalContrato,
+  Header,
+  LinhaEditalContrato,
+} from "./components";
 import "./style.scss";
 
 export const ConsultaEditaisContratos = () => {
@@ -95,108 +99,9 @@ export const ConsultaEditaisContratos = () => {
                         />
                         <hr />
                         <Collapse isOpened={editalContrato.ativo}>
-                          <div className="row">
-                            <div className="col-2 title">Objeto resumido:</div>
-                            <div className="col-10">
-                              {editalContrato.objeto}
-                            </div>
-                          </div>
-                          {editalContrato.contratos.map(
-                            (contrato, indexContrato) => {
-                              return (
-                                <div key={indexContrato}>
-                                  <div className="row pt-3">
-                                    <div className="col-12">
-                                      <div className="label">
-                                        <span className="com-linha">
-                                          Contratos Relacionados
-                                        </span>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div className="row pt-2">
-                                    <div className="col-6 d-flex">
-                                      <div className="title pe-2">
-                                        Contrato nº:
-                                      </div>
-                                      {contrato.numero}
-                                    </div>
-                                    <div className="col-6">
-                                      {contrato.vigencias.map(
-                                        (vigencia, indexVigencia) => {
-                                          return (
-                                            <div key={indexVigencia}>
-                                              {indexVigencia === 0 && (
-                                                <span className="title pe-2">
-                                                  Vigência:
-                                                </span>
-                                              )}
-                                              <span
-                                                className={`${
-                                                  indexVigencia ===
-                                                  contrato.vigencias.length - 1
-                                                    ? vigencia.status
-                                                    : ""
-                                                }`}
-                                                style={{
-                                                  paddingLeft: `${
-                                                    indexVigencia > 0
-                                                      ? "4.7em"
-                                                      : 0
-                                                  }`,
-                                                }}
-                                              >
-                                                {vigencia.data_inicial} até{" "}
-                                                {vigencia.data_final}
-                                              </span>
-                                            </div>
-                                          );
-                                        }
-                                      )}
-                                    </div>
-                                  </div>
-                                  <div className="row pt-2">
-                                    <div className="col-6 d-flex">
-                                      <div className="title pe-2">
-                                        Processo administrativo do contrato:
-                                      </div>
-                                      {contrato.processo}
-                                    </div>
-                                    <div className="col-6 d-flex">
-                                      <div className="title pe-2">
-                                        Data da proposta:
-                                      </div>
-                                      {contrato.data_proposta}
-                                    </div>
-                                  </div>
-                                  <div className="row pt-2">
-                                    <div className="col-6">
-                                      <div className="title">Lotes:</div>
-                                      {contrato.lotes
-                                        .map((lote) => lote.nome)
-                                        .join(" | ")}
-                                    </div>
-                                    <div className="col-6">
-                                      <div className="title">DREs:</div>
-                                      {contrato.diretorias_regionais.map(
-                                        (dre, indexDRE) => {
-                                          return (
-                                            <div key={indexDRE}>{dre.nome}</div>
-                                          );
-                                        }
-                                      )}
-                                    </div>
-                                  </div>
-                                  <div className="row pt-2">
-                                    <div className="col-12">
-                                      <div className="title">Empresa:</div>
-                                      {contrato.terceirizada.nome_fantasia}
-                                    </div>
-                                  </div>
-                                </div>
-                              );
-                            }
-                          )}
+                          <CamposEditalContrato
+                            editalContrato={editalContrato}
+                          />
                         </Collapse>
                       </div>
                     );
