954bf31ae8f23037e439b517628833cacfa1f364
Elder Santos <elder_rn@hotmail.com>
2024-03-07 10:05:12 -0300

    refactor(114538): aplica tratamento de erros no service listarLayoutsEmbalagens

--- src/services/layoutEmbalagem.service.js
+++ src/services/layoutEmbalagem.service.js
@@ -1,10 +1,18 @@
+import { getMensagemDeErro } from "../helpers/statusErrors";
+import { toastError } from "components/Shareable/Toast/dialogs";
+
 import axios from "./_base";
 
 export const cadastraLayoutEmbalagem = async (payload) =>
   await axios.post("/layouts-de-embalagem/", payload);
 
-export const listarLayoutsEmbalagens = async (params) =>
-  await axios.get("/layouts-de-embalagem/", { params });
+export const listarLayoutsEmbalagens = async (params) => {
+  try {
+    return await axios.get("/layouts-de-embalagem/", { params });
+  } catch (error) {
+    toastError(getMensagemDeErro(error.response.status));
+  }
+};
 
 export const detalharLayoutEmabalagem = async (uuid) =>
   await axios.get(`/layouts-de-embalagem/${uuid}/`);
