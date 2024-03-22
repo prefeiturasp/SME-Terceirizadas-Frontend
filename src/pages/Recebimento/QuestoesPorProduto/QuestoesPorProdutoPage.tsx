689485cad6f06729a7997858fa28bd5e7892daf1
Elder Santos <elder_rn@hotmail.com>
2024-03-21 21:26:40 -0300

    feat(117391): cria screen inicial da page Questões por Produto

--- src/pages/Recebimento/QuestoesPorProduto/QuestoesPorProdutoPage.tsx
+++ src/pages/Recebimento/QuestoesPorProduto/QuestoesPorProdutoPage.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { HOME, RECEBIMENTO, QUESTOES_POR_PRODUTO } from "constants/config";
 import Page from "components/Shareable/Page/Page";
 import Breadcrumb from "components/Shareable/Breadcrumb";
+import QuestoesPorProduto from "components/screens/Recebimento/QuestoesPorProduto";
 
 const atual = {
   href: `/${RECEBIMENTO}/${QUESTOES_POR_PRODUTO}`,
@@ -19,6 +20,6 @@ const anteriores = [
 export default () => (
   <Page botaoVoltar voltarPara="/" titulo={atual.titulo}>
     <Breadcrumb home={HOME} atual={atual} anteriores={anteriores} />
-    <></>
+    <QuestoesPorProduto />
   </Page>
 );
