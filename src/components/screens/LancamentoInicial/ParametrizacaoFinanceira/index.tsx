463053ee694d750114c3a3fc384f0ffecd36e193
Luis Guerreiro <luiscvlh11@gmail.com>
2024-03-25 11:55:57 -0300

    feat(101737): :sparkles: inclui botao de adicionar parametrizacao

--- src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/index.tsx
+++ src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_STYLE,
+  BUTTON_TYPE,
+} from "components/Shareable/Botao/constants";
+
 export default () => {
-  return <></>;
+  return (
+    <Botao
+      texto="Adicionar Parametrização"
+      titulo="Adicionar Parametrização"
+      className="mt-4"
+      onClick={() => {}}
+      style={BUTTON_STYLE.GREEN}
+      type={BUTTON_TYPE.BUTTON}
+    />
+  );
 };
