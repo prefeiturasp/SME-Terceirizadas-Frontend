6dd450b52f3710f10c17c1260f5d3b30f9d9cb4c
Luis Guerreiro <luiscvlh11@gmail.com>
2024-03-26 18:59:09 -0300

    feat(101737): :sparkles: inclui tabela de alimentacao para preenchimento dos valores unitarios e de reajuste

--- src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/AdicionarParametrizacaoFinanceira/index.tsx
+++ src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/AdicionarParametrizacaoFinanceira/index.tsx
@@ -2,7 +2,8 @@ import React, { ChangeEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Form, Field } from "react-final-form";
 
-import { Skeleton } from "antd";
+import { Skeleton, Table } from "antd";
+import Column from "antd/es/table/Column";
 
 import { required } from "helpers/fieldValidators";
 
@@ -12,10 +13,19 @@ import { getTiposUnidadeEscolar } from "services/cadastroTipoAlimentacao.service
 
 import { Select } from "components/Shareable/Select";
 import { Botao } from "components/Shareable/Botao";
+import { AInput, AInputNumber } from "components/Shareable/MakeField";
+
 import {
   BUTTON_STYLE,
   BUTTON_TYPE,
 } from "components/Shareable/Botao/constants";
+
+import {
+  formataValorDecimal,
+  parserValorDecimal,
+} from "components/screens/helper";
+
+import "./style.scss";
 
 type SelectOption = {
   uuid: string | Array<string>;
@@ -35,6 +45,7 @@ export default () => {
     SelectOption[]
   >([]);
   const [tiposUnidades, setTiposUnidades] = useState([]);
+  const [tiposAlimentacao, setTiposAlimentacao] = useState([]);
   const [carregando, setCarregando] = useState(true);
   const [erroAPI, setErroAPI] = useState("");
 
@@ -83,7 +94,8 @@ export default () => {
     const grupos = [
       ["CEI", "CEI CEU", "CCI"],
       ["CEMEI", "CEU CEMEI"],
-      ["EMEF", "CEU EMEF", "EMEFM", "EMEBS", "CIEJA", "CEU GESTAO"],
+      ["EMEF", "CEU EMEF", "EMEFM", "CIEJA", "CEU GESTAO"],
+      ["EMEBS"],
       ["EMEI", "CEU EMEI"],
     ];
 
@@ -157,8 +169,7 @@ export default () => {
       tiposAlimentacaoUnicos[tipoAlimentacao.uuid] = tipoAlimentacao.nome;
     });
 
-    // eslint-disable-next-line
-    console.log(
+    setTiposAlimentacao(
       Object.entries(tiposAlimentacaoUnicos).map(([uuid, nome]) => ({
         uuid,
         nome,
@@ -230,6 +241,81 @@ export default () => {
                     )}
                   </div>
                 </div>
+                {tiposAlimentacao.length > 0 && (
+                  <div className="row mt-5">
+                    <div className="col-4">
+                      <h2 className="text-start texto-simples-verde fw-bold">
+                        Preço das Alimentações Convencionais
+                      </h2>
+                      <Table
+                        pagination={false}
+                        bordered
+                        dataSource={tiposAlimentacao}
+                      >
+                        <Column
+                          title="Tipo de Alimentação"
+                          dataIndex="nome"
+                          key="nome"
+                          render={(value, record: any) => {
+                            return (
+                              <div className="d-flex gap-4 justify-content-between align-items-center">
+                                <p className="fw-bold mb-0">{value}</p>
+                                <Field
+                                  component="input"
+                                  name={`tabelas.alimentacao.${value}.lanche`}
+                                  type="hidden"
+                                  defaultValue={record.uuid}
+                                />
+                                <Field
+                                  component={AInput}
+                                  name={`tabelas.alimentacao.${value}.grupo`}
+                                />
+                              </div>
+                            );
+                          }}
+                        />
+                        <Column
+                          title="Valor Unitário"
+                          dataIndex="valor_unitario"
+                          key="valor_unitario"
+                          render={(_, record: any) => (
+                            <Field
+                              component={AInputNumber}
+                              name={`tabelas.alimentacao.${record.nome}.valor_unitario`}
+                              placeholder="0,00"
+                              min={0}
+                              formatter={(value: string) =>
+                                formataValorDecimal(value)
+                              }
+                              parser={(value: string) =>
+                                parserValorDecimal(value)
+                              }
+                            />
+                          )}
+                        />
+                        <Column
+                          title="Valor Unitário Reajuste"
+                          dataIndex="valor_unitario_reajuste"
+                          key="valor_unitario_reajuste"
+                          render={(_, record: any) => (
+                            <Field
+                              component={AInputNumber}
+                              name={`tabelas.alimentacao.${record.nome}.valor_unitario_reajuste`}
+                              placeholder="0,00"
+                              min={0}
+                              formatter={(value: string) =>
+                                formataValorDecimal(value)
+                              }
+                              parser={(value: string) =>
+                                parserValorDecimal(value)
+                              }
+                            />
+                          )}
+                        />
+                      </Table>
+                    </div>
+                  </div>
+                )}
                 <div className="d-flex justify-content-end gap-3">
                   <Botao
                     texto="Cancelar"
