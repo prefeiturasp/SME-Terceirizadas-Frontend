f76c7e5ce5c3be6f61684df71b94af72d2fdb0f9
Luis Guerreiro <luiscvlh11@gmail.com>
2024-03-28 10:36:30 -0300

    feat(101737): :sparkles: exibe modal ao clicar em cancelar na tela de adicao de parametrizacao financeira

--- src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/AdicionarParametrizacaoFinanceira/index.tsx
+++ src/components/screens/LancamentoInicial/ParametrizacaoFinanceira/AdicionarParametrizacaoFinanceira/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Field, Form } from "react-final-form";
+import { Modal } from "react-bootstrap";
 
 import { TextArea } from "components/Shareable/TextArea/TextArea";
 
@@ -26,6 +27,7 @@ type FormValues = {
 export default () => {
   const [tiposUnidades, setTiposUnidades] = useState([]);
   const [tiposAlimentacao, setTiposAlimentacao] = useState([]);
+  const [showModalCancelar, setShowModalCancelar] = useState(false);
 
   const navigate = useNavigate();
 
@@ -35,74 +37,111 @@ export default () => {
   };
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <Form
-          onSubmit={onSubmit}
-          initialValues={{
-            edital: "",
-            lote: "",
-            tipos_unidades: "",
-          }}
-          render={({ form, handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <Filtros
-                tiposUnidades={tiposUnidades}
-                setTiposUnidades={setTiposUnidades}
-                setTiposAlimentacao={setTiposAlimentacao}
-              />
-              {tiposAlimentacao.length > 0 && (
-                <>
-                  <TabelaAlimentacao
-                    form={form}
-                    tiposAlimentacao={tiposAlimentacao}
-                    tiposUnidades={tiposUnidades}
-                  />
-                  <div className="d-flex gap-4">
-                    <TabelaDietaTipoA
+    <>
+      <div className="card">
+        <div className="card-body">
+          <Form
+            onSubmit={onSubmit}
+            initialValues={{
+              edital: "",
+              lote: "",
+              tipos_unidades: "",
+            }}
+            render={({ form, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <Filtros
+                  tiposUnidades={tiposUnidades}
+                  setTiposUnidades={setTiposUnidades}
+                  setTiposAlimentacao={setTiposAlimentacao}
+                />
+                {tiposAlimentacao.length > 0 && (
+                  <>
+                    <TabelaAlimentacao
                       form={form}
                       tiposAlimentacao={tiposAlimentacao}
+                      tiposUnidades={tiposUnidades}
                     />
-                    <TabelaDietaTipoB
-                      form={form}
-                      tiposAlimentacao={tiposAlimentacao}
-                    />
-                  </div>
-                  <div className="row mt-5">
-                    <div className="col">
-                      <Field
-                        component={TextArea}
-                        label="Legenda"
-                        name="legenda"
-                        placeholder={
-                          "Insira uma Legenda para os lançamentos\n\n" +
-                          "Fonte: Relatório de Medição Inicial do Serviço de Alimentação e Nutrição Escolar realizada pela direção das unidades educacionais, conforme disposto no edital Pregão 78/2016 e nas Portarias Intersecretariais SMG/SME n° 005/2006 e 001/2008."
-                        }
-                        maxLength={1500}
-                        height="150"
+                    <div className="d-flex gap-4">
+                      <TabelaDietaTipoA
+                        form={form}
+                        tiposAlimentacao={tiposAlimentacao}
+                      />
+                      <TabelaDietaTipoB
+                        form={form}
+                        tiposAlimentacao={tiposAlimentacao}
                       />
                     </div>
-                  </div>
-                </>
-              )}
-              <div className="d-flex justify-content-end gap-3 mt-5">
-                <Botao
-                  texto="Cancelar"
-                  onClick={() => {
-                    navigate(-1);
-                  }}
-                  style={BUTTON_STYLE.GREEN_OUTLINE}
-                />
-                <Botao
-                  texto="Salvar"
-                  style={BUTTON_STYLE.GREEN}
-                  type={BUTTON_TYPE.SUBMIT}
-                />
-              </div>
-            </form>
-          )}
-        />
+                    <div className="row mt-5">
+                      <div className="col">
+                        <Field
+                          component={TextArea}
+                          label="Legenda"
+                          name="legenda"
+                          placeholder={
+                            "Insira uma Legenda para os lançamentos\n\n" +
+                            "Fonte: Relatório de Medição Inicial do Serviço de Alimentação e Nutrição Escolar realizada pela direção das unidades educacionais, conforme disposto no edital Pregão 78/2016 e nas Portarias Intersecretariais SMG/SME n° 005/2006 e 001/2008."
+                          }
+                          maxLength={1500}
+                          height="150"
+                        />
+                      </div>
+                    </div>
+                  </>
+                )}
+                <div className="d-flex justify-content-end gap-3 mt-5">
+                  <Botao
+                    texto="Cancelar"
+                    onClick={() => {
+                      setShowModalCancelar(true);
+                    }}
+                    style={BUTTON_STYLE.GREEN_OUTLINE}
+                  />
+                  <Botao
+                    texto="Salvar"
+                    style={BUTTON_STYLE.GREEN}
+                    type={BUTTON_TYPE.SUBMIT}
+                  />
+                </div>
+              </form>
+            )}
+          />
+        </div>
       </div>
-    </div>
+      <Modal
+        show={showModalCancelar}
+        onHide={() => {
+          setShowModalCancelar(false);
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Cancelar Adição de Parametrização Financeira
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Você deseja cancelar a Adição da Parametrização Financeira?
+        </Modal.Body>
+        <Modal.Footer>
+          <Botao
+            texto="Não"
+            type={BUTTON_TYPE.BUTTON}
+            onClick={() => {
+              setShowModalCancelar(false);
+            }}
+            style={BUTTON_STYLE.GREEN_OUTLINE}
+            className="ms-3"
+          />
+          <Botao
+            texto="Sim"
+            type={BUTTON_TYPE.BUTTON}
+            onClick={() => {
+              navigate(-1);
+            }}
+            style={BUTTON_STYLE.GREEN}
+            className="ms-3"
+          />
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
