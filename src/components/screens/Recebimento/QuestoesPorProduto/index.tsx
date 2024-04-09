6ae9cae4b07ffbe54626d28233fde41994c2312c
Elder Santos <elder_rn@hotmail.com>
2024-04-09 20:09:04 -0300

    feat(117399): adiciona Filtros, Listagem e Paginacao na página de Questões por Produtos

--- src/components/screens/Recebimento/QuestoesPorProduto/index.tsx
+++ src/components/screens/Recebimento/QuestoesPorProduto/index.tsx
@@ -1,23 +1,125 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { Spin } from "antd";
 
-import { RECEBIMENTO, ATRIBUIR_QUESTOES_CONFERENCIA } from "configs/constants";
+import { FichaTecnicaSimples } from "interfaces/pre_recebimento.interface";
 import {
-  BUTTON_TYPE,
-  BUTTON_STYLE,
-} from "components/Shareable/Botao/constants";
-import Botao from "components/Shareable/Botao";
+  FiltrosQuestoesPorProduto,
+  QuestaoConferencia,
+  QuestoesPorProduto,
+} from "interfaces/recebimento.interface";
+import { getListaFichasTecnicasSimples } from "services/fichaTecnica.service";
+import {
+  listarQuestoesConferenciaSimples,
+  listarQuestoesPorProduto,
+} from "services/recebimento/questoesConferencia.service";
+import { gerarParametrosConsulta } from "helpers/utilities";
+import { Paginacao } from "components/Shareable/Paginacao";
+
+import Filtros from "./components/Filtros";
+import Listagem from "./components/Listagem";
 
 export default () => {
+  const [carregando, setCarregando] = useState(false);
+
+  const [fichasTecnicas, setFichasTecnicas] = useState<FichaTecnicaSimples[]>(
+    []
+  );
+  const [questoesConferencia, setQuestoesConferencia] = useState<
+    QuestaoConferencia[]
+  >([]);
+  const [questoesPorProdutos, setQuestoesPorProdutos] = useState<
+    QuestoesPorProduto[]
+  >([]);
+
+  const [filtros, setFiltros] = useState<FiltrosQuestoesPorProduto>(
+    {} as FiltrosQuestoesPorProduto
+  );
+  const [consultaRealizada, setConsultaRealizada] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResultados, setTotalResultados] = useState(0);
+
+  const carregarDados = useCallback(async () => {
+    setCarregando(true);
+
+    try {
+      const [responseFichas, responseQuestoes] = await Promise.all([
+        getListaFichasTecnicasSimples(),
+        listarQuestoesConferenciaSimples(),
+        buscarResultados(1),
+      ]);
+
+      setFichasTecnicas(responseFichas.data.results);
+      setQuestoesConferencia(responseQuestoes.data.results);
+      setPage(1);
+    } finally {
+      setCarregando(false);
+    }
+  }, []);
+
+  const buscarResultados = async (pageNumber: number) => {
+    try {
+      setCarregando(true);
+
+      const params: URLSearchParams = gerarParametrosConsulta({
+        page: pageNumber,
+        ...filtros,
+      });
+      const response = await listarQuestoesPorProduto(params);
+
+      if (response?.status === 200) {
+        setQuestoesPorProdutos(response.data.results);
+        setTotalResultados(response.data.count);
+      }
+    } finally {
+      setCarregando(false);
+    }
+  };
+
+  useEffect(() => {
+    carregarDados();
+  }, []);
+
+  useEffect(() => {
+    buscarResultados(1);
+    setPage(1);
+  }, [filtros]);
+
+  const carregarProximaPagina = (page: number) => {
+    buscarResultados(page);
+    setPage(page);
+  };
+
   return (
-    <div className="pt-4 pb-4">
-      <NavLink to={`/${RECEBIMENTO}/${ATRIBUIR_QUESTOES_CONFERENCIA}`}>
-        <Botao
-          texto="Atribuir Questões"
-          type={BUTTON_TYPE.BUTTON}
-          style={BUTTON_STYLE.GREEN}
-        />
-      </NavLink>
-    </div>
+    <Spin tip="Carregando..." spinning={carregando}>
+      <div className="card mt-3 card-ficha-tecnica">
+        <div className="card-body ficha-tecnica">
+          <Filtros
+            fichasTecnicas={fichasTecnicas}
+            questoesConferencia={questoesConferencia}
+            setFiltros={setFiltros}
+            setQuestoesPorProduto={setQuestoesPorProdutos}
+            setConsultaRealizada={setConsultaRealizada}
+          />
+
+          {questoesPorProdutos.length ? (
+            <>
+              <Listagem questoesPorProdutos={questoesPorProdutos} />
+
+              <Paginacao
+                current={page}
+                total={totalResultados}
+                onChange={carregarProximaPagina}
+              />
+            </>
+          ) : (
+            consultaRealizada && (
+              <div className="text-center mt-3 mb-4">
+                Nenhum resultado encontrado
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </Spin>
   );
 };
