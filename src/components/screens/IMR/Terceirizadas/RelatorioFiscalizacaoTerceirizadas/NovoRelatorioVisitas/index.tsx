2f2b0a345e16d03d13f07ede4073c79c20f45e49
Calvin Rossinhole <calvin.masters@gmail.com>
2024-05-08 15:43:50 -0300

    feat(106927): :sparkles: botoes cancelar enviar rascunho

--- src/components/screens/IMR/Terceirizadas/RelatorioFiscalizacaoTerceirizadas/NovoRelatorioVisitas/index.tsx
+++ src/components/screens/IMR/Terceirizadas/RelatorioFiscalizacaoTerceirizadas/NovoRelatorioVisitas/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Form } from "react-final-form";
 import { Cabecalho } from "./components/Cabecalho";
+import Botao from "components/Shareable/Botao";
+import {
+  BUTTON_STYLE,
+  BUTTON_TYPE,
+} from "components/Shareable/Botao/constants";
 import "./styles.scss";
 
 export const NovoRelatorioVisitas = () => {
@@ -10,9 +15,34 @@ export const NovoRelatorioVisitas = () => {
     <div className="card novo-relatorio-visitas mt-3">
       <div className="card-body">
         <Form onSubmit={onSubmit}>
-          {({ handleSubmit, form }) => (
+          {({ handleSubmit, values, form, submitting }) => (
             <form onSubmit={handleSubmit}>
               <Cabecalho values={form.getState().values} form={form} />
+              <div className="row">
+                <div className="col-12">
+                  <hr />
+                </div>
+              </div>
+              <div className="row float-end mt-4">
+                <div className="col-12">
+                  <Botao
+                    texto="Cancelar"
+                    onClick={() => {
+                      form.reset();
+                    }}
+                    style={BUTTON_STYLE.GREEN_OUTLINE}
+                  />
+                  <Botao
+                    texto={
+                      values.uuid ? "Atualizar rascunho" : "Salvar rascunho"
+                    }
+                    className="ms-3"
+                    disabled={submitting}
+                    type={BUTTON_TYPE.SUBMIT}
+                    style={BUTTON_STYLE.GREEN_OUTLINE}
+                  />
+                </div>
+              </div>
             </form>
           )}
         </Form>
