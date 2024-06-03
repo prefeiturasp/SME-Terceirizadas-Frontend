a0ac24a4446df58dfcaebab86318985eb4b11025
Calvin Rossinhole <calvin.masters@gmail.com>
2024-06-03 10:49:50 -0300

    chore(-): :fire: remove arquivo empresa.reducer.js inutilizado

--- src/reducers.js
+++ src/reducers.js
@@ -1,26 +1,25 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
-import suspensaoDeAlimentacaoReducer from "./reducers/suspensaoDeAlimentacaoReducer";
-import inversaoDeDiaDeCardapioReducer from "./reducers/inversaoDeDiaDeCardapio.reducer";
 import alteracaoCardapioReducer from "./reducers/alteracaoCardapioReducer";
-import loteReducer from "./reducers/lote.reducer";
-import loadTipoAlimentacao from "./reducers/tipoAlimentacaoReducer";
-import loadEmpresa from "./reducers/empresa.reducer";
-import loadFiltroBusca from "./reducers/loadFiltroBusca";
-import loadProduto from "./reducers/produto.reducer";
-import kitLancheReducer from "./reducers/kitLanche.reducer";
-import { produtoReducer } from "./reducers/produtoReducer";
-import finalFormReducer from "./reducers/finalForm";
 import avaliarReclamacaoProdutoReducer from "./reducers/avaliarReclamacaoProduto";
-import reclamacaoProdutoReducer from "./reducers/reclamacaoProduto";
-import responderAnaliseSensorialReducer from "./reducers/responderAnaliseSensorial";
 import buscaAvancadaProdutoReducer from "./reducers/buscaAvancadaProduto";
-import responderReclamacaoProdutoReducer from "./reducers/responderReclamacaoProduto";
-import incluirDietaEspecialReducer from "./reducers/incluirDietaEspecialReducer";
 import dietasAtivasInativasPorAlunoReducer from "./reducers/dietasAtivasInativasPorAlunoReducer";
+import { filtersAlimentacaoReducer } from "./reducers/filtersAlimentacaoReducer";
 import { filtersDietaReducer } from "./reducers/filtersDietaReducer";
 import { filtersProdutoReducer } from "./reducers/filtersProdutoReducer";
-import { filtersAlimentacaoReducer } from "./reducers/filtersAlimentacaoReducer";
+import finalFormReducer from "./reducers/finalForm";
+import incluirDietaEspecialReducer from "./reducers/incluirDietaEspecialReducer";
+import inversaoDeDiaDeCardapioReducer from "./reducers/inversaoDeDiaDeCardapio.reducer";
+import kitLancheReducer from "./reducers/kitLanche.reducer";
+import loadFiltroBusca from "./reducers/loadFiltroBusca";
+import loteReducer from "./reducers/lote.reducer";
+import loadProduto from "./reducers/produto.reducer";
+import { produtoReducer } from "./reducers/produtoReducer";
+import reclamacaoProdutoReducer from "./reducers/reclamacaoProduto";
+import responderAnaliseSensorialReducer from "./reducers/responderAnaliseSensorial";
+import responderReclamacaoProdutoReducer from "./reducers/responderReclamacaoProduto";
+import suspensaoDeAlimentacaoReducer from "./reducers/suspensaoDeAlimentacaoReducer";
+import loadTipoAlimentacao from "./reducers/tipoAlimentacaoReducer";
 
 // Junta todos os reducers do sistema! Vai pra store
 const rootReducer = combineReducers({
@@ -30,7 +29,6 @@ const rootReducer = combineReducers({
   loteForm: loteReducer,
   suspensaoDeAlimentacao: suspensaoDeAlimentacaoReducer,
   TipoDeAlimentacaoForm: loadTipoAlimentacao,
-  cadastroEmpresaForm: loadEmpresa,
   FiltrosDeBuscaForm: loadFiltroBusca,
   kitLanche: kitLancheReducer,
   cadastroProduto: loadProduto,
