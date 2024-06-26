a8f2f851df0ec4411f6cadcdedc6d9c494438ffd
Kelwy Oliveira <kelwyduarte@gmail.com>
2024-06-26 09:25:24 -0300

    feat(122895): :sparkles: cria e realoca services

--- src/services/imr/painelGerencial/index.tsx
+++ src/services/imr/painelGerencial/index.tsx
@@ -1,7 +1,13 @@
 import axios from "../../_base";
 import { API_URL } from "constants/config";
 import { ErrorHandlerFunction } from "../../service-helpers";
+import { getMensagemDeErro } from "helpers/statusErrors";
+import { toastError } from "components/Shareable/Toast/dialogs";
 import { FiltrosRelatoriosVisitasInterface } from "interfaces/imr.interface";
+import {
+  ResponseNomesNutricionistas,
+  ResponseRelatoriosVisitas,
+} from "interfaces/responses.interface";
 
 export const getDashboardPainelGerencialSupervisao = async (
   params: FiltrosRelatoriosVisitasInterface
@@ -13,3 +19,23 @@ export const getDashboardPainelGerencialSupervisao = async (
     return data;
   }
 };
+
+export const getListRelatoriosVisitaSupervisao = async (
+  params: URLSearchParams
+): Promise<ResponseRelatoriosVisitas> => {
+  try {
+    return await axios.get("/imr/formulario-supervisao/", { params });
+  } catch (error) {
+    toastError(getMensagemDeErro(error.response.status));
+  }
+};
+
+export const getListNomesNutricionistas =
+  async (): Promise<ResponseNomesNutricionistas> => {
+    const url = "/imr/formulario-supervisao/lista_nomes_nutricionistas/";
+    try {
+      return await axios.get(url);
+    } catch (error) {
+      toastError(getMensagemDeErro(error.response.status));
+    }
+  };
