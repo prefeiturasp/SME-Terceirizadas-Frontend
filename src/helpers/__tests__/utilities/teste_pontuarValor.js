f2ca3cd6dcd4edc39cfc301947d4af487ecb00bb
Calvin Rossinhole <calvin.masters@gmail.com>
2024-12-09 17:58:40 -0300

    test(-): :heavy_check_mark: corrige teste_pontuarValor

--- src/helpers/__tests__/utilities/teste_pontuarValor.js
+++ src/helpers/__tests__/utilities/teste_pontuarValor.js
@@ -2,7 +2,7 @@ import { pontuarValor } from "../../utilities";
 
 describe("Teste pontuarValor", () => {
   it("retorna valor pontuado", () => {
-    expect(pontuarValor(123456789)).toEqual("123.456.789");
+    expect(pontuarValor(123456789)).toEqual("123,456,789");
   });
   it("retorna valor sem pontuar", () => {
     expect(pontuarValor(123)).toEqual("123");
